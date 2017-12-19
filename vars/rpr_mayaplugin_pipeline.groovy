e6fe0f1b08fcaacca628e6b6ad163ca28baabbf4
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2017-12-19 16:47:31 +0300

    Update rpr_mayaplugin_pipeline.groovy

--- vars/rpr_mayaplugin_pipeline.groovy
+++ vars/rpr_mayaplugin_pipeline.groovy
@@ -1,30 +1,3 @@
-def notifyBuild(String buildStatus = 'STARTED') {
-  // build status of null means successful
-  buildStatus =  buildStatus ?: 'SUCCESSFUL'
- 
-  // Default values
-  def colorName = 'RED'
-  def colorCode = '#FF0000'
-  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def summary = "${subject} (${env.BUILD_URL})"
-  def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-    <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>"""
- 
-  // Override default values based on build status
-  if (buildStatus == 'STARTED') {
-    color = 'YELLOW'
-    colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESSFUL') {
-    color = 'GREEN'
-    colorCode = '#00FF00'
-  } else {
-    color = 'RED'
-    colorCode = '#FF0000'
-  }
- 
-  // Send notifications
-  slackSend (color: colorCode, message: summary)
-}
 
 def call(Map pipelineParams) {
   
@@ -466,7 +439,7 @@ def call(Map pipelineParams) {
         post {
             always {
                 echo 'sending notification result...'
-                notifyBuild(currentBuild.result)
+                sendBuildStatusNotification(currentBuild.result)
             }
         }
     }
