733d1dcd6bb1e75ec4d64905572810f00a5c5900
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2017-12-20 19:45:47 +0300

    Update checkOutBranchOrScm.groovy

--- vars/checkOutBranchOrScm.groovy
+++ vars/checkOutBranchOrScm.groovy
@@ -7,7 +7,7 @@ def call(String branchName, String repoName) {
             [$class: 'CleanCheckout'],
             [$class: 'CheckoutOption', timeout: 30],
             [$class: 'CloneOption', timeout: 30, noTags: false]
-            ], submoduleCfg: [], userRemoteConfigs: [[url: "${repoName}"]]])
+            ], submoduleCfg: [], userRemoteConfigs: [[url: "${repoName}, credentialsId: '37792118-83c9-474d-8549-e0b1ab07e442'"]]])
     }
     else
     {
