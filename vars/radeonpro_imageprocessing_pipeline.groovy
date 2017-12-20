f1d2af7355e47ca6f6f2de92be9bc2d797730663
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2017-12-20 09:32:40 +0300

    Update radeonpro_imageprocessing_pipeline.groovy

--- vars/radeonpro_imageprocessing_pipeline.groovy
+++ vars/radeonpro_imageprocessing_pipeline.groovy
@@ -1,1 +1,57 @@
 
+def call(String projectBranch) {
+  
+    pipeline {
+        agent none
+        options {
+            timestamps()
+            skipDefaultCheckout()
+        }
+        environment
+        {
+            JOB_NAME_FMT="${JOB_NAME}".replace('%2F', '_')
+            UPLOAD_PATH="builds/rpr-plugins/${JOB_NAME_FMT}/Build-${BUILD_ID}"
+        }
+        stages {
+            stage('Build') {
+                parallel {
+                    stage('Build On Windows') {
+                        agent {
+                            label "Windows && VS2015"
+                        }
+
+                        steps {
+                            ws("WS/${JOB_NAME_FMT}") {
+                                bat 'set'
+                                checkOutBranchOrScm(projectBranch, 'https://github.com/Radeon-Pro/RadeonProImageProcessing.git')
+                              
+                                bat '''
+                                HOSTNAME > Build_Windows_VS2015.log
+                                set msbuild="C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
+                                if not exist %msbuild% (
+                                    set msbuild="C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe"
+                                )
+                                set target=build
+                                set maxcpucount=/maxcpucount 
+                                set PATH=C:\\Python27\\;%PATH%
+
+                                .\\Tools\\premake\\win\\premake5 vs2015 >> Build_Windows_VS2015.log 2>&1
+                                set solution=.\\RadeonImageFilters.sln
+                                rem %msbuild% /target:%target% %maxcpucount% /property:Configuration=Debug;Platform=x64 %parameters% %solution%
+                                %msbuild% /target:%target% %maxcpucount% /property:Configuration=Release;Platform=x64 %parameters% %solution% >> Build_Windows_VS2015.log 2>&1
+                                '''
+                                stash includes: 'Bin/**/*', name: 'appWindows'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                echo 'sending notification result...'
+                sendBuildStatusNotification(currentBuild.result)
+            }
+        }
+    }
+}
