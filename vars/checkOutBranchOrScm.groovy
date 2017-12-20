53fef5efcbf535a10f5231e792dfa664c3196207
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2017-12-20 19:38:19 +0300

    Update checkOutBranchOrScm.groovy

--- vars/checkOutBranchOrScm.groovy
+++ vars/checkOutBranchOrScm.groovy
@@ -4,10 +4,13 @@ def call(String branchName, String repoName) {
     {
         echo "checkout from user branch: ${branchName}; repo: ${repoName}"
         checkout([$class: 'GitSCM', branches: [[name: "*/${branchName}"]], doGenerateSubmoduleConfigurations: false, extensions: [
+
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: []])
+
             [$class: 'CleanCheckout'],
             [$class: 'CheckoutOption', timeout: 30],
             [$class: 'CloneOption', timeout: 30, noTags: false]
-            ], submoduleCfg: [], userRemoteConfigs: [[url: "${repoName}"]]])
+            ], submoduleCfg: [], userRemoteConfigs: [[url: "${repoName}", credentialsId: '37792118-83c9-474d-8549-e0b1ab07e442']]])
     }
     else
     {
