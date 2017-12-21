73786ce7713f31a7ea8f548777ab34757e36288a
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2017-12-21 16:55:48 +0300

    Update checkOutBranchOrScm.groovy

--- vars/checkOutBranchOrScm.groovy
+++ vars/checkOutBranchOrScm.groovy
@@ -7,7 +7,7 @@ def call(String branchName, String repoName) {
             [$class: 'CleanCheckout'],
             [$class: 'CheckoutOption', timeout: 30],
             [$class: 'CloneOption', timeout: 30, noTags: false],
-            [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: true]
+            [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]
             ], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'radeonprorender', url: "${repoName}"]]])
     }
     else
