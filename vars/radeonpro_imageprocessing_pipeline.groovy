754a4c29bee2687fd15cbceddcfa504fc7ba1144
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2017-12-21 19:02:21 +0300

    Update radeonpro_imageprocessing_pipeline.groovy

--- vars/radeonpro_imageprocessing_pipeline.groovy
+++ vars/radeonpro_imageprocessing_pipeline.groovy
@@ -21,10 +21,12 @@ def executeTest(String asicName)
 
 def executeTests()
 {
+    /*
     def tasks = [:]
     
     tasks["AMD_RXVEGA"] = executeTest('AMD_RXVEGA')
     parallel tasks
+    */
 }
 
 def call(String projectBranch) {
