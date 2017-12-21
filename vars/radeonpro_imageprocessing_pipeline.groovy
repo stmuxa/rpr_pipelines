866c7d74c7c7d8e53c9ee709897dda47fb3aa6c0
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2017-12-21 19:17:02 +0300

    Update radeonpro_imageprocessing_pipeline.groovy

--- vars/radeonpro_imageprocessing_pipeline.groovy
+++ vars/radeonpro_imageprocessing_pipeline.groovy
@@ -50,33 +50,31 @@ def call(String projectBranch) {
                             label "Windows && VS2015"
                         }
                         steps {
-                            ws("WS/${JOB_NAME_FMT}") {
-                                steps{
-                                    bat 'set'
-                                    checkOutBranchOrScm(projectBranch, 'https://github.com/Radeon-Pro/RadeonProImageProcessing.git')
+                            //ws("WS/${JOB_NAME_FMT}") {
+                                bat 'set'
+                                checkOutBranchOrScm(projectBranch, 'https://github.com/Radeon-Pro/RadeonProImageProcessing.git')
 
-                                    bat '''
-                                    HOSTNAME > Build_Windows_VS2015.log
-                                    set msbuild="C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
-                                    if not exist %msbuild% (
-                                        set msbuild="C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe"
-                                    )
-                                    set target=build
-                                    set maxcpucount=/maxcpucount 
-                                    set PATH=C:\\Python27\\;%PATH%
+                                bat '''
+                                HOSTNAME > Build_Windows_VS2015.log
+                                set msbuild="C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
+                                if not exist %msbuild% (
+                                    set msbuild="C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe"
+                                )
+                                set target=build
+                                set maxcpucount=/maxcpucount 
+                                set PATH=C:\\Python27\\;%PATH%
 
-                                    .\\Tools\\premake\\win\\premake5 vs2015 >> Build_Windows_VS2015.log 2>&1
-                                    set solution=.\\RadeonImageFilters.sln
-                                    rem %msbuild% /target:%target% %maxcpucount% /property:Configuration=Debug;Platform=x64 %parameters% %solution%
-                                    %msbuild% /target:%target% %maxcpucount% /property:Configuration=Release;Platform=x64 %parameters% %solution% >> Build_Windows_VS2015.log 2>&1
-                                    '''
-                                    stash includes: 'Bin/**/*', name: 'appWindows'
-                                }
-                                post {
-                                    always {
-                                        archiveArtifacts 'Build_Windows_VS2015.log'
-                                    }
-                                }
+                                .\\Tools\\premake\\win\\premake5 vs2015 >> Build_Windows_VS2015.log 2>&1
+                                set solution=.\\RadeonImageFilters.sln
+                                rem %msbuild% /target:%target% %maxcpucount% /property:Configuration=Debug;Platform=x64 %parameters% %solution%
+                                %msbuild% /target:%target% %maxcpucount% /property:Configuration=Release;Platform=x64 %parameters% %solution% >> Build_Windows_VS2015.log 2>&1
+                                '''
+                                stash includes: 'Bin/**/*', name: 'appWindows'
+                            //}
+                        }
+                        post {
+                            always {
+                                archiveArtifacts 'Build_Windows_VS2015.log'
                             }
                         }
                     }
