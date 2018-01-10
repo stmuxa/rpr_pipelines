b46efe8f6dc0b9579eef4beccbf79f5cdcb0868a
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-01-10 10:46:38 +0300

    Create rpr_maya_plugin_script.groovy

---
+++ vars/rpr_maya_plugin_script.groovy
@@ -0,0 +1,3 @@
+def call(String pluginBranch = "", String thirdpartyBranch = "master", String packageBranch = "master", String testBranch = "master", bool skipNotification = false, bool ignoreBuild = false) {
+{
+}
