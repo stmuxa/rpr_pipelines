1f1033a9517774a2cbab37884fd3813f9428006d
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-01-10 11:43:56 +0300

    Update radeonpro_imageprocessing_script.groovy

--- vars/radeonpro_imageprocessing_script.groovy
+++ vars/radeonpro_imageprocessing_script.groovy
@@ -24,20 +24,62 @@ def executeTestWindows(String asicName, String projectBranch)
     }
     return retNode
 }
+def executeBuildWindowsVS2015(String projectBranch)
+{
+    def retNode = {
+        node("Windows && VS2015") {
+            String current_profile="${asicName}-Windows"
 
+            stage("Build-Windows") {
+                bat 'set'
+                checkOutBranchOrScm(projectBranch, 'https://github.com/Radeon-Pro/RadeonProImageProcessing.git')
+
+                try {
+                    bat '''
+                    HOSTNAME > Build_Windows_VS2015.log
+                    set msbuild="C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
+                    if not exist %msbuild% (
+                        set msbuild="C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe"
+                    )
+                    set target=build
+                    set maxcpucount=/maxcpucount 
+                    set PATH=C:\\Python27\\;%PATH%
+                    .\\Tools\\premake\\win\\premake5 vs2015 >> Build_Windows_VS2015.log 2>&1
+                    set solution=.\\RadeonImageFilters.sln
+                    rem %msbuild% /target:%target% %maxcpucount% /property:Configuration=Debug;Platform=x64 %parameters% %solution%
+                    %msbuild% /target:%target% %maxcpucount% /property:Configuration=Release;Platform=x64 %parameters% %solution% >> Build_Windows_VS2015.log 2>&1
+                    '''
+                    stash includes: 'Bin/**/*', name: 'appWindows'
+                }
+                finally {
+                    archiveArtifacts 'Build_Windows_VS2015.log'
+                }
+            }
+        }
+    }
+    return retNode
+}
 def executeTests(String projectBranch)
 {
     def tasks = [:]
-    
+    /*
     tasks["AMD_RXVEGA"] = executeTestWindows('AMD_RXVEGA', projectBranch)
     tasks["AMD_WX9100"] = executeTestWindows('AMD_WX9100', projectBranch)
     tasks["AMD_WX7100"] = executeTestWindows('AMD_WX7100', projectBranch)
     tasks["NVIDIA_GF1080TI"] = executeTestWindows('NVIDIA_GF1080TI', projectBranch)
     tasks["AMD_R9_200"] = executeTestWindows('AMD_R9_200', projectBranch)
     tasks["AMD_RX460"] = executeTestWindows('AMD_RX460', projectBranch)
+    */
     parallel tasks
 }
-
+def executeBuilds(String projectBranch)
+{
+    def tasks = [:]
+    /*
+    tasks["Windows"] = executeBuildWindowsVS2015(projectBranch)
+    */
+    parallel tasks
+}
 def call(String projectBranch='') {
   
     pipeline {
@@ -53,39 +95,8 @@ def call(String projectBranch='') {
         }
         stages {
             stage('Build') {
-                parallel {
-                    stage('Build On Windows') {
-                        agent {
-                            label "Windows && VS2015"
-                        }
-                        steps {
-                            //ws("WS/${JOB_NAME_FMT}") {
-                                bat 'set'
-                                checkOutBranchOrScm(projectBranch, 'https://github.com/Radeon-Pro/RadeonProImageProcessing.git')
-
-                                bat '''
-                                HOSTNAME > Build_Windows_VS2015.log
-                                set msbuild="C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
-                                if not exist %msbuild% (
-                                    set msbuild="C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe"
-                                )
-                                set target=build
-                                set maxcpucount=/maxcpucount 
-                                set PATH=C:\\Python27\\;%PATH%
-                                .\\Tools\\premake\\win\\premake5 vs2015 >> Build_Windows_VS2015.log 2>&1
-                                set solution=.\\RadeonImageFilters.sln
-                                rem %msbuild% /target:%target% %maxcpucount% /property:Configuration=Debug;Platform=x64 %parameters% %solution%
-                                %msbuild% /target:%target% %maxcpucount% /property:Configuration=Release;Platform=x64 %parameters% %solution% >> Build_Windows_VS2015.log 2>&1
-                                '''
-                                stash includes: 'Bin/**/*', name: 'appWindows'
-                            //}
-                        }
-                        post {
-                            always {
-                                archiveArtifacts 'Build_Windows_VS2015.log'
-                            }
-                        }
-                    }
+                steps {
+                    executeTests(projectBranch)
                 }
             }
             stage('Test') {
@@ -97,8 +108,12 @@ def call(String projectBranch='') {
       
         post {
             always {
-                echo 'sending notification result...'
-                sendBuildStatusNotification(currentBuild.result)
+                when { environment name: 'enableNotification', value: 'true' }
+                steps
+                {
+                    echo 'sending notification result...'
+                    sendBuildStatusNotification(currentBuild.result)
+                }
             }
         }
     }
