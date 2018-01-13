e5df3dc742528ac0d2522fa269fcd8d16587b2ef
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-01-14 02:46:21 +0300

    Update outputEnvironmentInfo.groovy

--- vars/outputEnvironmentInfo.groovy
+++ vars/outputEnvironmentInfo.groovy
@@ -3,10 +3,12 @@ def call(String osName)
 {
     if(osName == 'Windows')
     {
-         bat "set > ${STAGE_NAME}.log"
+         bat "HOSTNAME  >  ${STAGE_NAME}.log"
+         bat "set       >> ${STAGE_NAME}.log"
     }
     else
     {
-         sh "env > ${STAGE_NAME}.log"
+         sh "uname -a   >  ${STAGE_NAME}.log"
+         sh "env        >> ${STAGE_NAME}.log"
     }
 }
