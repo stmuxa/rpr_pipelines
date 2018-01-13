bc6b85c5dad3cd06f0b2b62ee47e017fdbed2238
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-01-14 02:51:43 +0300

    Update sendFiles.groovy

--- vars/sendFiles.groovy
+++ vars/sendFiles.groovy
@@ -1,1 +1,16 @@
 
+def call(String osName, String local, String remote)
+{
+    if(osName == 'Windows')
+    {
+        bat """
+            %CIS_TOOLS%\\sendFiles.bat ${local} ${remote}
+        """
+    }
+    else
+    {
+        sh """
+            ${CIS_TOOLS}/sendFiles.sh \"${local}\" ${remote}
+        """
+    }
+}
