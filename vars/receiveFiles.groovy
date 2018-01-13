96f0c4d53b327f3bccf4bdd029a0ce649cef20a6
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-01-14 02:52:02 +0300

    Update receiveFiles.groovy

--- vars/receiveFiles.groovy
+++ vars/receiveFiles.groovy
@@ -1,1 +1,16 @@
 
+def call(String osName, String remote, String local)
+{
+    if(osName == 'Windows')
+    {
+        bat """
+            %CIS_TOOLS%\\receiveFiles.bat ${remote} ${local}
+        """
+    }
+    else
+    {
+        sh """
+            ${CIS_TOOLS}/receiveFiles.sh \"${remote}\" ${local}
+        """
+    }
+}
