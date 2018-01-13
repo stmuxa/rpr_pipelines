b1c27e815e8982130d04060b1ff9ac3160d93638
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-01-14 01:06:36 +0300

    Update radeonpro_imageprocessing_script.groovy

--- vars/radeonpro_imageprocessing_script.groovy
+++ vars/radeonpro_imageprocessing_script.groovy
@@ -1,218 +1,248 @@
-def executeTestWindows(String asicName, String projectBranch)
+                        
+
+
+def executeTestCommand(String osName)
 {
-    def retNode = {
-        node("Windows && Tester && OpenCL && gpu${asicName}") {
-            String current_profile="${asicName}-Windows"
-
-            stage("Test-${current_profile}") {
-                bat 'set'
-
-                try {
-                    checkOutBranchOrScm(projectBranch, 'https://github.com/Radeon-Pro/RadeonProImageProcessing.git')
-                    unstash 'appWindows'
-
-                    dir('UnitTest')
-                    {
-                        bat "mkdir testSave"
-                        bat "..\\Bin\\Release\\x64\\UnitTest64.exe >> ..\\Test${current_profile}.log  2>&1"
-                    }
-                }
-                finally {
-                    archiveArtifacts "Test${current_profile}.log"
-                }
-            }
-        }
-    }
-    return retNode
-}
-def executeTestOSX(String asicName, String projectBranch, String osName = "OSX")
-{
-    def retNode = {
-        node("${osName} && Tester && OpenCL && gpu${asicName}") {
-            String current_profile="${asicName}-${osName}"
-
-            stage("Test-${current_profile}") {
-                sh 'env'
-
-                try {
-                    checkOutBranchOrScm(projectBranch, 'https://github.com/Radeon-Pro/RadeonProImageProcessing.git')
-                    unstash "app${osName}"
-                    
-                    dir('UnitTest')
-                    {
-                        sh "mkdir testSave"
-                        sh "../Bin/Release/x64/UnitTest64 >> ../Test${current_profile}.log  2>&1"
-                    }
-                }
-                finally {
-                    archiveArtifacts "Test${current_profile}.log"
-                }
-            }
-        }
-    }
-    return retNode
-}
-
-def executeTestLinux(String asicName, String projectBranch, String osName)
-{
-    def retNode = {
-        node("${osName} && Tester && OpenCL && gpu${asicName}") {
-            String current_profile="${asicName}-${osName}"
-
-            stage("Test-${current_profile}") {
-                sh 'env'
-
-                try {
-                    checkOutBranchOrScm(projectBranch, 'https://github.com/Radeon-Pro/RadeonProImageProcessing.git')
-                    unstash "app${osName}"
-                    
-                    dir('UnitTest')
-                    {
-                        sh "mkdir testSave"
-                        sh "../Bin/Release/x64/UnitTest64 >> ../Test${current_profile}.log  2>&1"
-                    }
-                }
-                finally {
-                    archiveArtifacts "Test${current_profile}.log"
-                }
-            }
-        }
-    }
-    return retNode
-}
-
-def executeBuildWindowsVS2015(String projectBranch)
-{
-    def retNode = {
-        node("Windows && VS2015") {
-
-            stage("Build-Windows") {
-                bat 'set'
-
-                try {
-                    checkOutBranchOrScm(projectBranch, 'https://github.com/Radeon-Pro/RadeonProImageProcessing.git')
-
-                    bat '''
-                    HOSTNAME > Build_Windows_VS2015.log
-                    set msbuild="C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
-                    if not exist %msbuild% (
-                        set msbuild="C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe"
-                    )
-                    set target=build
-                    set maxcpucount=/maxcpucount 
-                    set PATH=C:\\Python27\\;%PATH%
-                    .\\Tools\\premake\\win\\premake5 vs2015 >> Build_Windows_VS2015.log 2>&1
-                    set solution=.\\RadeonImageFilters.sln
-                    rem %msbuild% /target:%target% %maxcpucount% /property:Configuration=Debug;Platform=x64 %parameters% %solution%
-                    %msbuild% /target:%target% %maxcpucount% /property:Configuration=Release;Platform=x64 %parameters% %solution% >> Build_Windows_VS2015.log 2>&1
-                    '''
-                    stash includes: 'Bin/**/*', name: 'appWindows'
-                }
-                finally {
-                    echo "${currentBuild.getCurrentResult()}"
-                    archiveArtifacts 'Build_Windows_VS2015.log'
-                }
-            }
-        }
-    }
-    return retNode
-}
-def executeBuildLinux(String projectBranch, String linuxName)
-{
-    def retNode = {
-        node("${linuxName}") {
-
-            stage("Build-${linuxName}") {
-                sh 'env'
-
-                try {
-                    checkOutBranchOrScm(projectBranch, 'https://github.com/Radeon-Pro/RadeonProImageProcessing.git')
-                    
-                    sh """
-                    uname -a > Build_${linuxName}.log
-                    chmod +x Tools/premake/linux64/premake5
-                    Tools/premake/linux64/premake5 --use_opencl --embed_kernels gmake   >> Build_${linuxName}.log 2>&1
-                    make config=release_x64                                             >> Build_${linuxName}.log 2>&1
-                    """
-                    stash includes: 'Bin/**/*', name: "app${linuxName}"
-                }
-                finally {
-                    archiveArtifacts "Build_${linuxName}.log"
-                }
-            }
-        }
-    }
-    return retNode
-}
-
-def executeBuildOSX(String projectBranch, String osName = "OSX")
-{
-    def retNode = {
-        node("Builder && ${osName}") {
-
-            stage("Build-${osName}") {
-                sh 'env'
-
-                try {
-                    checkOutBranchOrScm(projectBranch, 'https://github.com/Radeon-Pro/RadeonProImageProcessing.git')
-                    
-                    sh """
-                        uname -a > Build_${osName}.log
-                        Tools/premake/osx/premake5 --use_opencl --embed_kernels gmake >> Build_${osName}.log 2>&1
-                        make config=release_x64          >> Build_${osName}.log 2>&1
-                    """
-                    stash includes: 'Bin/**/*', name: "app${osName}"
-                }
-                finally {
-                    archiveArtifacts "Build_${osName}.log"
-                }
-            }
-        }
-    }
-    return retNode
-}
-
-def executeTests(String projectBranch, String testPlatforms)
-{
-    def tasks = [:]
-    
-    testPlatforms.split(';').each()
+    switch(osName)
     {
-        def (osName, gpuName) = "${it}".tokenize(':')
+    case 'Windows':
+        bat "mkdir testSave"
+        bat "..\\Bin\\Release\\x64\\UnitTest64.exe >> ..\\Test${current_profile}.log  2>&1"
+        break;
+    case 'OSX':
+        sh "mkdir testSave"
+        sh "../Bin/Release/x64/UnitTest64 >> ../Test${current_profile}.log  2>&1"
+        break;
+    default:
+        sh "mkdir testSave"
+        sh "../Bin/Release/x64/UnitTest64 >> ../Test${current_profile}.log  2>&1"
+    }
+}
+
+def sendFiles(String osName, String local, String remote)
+{
+    if(osName == 'Windows')
+    {
+        bat """
+            %CIS_TOOLS%\\sendFiles.bat ${local} ${remote}
+        """
+    }
+    else
+    {
+        sh """
+            ${CIS_TOOLS}/sendFiles.sh \"${local}\" ${remote}
+        """
+    }
+}
+
+def receiveFiles(String osName, String remote, String local)
+{
+    if(osName == 'Windows')
+    {
+        bat """
+            %CIS_TOOLS%\\receiveFiles.bat ${remote} ${local}
+        """
+    }
+    else
+    {
+        sh """
+            ${CIS_TOOLS}/receiveFiles.sh \"${remote}\" ${local}
+        """
+    }
+}
+
+def printEnv(String osName)
+{
+    if(osName == 'Windows')
+    {
+         bat "set > ${STAGE_NAME}.log"
+    }
+    else
+    {
+         sh "env > ${STAGE_NAME}.log"
+    }
+}
+
+def executeTests(String asicName, String projectBranch, Boolean updateRefs, String osName)
+{
+    //String PRJ_PATH="builds/rpr-core/RadeonProImageProcessor"
+    //String REF_PATH="${PRJ_PATH}/ReferenceImages/${asicName}-${osName}"
+    //String JOB_PATH="${PRJ_PATH}/${JOB_NAME}/Build-${BUILD_ID}/${asicName}-${osName}".replace('%2F', '_')
+
+    try {
+        checkOutBranchOrScm(projectBranch, 'https://github.com/Radeon-Pro/RadeonProImageProcessing.git')
+
+        printEnv(osName)
+        unstash "app${osName}"
+
+        dir('UnitTest')
+        {
+            executeTestCommand(osName)
+        }               
+    }
+    catch (e) {
+        println(e.toString());
+        println(e.getMessage());
+        println(e.getStackTrace());    
+        
+        //send if needed
+        //sendFiles(osName, './OutputImages/*.*', PRJ_PATH)
+
+        currentBuild.result = "FAILED"
+        throw e
+    }
+    finally {
+        archiveArtifacts "*.log"
+        junit "*.gtest.xml"
+    }
+}
+
+def executeBuildWindows()
+{
+    bat '''
+    HOSTNAME > Build_Windows_VS2015.log
+    set msbuild="C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
+    if not exist %msbuild% (
+        set msbuild="C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe"
+    )
+    set target=build
+    set maxcpucount=/maxcpucount 
+    set PATH=C:\\Python27\\;%PATH%
+    .\\Tools\\premake\\win\\premake5 vs2015 >> Build_Windows_VS2015.log 2>&1
+    set solution=.\\RadeonImageFilters.sln
+    rem %msbuild% /target:%target% %maxcpucount% /property:Configuration=Debug;Platform=x64 %parameters% %solution%
+    %msbuild% /target:%target% %maxcpucount% /property:Configuration=Release;Platform=x64 %parameters% %solution% >> Build_Windows_VS2015.log 2>&1
+    '''
+}
+
+def executeBuildOSX()
+{
+    sh """
+        uname -a > Build_${osName}.log
+        Tools/premake/osx/premake5 --use_opencl --embed_kernels gmake >> Build_${osName}.log 2>&1
+        make config=release_x64          >> Build_${osName}.log 2>&1
+    """
+}
+
+def executeBuildLinux()
+{
+    sh """
+    uname -a > Build_${linuxName}.log
+    chmod +x Tools/premake/linux64/premake5
+    Tools/premake/linux64/premake5 --use_opencl --embed_kernels gmake   >> Build_${linuxName}.log 2>&1
+    make config=release_x64                                             >> Build_${linuxName}.log 2>&1
+    """
+
+}
+def executeBuild(String projectBranch, String osName)
+{
+    try {
+        checkOutBranchOrScm(projectBranch, 'https://github.com/GPUOpen-LibrariesAndSDKs/RadeonProRender-Baikal.git')
+        printEnv(osName)
+
+        switch(osName)
+        {
+        case 'Windows': 
+            executeBuildWindows(); 
+            break;
+        case 'OSX':
+            executeBuildOSX();
+            break;
+        default: 
+            executeBuildLinux();
+        }
         if(osName == 'Windows')
         {
-            tasks["${it}"] = executeTestWindows("${gpuName}", projectBranch)
-        }
-        else
+            executeBuildWindows()
+        }else
         if(osName == 'OSX')
         {
-            tasks["${it}"] = executeTestOSX("${gpuName}", projectBranch)
-        }
-        else
+            executeBuildOSX()
+        }else
         {
-            tasks["${it}"] = executeTestLinux("${gpuName}", projectBranch, osName)
+            executeBuildLinux()
+        }
+        stash includes: 'Bin/**/*', name: "app${osName}"
+    }
+    catch (e) {
+        currentBuild.result = "FAILED"
+        throw e
+    }
+    finally {
+        archiveArtifacts "${STAGE_NAME}.log"
+    }                        
+
+}
+def executePlatform(String osName, String gpuNames, Boolean updateRefs, String projectBranch)
+{
+    def retNode =  
+    {
+        try {
+            node("${osName} && Builder")
+            {
+                stage("Build-${osName}")
+                {
+                    String JOB_NAME_FMT="${JOB_NAME}".replace('%2F', '_')
+                    ws("WS/${JOB_NAME_FMT}") {
+                        executeBuild(projectBranch, osName)
+                    }
+                }
+            }
+
+            if(gpuNames)
+            {
+                def testTasks = [:]
+                gpuNames.split(',').each()
+                {
+                    String asicName = it
+                    echo "Scheduling Test ${osName}:${asicName}"
+                    testTasks["Test-${it}-${osName}"] = {
+                        node("${osName} && Tester && OpenCL && gpu${asicName}")
+                        {
+                            stage("Test-${asicName}-${osName}")
+                            {
+                                executeTests(asicName, projectBranch, updateRefs, osName)
+                            }
+                        }
+                    }
+                }
+                parallel testTasks
+            }
+            else
+            {
+                echo "No tests found for ${osName}"
+            }
+        }
+        catch (e) {
+            println(e.toString());
+            println(e.getMessage());
+            println(e.getStackTrace());        
+            currentBuild.result = "FAILED"
+            throw e
         }
     }
-    
-    parallel tasks
+    return retNode
 }
-def executeBuilds(String projectBranch)
-{
-    def tasks = [:]
 
-    tasks["Build-Windows"] = executeBuildWindowsVS2015(projectBranch)
-    tasks["Build-Ubuntu"] = executeBuildLinux(projectBranch, "Ubuntu")
-    tasks["Build-OSX"] = executeBuildOSX(projectBranch)
-
-    parallel tasks
-}
-def call(String projectBranch='', String testPlatforms = 'Windows:AMD_RXVEGA;Windows:AMD_WX9100;Windows:AMD_WX7100;Ubuntu:AMD_RX460', Boolean enableNotifications = true) {
+def call(String projectBranch = "", 
+         String platforms = 'Windows:AMD_RXVEGA,AMD_WX9100,AMD_WX7100;Ubuntu;OSX:Intel_Iris', 
+         Boolean updateRefs = false, Boolean enableNotifications = true) {
       
     try {
         timestamps {
-            executeBuilds(projectBranch)
-            executeTests(projectBranch, testPlatforms)
+            def tasks = [:]
+            
+            platforms.split(';').each()
+            {
+                def (osName, gpuNames) = it.tokenize(':')
+                                
+                tasks[osName]=executePlatform(osName, gpuNames, updateRefs, projectBranch)
+            }
+            parallel tasks
         }
+    }
+    catch (e) {
+        currentBuild.result = "FAILED"
     }
     finally {
         if(enableNotifications)
@@ -221,3 +251,6 @@ def call(String projectBranch='', String testPlatforms = 'Windows:AMD_RXVEGA;Win
         }
     }
 }
+
+
+
