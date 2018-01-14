19f6bedb11ec5b04c0671841a22a015d0d37c6c4
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-01-14 14:52:38 +0300

    Update radeonpro_imageprocessing_script_exp.groovy

--- vars/radeonpro_imageprocessing_script_exp.groovy
+++ vars/radeonpro_imageprocessing_script_exp.groovy
@@ -122,7 +122,7 @@ def call(String projectBranch = "",
          String platforms = 'Windows:AMD_RXVEGA,AMD_WX9100,AMD_WX7100;Ubuntu;OSX:Intel_Iris', 
          Boolean updateRefs = false, Boolean enableNotifications = true) {
     
-    multiplatform_pipeline(platforms, this.&executeBuild, this.&executeTests, {}, 
+    multiplatform_pipeline(platforms, this.&executeBuild, this.&executeTests, this.&executeDeploy, 
                            [projectBranch:projectBranch, 
                            enableNotifications:enableNotifications])
 }
