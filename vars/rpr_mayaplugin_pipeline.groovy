cc8cd525b00dc91ad9a43554916410dd0c7fdb24
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-01-15 00:33:02 +0300

    Update rpr_mayaplugin_pipeline.groovy

--- vars/rpr_mayaplugin_pipeline.groovy
+++ vars/rpr_mayaplugin_pipeline.groovy
@@ -269,60 +269,9 @@ def call(String pluginBranch = "", String thirdpartyBranch = "master", String pa
                                     )
                                 '''
                             }
-
                         }
                     }                
-                    stage('Test-Windows-AMD_RX460') {
-                        agent {
-                            label "Windows && Tester && OpenCL && gpuAMD_RX460"
-                        }
-                        environment { 
-                            current_host="${env.COMPUTERNAME}"
-                            current_profile="AMD_RX460-Windows"
-                        }
-                        steps {
-
-                            dir('jobs_test_maya')
-                            {
-                                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [
-                                    [$class: 'CleanCheckout'],
-                                    [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]
-                                    ], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/luxteam/jobs_test_maya.git']]])
-
-                            }
-                            dir('jobs_test_maya/temp/install_plugin')
-                            {
-                                unstash 'appWindows'
-
-                                bat '''
-                                msiexec /i "RadeonProRenderForMaya.msi" /quiet /qn PIDKEY=GPUOpen2016 /log install_maya_plugin_%current_profile%.log /norestart
-                                '''
-                            }
-
-                            dir('jobs_test_maya/scripts')
-                            {
-                                bat'''
-                                auto_config.bat
-                                '''
-                                bat'''
-                                run.bat
-                                '''
-                            }
-                            dir("jobs_test_maya/Results/Maya")
-                            {
-                                bat'''
-                                copy session_report_embed_img.html session_report_%current_profile%.html
-                                '''
-                                archiveArtifacts "session_report_${env.current_profile}.html"
-
-                                bat '''
-                                IF EXIST "%CIS_TOOLS%\\sendFiles.bat" (
-                                    %CIS_TOOLS%\\sendFiles.bat session_report_%current_profile%.html %UPLOAD_PATH%
-                                    )
-                                '''
-                            }
-                        }
-                    }                
+                    
                     stage('Test-Windows-AMD_RX9200') {
                         agent {
                             label "Windows && Tester && OpenCL && gpuAMD_RX9200"
