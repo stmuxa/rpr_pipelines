621beff84ce356a1c3dfad86c69d7ff8cf969e1d
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-01-15 11:49:17 +0300

    Update multiplatform_example_script.groovy

--- vars/multiplatform_example_script.groovy
+++ vars/multiplatform_example_script.groovy
@@ -4,18 +4,17 @@ def executeGenTestRefCommand(String osName)
     {
     case 'Windows':
         bat """
-        ..\\Bin\\Release\\x64\\BaikalTest64.exe -genref 1 --gtest_output=xml:../${STAGE_NAME}.gtest.xml >> ..\\${STAGE_NAME}.log 2>&1
+        echo 'sample image' > .\\ReferenceImages\\sample_image.txt
         """
         break;
     case 'OSX':
         sh """
-            ../Bin/Release/x64/BaikalTest64 -genref 1 --gtest_output=xml:../${STAGE_NAME}.gtest.xml >> ../${STAGE_NAME}.log 2>&1
+        echo 'sample image' > ./ReferenceImages/sample_image.txt
         """
         break;
     default:
         sh """
-            export LD_LIBRARY_PATH=`pwd`/../Bin/Release/x64/:\${LD_LIBRARY_PATH}
-            ../Bin/Release/x64/BaikalTest64 -genref 1 --gtest_output=xml:../${STAGE_NAME}.gtest.xml >> ../${STAGE_NAME}.log 2>&1
+        echo 'sample image' > ./ReferenceImages/sample_image.txt
         """
     }
 }
@@ -26,34 +25,33 @@ def executeTestCommand(String osName)
     {
     case 'Windows':
         bat """
-            ..\\Bin\\Release\\x64\\BaikalTest64.exe --gtest_output=xml:../${STAGE_NAME}.gtest.xml >> ..\\${STAGE_NAME}.log 2>&1
+        echo 'sample image' > .\\OutputImages\\sample_image.txt
         """
         break;
     case 'OSX':
         sh """
-            ../Bin/Release/x64/BaikalTest64 --gtest_output=xml:../${STAGE_NAME}.gtest.xml >> ../${STAGE_NAME}.log 2>&1
+        echo 'sample image' > ./OutputImages/sample_image.txt
         """
         break;
     default:
         sh """
-            export LD_LIBRARY_PATH=`pwd`/../Bin/Release/x64/:\${LD_LIBRARY_PATH}
-            ../Bin/Release/x64/BaikalTest64 --gtest_output=xml:../${STAGE_NAME}.gtest.xml >> ../${STAGE_NAME}.log 2>&1
+        echo 'sample image' > ./OutputImages/sample_image.txt
         """
     }
 }
 
 def executeTests(String osName, String asicName, Map options)
 {
-    String PRJ_PATH="builds/rpr-core/RadeonProRender-Baikal"
+    String PRJ_PATH="builds/sample-projects/MultiplatformSampleProject"
     String REF_PATH="${PRJ_PATH}/ReferenceImages/${asicName}-${osName}"
     String JOB_PATH="${PRJ_PATH}/${JOB_NAME}/Build-${BUILD_ID}/${asicName}-${osName}".replace('%2F', '_')
 
     try {
-        checkOutBranchOrScm(options['projectBranch'], 'https://github.com/luxteam/MultiplatformSampleProject.git')
+        //checkOutBranchOrScm(options['projectBranch'], 'https://github.com/luxteam/MultiplatformSampleProject.git')
 
         outputEnvironmentInfo(osName)
         
-        unstash "app${osName}"
+        //unstash "app${osName}"
 
         dir('Tests')
         {
@@ -88,7 +86,7 @@ def executeTests(String osName, String asicName, Map options)
                 receiveFiles(osName, "${REF_PATH}/*", './ReferenceImages/')
                 executeTestCommand(osName)
             }
-        }        
+        }
         currentBuild.result = "FAILED"
         throw e
     }
@@ -124,7 +122,7 @@ def executeBuildLinux()
 def executeBuild(String osName, Map options)
 {
     try {
-        checkOutBranchOrScm(options['projectBranch'], 'https://github.com/GPUOpen-LibrariesAndSDKs/RadeonProRender-Baikal.git')
+        checkOutBranchOrScm(options['projectBranch'], 'https://github.com/luxteam/MultiplatformSampleProject.git')
         outputEnvironmentInfo(osName)
 
         switch(osName)
@@ -139,7 +137,7 @@ def executeBuild(String osName, Map options)
             executeBuildLinux();
         }
         
-        stash includes: 'Bin/**/*', name: "app${osName}"
+        //stash includes: 'Bin/**/*', name: "app${osName}"
     }
     catch (e) {
         currentBuild.result = "FAILED"
