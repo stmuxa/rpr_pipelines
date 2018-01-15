c075e6748e2d712a9d5d68e2db8020caf24a8c07
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-01-16 02:57:06 +0300

    Create pipeline_tools.groovy

---
+++ vars/pipeline_tools.groovy
@@ -0,0 +1,18 @@
+
+String property1 = 'pipeline_tools property value'
+static String static_property1 = 'pipeline_tools static_property1 value'
+
+def call()
+{
+    echo 'pipeline_tools Constructor'
+}
+
+def method1()
+{
+    echo 'pipeline_tools method1 read property1: ${property1}'
+}
+
+static def static_method1()
+{
+    echo 'pipeline_tools static_method1 read static_property1: ${static_property1}'
+}
