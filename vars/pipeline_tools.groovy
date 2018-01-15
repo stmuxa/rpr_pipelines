983a98a8a88a790315aa2748236b48603a0caeb2
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-01-16 02:59:15 +0300

    Update pipeline_tools.groovy

--- vars/pipeline_tools.groovy
+++ vars/pipeline_tools.groovy
@@ -2,17 +2,17 @@
 String property1 = 'pipeline_tools property value'
 static String static_property1 = 'pipeline_tools static_property1 value'
 
-def call()
+def call(String param)
 {
-    echo 'pipeline_tools Constructor'
+    echo "pipeline_tools Constructor param = ${param}"
 }
 
 def method1()
 {
-    echo 'pipeline_tools method1 read property1: ${property1}'
+    echo "pipeline_tools method1 read property1: ${property1}"
 }
 
 static def static_method1()
 {
-    echo 'pipeline_tools static_method1 read static_property1: ${static_property1}'
+    echo "pipeline_tools static_method1 read static_property1: ${static_property1}"
 }
