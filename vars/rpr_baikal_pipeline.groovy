852d97ce2e03c9fe8208dff7cc241a72bd336c74
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-01-17 02:13:20 +0300

    Delete rpr_baikal_pipeline.groovy

--- vars/rpr_baikal_pipeline.groovy
+++
@@ -1,6 +0,0 @@
-def call(String projectBranch = '', 
-         String platforms = 'Windows:AMD_RXVEGA,AMD_WX9100,AMD_WX7100;Ubuntu;OSX:Intel_Iris', 
-         Boolean updateRefs = false, Boolean enableNotifications = true) {
-    
-    rpr_baikal_script(projectBranch, platforms, updateRefs, enableNotifications)
-}
