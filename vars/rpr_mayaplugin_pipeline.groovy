5e0655a99e17e07a29d1224deeae47f3ef6545a5
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-01-22 15:39:36 +0300

    Update rpr_mayaplugin_pipeline.groovy

--- vars/rpr_mayaplugin_pipeline.groovy
+++ vars/rpr_mayaplugin_pipeline.groovy
@@ -8,7 +8,7 @@ def executeGenTestRefCommand(String osName, Map options)
         bat """
         set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 
-        python ..\jobs_launcher\common\scripts\generate_baseline.py --results_root ..\Results\Maya  --baseline_root ..\Baseline
+        python jobs_launcher\\common\\scripts\\generate_baseline.py --results_root Results\\Maya --baseline_root Baseline
         """
         break;
     case 'OSX':
