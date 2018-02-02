d2184728d0d9aae4629f73c7bab6253232e6d030
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-02-02 12:47:46 +0300

    Update appendHtmlLinkToFile.groovy

--- vars/appendHtmlLinkToFile.groovy
+++ vars/appendHtmlLinkToFile.groovy
@@ -1,7 +1,7 @@
 
-def call(String fileName, String link)
+def call(String fileName, String linkText, String link)
 {
     f = new File(fileName)
-    String str = "<a href=\"${link}\">${link}</a><br>"    
+    String str = "<a href=\"${link}\">${linkText}</a><br>"    
     f.append(str)
 }
