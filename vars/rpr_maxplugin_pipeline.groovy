37aecfb1e2e7468c03dd6eec2ea2f4a6932698a5
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-02-02 10:49:39 +0300

    Update rpr_maxplugin_pipeline.groovy

--- vars/rpr_maxplugin_pipeline.groovy
+++ vars/rpr_maxplugin_pipeline.groovy
@@ -146,7 +146,7 @@ def executeBuildWindows(Map options)
         """
 
         //remove when installer will be redesigned same way as maya
-        sendFiles(osName, 'RadeonProRender3dsMax.msi', "${options.JOB_PATH}")
+        sendFiles(osName, 'RadeonProRender3dsMax*.msi', "${options.JOB_PATH}")
         //uncomment to use when installer will be redesigned same way as maya
         //sendFiles(osName, 'output/_ProductionBuild/RadeonProRender*.msi', options[JOB_PATH])
 
