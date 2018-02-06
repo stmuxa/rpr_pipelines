ce21699c9bd2e9e2d0543940f54ad537d9f7e064
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-02-06 20:38:09 +0300

    Update version_read.groovy

--- vars/version_read.groovy
+++ vars/version_read.groovy
@@ -1,5 +1,40 @@
-def call(String path)
+def call(String path, String prefix)
 {
-    String ver
+    String ver = python3 (
+"""
+	file = ${path}
+	prefix = ${prefix}
+
+	old_version = []
+
+	with open(file, encoding="utf8") as f:
+		for line in f:
+			if line.find(prefix) != -1:
+				prefix_line = line
+
+	try:
+
+		old_version = re.findall(r'\d+.\d+.\d+.\d+', prefix_line)
+
+		if len(old_version) == 0:
+			old_version = re.findall(r'\d+.\d+.\d+', prefix_line)
+			if len(old_version) == 0:
+				old_version = re.findall(r'\d+.\d+', prefix_line)
+				if len(old_version) == 0: 
+					old_version = re.findall(r'\d+', prefix_line)
+
+		if len(old_version) == 0:
+			print('Unsupported version. No numbers in prefix line.')
+		else:	
+			print(old_version[0])
+				
+	except UnboundLocalError:
+		print('Error. No search string.')
+		exit(0)
+        
+"""
+).split('\r\n')[4].trim()
+    
+    
     return ver
 }
