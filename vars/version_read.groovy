70e53bb4c1a70008fdd7af362877e0e6c47d93a1
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-02-06 23:53:24 +0300

    Update version_read.groovy

--- vars/version_read.groovy
+++ vars/version_read.groovy
@@ -1,7 +1,7 @@
 def call(String path, String ver_prefix)
 {
     String currentversion=python3(
-        "${CIS_TOOLS}/version_read.py --file version.h --prefix \"#define VERSION_STR\""
+        "${CIS_TOOLS}/version_read.py --file \"${path}\" --prefix \"${ver_prefix}\""
     ).split('\r\n')[2].trim()
     return currentversion;
 }
