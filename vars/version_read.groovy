bde9fccdaa1699c4a3d6535cc85015a31e2a8c59
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-02-06 23:51:11 +0300

    Update version_read.groovy

--- vars/version_read.groovy
+++ vars/version_read.groovy
@@ -1,40 +1,7 @@
 def call(String path, String ver_prefix)
 {
-    String version_read_script = 
-"""
-	file = ${path}
-	prefix = ${ver_prefix}
-
-	old_version = []
-
-	with open(file, encoding=\"utf8\") as f:
-		for line in f:
-			if line.find(prefix) != -1:
-				prefix_line = line
-
-	try:
-
-		old_version = re.findall(r'\\d+.\\d+.\\d+.\\d+', prefix_line)
-
-		if len(old_version) == 0:
-			old_version = re.findall(r'\\d+.\\d+.\\d+', prefix_line)
-			if len(old_version) == 0:
-				old_version = re.findall(r'\\d+.\\d+', prefix_line)
-				if len(old_version) == 0: 
-					old_version = re.findall(r'\\d+', prefix_line)
-
-		if len(old_version) == 0:
-			print('Unsupported version. No numbers in prefix line.')
-		else:	
-			print(old_version[0])
-				
-	except UnboundLocalError:
-		print('Error. No search string.')
-		exit(0)
-        
-"""
-	ver = python3(version_read_script).split('\r\n')[4].trim()
-    
-    
-    return ver
+    String currentversion=python3(
+        "${CIS_TOOLS}/version_read.py --file version.h --prefix \"#define VERSION_STR\""
+    ).split('\r\n')[2].trim()
+    return currentversion;
 }
