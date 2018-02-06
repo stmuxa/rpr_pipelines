13e79e965d7ae6431f2aff4f3babc44a773c3ef9
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-02-07 00:01:23 +0300

    Update version_write.groovy

--- vars/version_write.groovy
+++ vars/version_write.groovy
@@ -1,4 +1,6 @@
-def call(String path, String prefix, String newVer)
+def call(String path, String prefix, String new_version)
 {
-
+    python3(
+      "${CIS_TOOLS}/version_write.py --file \"${path}\" --prefix \"${prefix}\" --version \"${new_version}\""
+        )
 }
