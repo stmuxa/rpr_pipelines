1015ee669961a32c4508385f27a3b73aff833707
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-02-07 23:00:50 +0300

    Update version_inc.groovy

--- vars/version_inc.groovy
+++ vars/version_inc.groovy
@@ -1,7 +1,7 @@
-def call(String version, Integer index = 3)
+def call(String version, Integer index = 3, String delimeter = '.')
 {
     String new_version =python3(
-        "${CIS_TOOLS}/version_inc.py --version \"${version}\" --index ${index}"
+        "${CIS_TOOLS}/version_inc.py --version \"${version}\" --index ${index} --delimeter ${delimeter}"
                 ).split('\r\n')[2].trim()
     return new_version
 }
