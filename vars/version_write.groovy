3cf053e1e1d2dcfec5cf790fce238661be77005c
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-02-07 23:01:45 +0300

    Update version_write.groovy

--- vars/version_write.groovy
+++ vars/version_write.groovy
@@ -1,6 +1,6 @@
-def call(String path, String prefix, String new_version)
+def call(String path, String prefix, String new_version, String delimeter)
 {
     python3(
-      "${CIS_TOOLS}/version_write.py --file \"${path}\" --prefix \"${prefix}\" --version \"${new_version}\""
+      "${CIS_TOOLS}/version_write.py --file \"${path}\" --prefix \"${prefix}\" --version \"${new_version}\" --delimeter ${delimeter}"
         )
 }
