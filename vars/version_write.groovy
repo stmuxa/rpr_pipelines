2fd94cedbd49095c80a63fd8ffcea69fa7019ab2
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-02-08 22:41:21 +0300

    Update version_write.groovy

--- vars/version_write.groovy
+++ vars/version_write.groovy
@@ -1,6 +1,8 @@
 def call(String path, String prefix, String new_version, String delimeter = '.')
 {
+    String new_prefix = ver_prefix.replace('"', '""')
+
     python3(
-      "${CIS_TOOLS}/version_write.py --file \"${path}\" --prefix \"${prefix}\" --version \"${new_version}\" --delimeter ${delimeter}"
+      "${CIS_TOOLS}/version_write.py --file \"${path}\" --prefix \"${new_prefix}\" --version \"${new_version}\" --delimeter \"${delimeter}\""
         )
 }
