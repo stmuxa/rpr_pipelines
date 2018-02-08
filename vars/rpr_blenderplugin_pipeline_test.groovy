e67ae1a8c1793793b9719ad068b44096e109969e
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-02-08 14:26:10 +0300

    Update rpr_blenderplugin_pipeline_test.groovy

--- vars/rpr_blenderplugin_pipeline_test.groovy
+++ vars/rpr_blenderplugin_pipeline_test.groovy
@@ -66,10 +66,11 @@ def executeTestCommand(String osName, Map options)
     default:
         dir("scripts")
         {
+            echo "./run.sh ${options.runParameters} >> ../${STAGE_NAME}.log 2>&1"
+            
             sh """
             ./run.sh ${options.runParameters} >> ../${STAGE_NAME}.log 2>&1
             """
-            echo "./run.sh ${options.runParameters} >> ../${STAGE_NAME}.log 2>&1"
         }
         dir("Work/Results/Blender")
         {
