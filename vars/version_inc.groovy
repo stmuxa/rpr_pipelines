60901d43fc2e282f9d030968b34e5c284640b36c
epozine <epozine@inbox.ru>
2018-02-09 16:35:31 +0300

    Update version_inc.groovy

--- vars/version_inc.groovy
+++ vars/version_inc.groovy
@@ -1,7 +1,7 @@
-def call(String version, Integer index = 3, String delimeter = '.')
+def call(String version, Integer index = 3, String delimiter = '.')
 {
     String new_version =python3(
-        "${CIS_TOOLS}/version_inc.py --version \"${version}\" --index ${index} --delimeter \"${delimeter}\""
+        "${CIS_TOOLS}/version_inc.py --version \"${version}\" --index ${index} --delimiter \"${delimiter}\""
                 ).split('\r\n')[2].trim()
     return new_version
 }
