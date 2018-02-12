d0a5e07534afa82f6c6ff2af9464f200249be75e
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-02-12 14:54:18 +0300

    Update multiplatform_pipeline.groovy

--- vars/multiplatform_pipeline.groovy
+++ vars/multiplatform_pipeline.groovy
@@ -3,7 +3,7 @@ def executePlatform(String osName, String gpuNames, def executeBuild, def execut
     def retNode =  
     {
         try {
-            if(options['withBuild'])
+            if(!options['skipBuils'])
             {
                 node("${osName} && ${options.BUILDER_TAG}")
                 {
