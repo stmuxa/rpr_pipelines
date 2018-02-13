875019de7cba90f62b9506691d9cde9878450a23
epozine <epozine@inbox.ru>
2018-02-13 11:39:44 +0300

    Update version_read.groovy

--- vars/version_read.groovy
+++ vars/version_read.groovy
@@ -2,7 +2,7 @@ def call(String path, String ver_prefix, String delimiter = '.')
 {
     String new_prefix = ver_prefix.replace('"', '""')
     String currentversion=python3(
-        "${CIS_TOOLS}/version_read.py --file \"${path}\" --prefix \"${new_prefix}\" --delimiter \"${delimiter}\""
+        "${CIS_TOOLS}/version_read.py --file \"${path}\" --prefix \"${new_prefix}\" --delimiter \"${delimiter}\" --format \"${format}\""
     ).split('\r\n')[2].trim()
     return currentversion;
 }
