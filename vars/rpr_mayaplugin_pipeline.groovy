a3c7fff3434f76adeb210d4a90dbfb70af0c9dac
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-02-14 20:22:43 +0300

    Update rpr_mayaplugin_pipeline.groovy

--- vars/rpr_mayaplugin_pipeline.groovy
+++ vars/rpr_mayaplugin_pipeline.groovy
@@ -35,18 +35,18 @@ def executeTestCommand(String osName, Map options)
                 '''*/
                 
                 powershell"""
-                $uninstall32 = gci "HKLM:\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall" | foreach { gp $_.PSPath } | ? { $_ -match "Radeon ProRender for Autodesk Maya®" } | select UninstallString
-                $uninstall64 = gci "HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall" | foreach { gp $_.PSPath } | ? { $_ -match "Radeon ProRender for Autodesk Maya®" } | select UninstallString
-                if ($uninstall64) {
-                $uninstall64 = $uninstall64.UninstallString -Replace "msiexec.exe","" -Replace "/I","" -Replace "/X",""
-                $uninstall64 = $uninstall64.Trim()
+                \$uninstall32 = gci "HKLM:\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall" | foreach { gp \$_.PSPath } | ? { \$_ -match "Radeon ProRender for Autodesk Maya®" } | select UninstallString
+                \$uninstall64 = gci "HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall" | foreach { gp \$_.PSPath } | ? { \$_ -match "Radeon ProRender for Autodesk Maya®" } | select UninstallString
+                if (\$uninstall64) {
+                \$uninstall64 = \$uninstall64.UninstallString -Replace "msiexec.exe","" -Replace "/I","" -Replace "/X",""
+                \$uninstall64 = \$uninstall64.Trim()
                 Write "Uninstalling..."
-                start-process "msiexec.exe" -arg "/X $uninstall64 /qn /quiet /L+ie ../../${STAGE_NAME}.uninstall.log /norestart" -Wait}
-                if ($uninstall32) {
-                $uninstall32 = $uninstall32.UninstallString -Replace "msiexec.exe","" -Replace "/I","" -Replace "/X",""
-                $uninstall32 = $uninstall32.Trim()
+                start-process "msiexec.exe" -arg "/X \$uninstall64 /qn /quiet /L+ie ../../${STAGE_NAME}.uninstall.log /norestart" -Wait}
+                if (\$uninstall32) {
+                \$uninstall32 = \$uninstall32.UninstallString -Replace "msiexec.exe","" -Replace "/I","" -Replace "/X",""
+                \$uninstall32 = \$uninstall32.Trim()
                 Write "Uninstalling..."
-                start-process "msiexec.exe" -arg "/X $uninstall32 /qn /quiet /L+ie ../../${STAGE_NAME}.uninstall.log /norestart" -Wait}
+                start-process "msiexec.exe" -arg "/X \$uninstall32 /qn /quiet /L+ie ../../${STAGE_NAME}.uninstall.log /norestart" -Wait}
                 """
             }
             catch(e)
