4ed182cbb125f946849ae91678ea51d83642d05c
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-02-16 19:38:13 +0300

    Update branch_detect_test_pipeline.groovy

--- vars/branch_detect_test_pipeline.groovy
+++ vars/branch_detect_test_pipeline.groovy
@@ -3,26 +3,19 @@ def call() {
     stage('PreBuild') {
       echo "Prebuld"
       echo "=============="
-      if("${BRANCH_NAME}" == "master" ){
+      if("${BRANCH_NAME}" == "master" && "${AUTHOR_NAME}" != "radeonprorender"){
         def commitHash = checkout(scm).GIT_COMMIT
         echo "${BRANCH_NAME} is master branch. build it by sha: ${commitHash}"
         
-        AUTHOR_NAME = bat (
-                            script: "git show -s --format='%%an' HEAD ",
+        AUTHOR_NAME = bat ( script: "git show -s --format='%%an' HEAD ",
                             returnStdout: true
                             ).split('\r\n')[2].trim()
         echo "Commit author: ${AUTHOR_NAME}"
-        
-        if("${AUTHOR_NAME}" != "radeonprorender") {
-           echo "Incrementing..."
-           //TODO: increment
-         } else {
-           echo "Last author ${AUTHOR_NAME} - not incremen"
-         }    
-        
+        echo "Incrementing..."
+        //TODO: make push    
       } else {
-        def commitHash = checkout(scm).GIT_COMMIT
-        //def commitMessage = checkout(scm).GIT_COMMIT
+        //def commitHash = checkout(scm).GIT_COMMIT
+        checkout(scm).each { name, value -> println "Name: $name -> Value $value" }
         echo "${BRANCH_NAME} isn't master branch. Parsing commit message..."
       }
       /*checkout([$class: 'GitSCM',
@@ -32,16 +25,8 @@ def call() {
     stage('Build') {
       echo "Build"
       echo "=============="
-      env.getEnvironment().each { name, value -> println "Name: $name -> Value $value" }
-      /*def scmVars = checkout scm
-      echo scmVars.getClass()
-      */
-      //def commitHash = checkout(scm).GIT_COMMIT
-      //echo "${commitHash}"
-      /*scmVars.each { key, value
-        echo "${key} : ${value}"
-        println "${key} : ${value}"
-      }*/
+      //env.getEnvironment().each { name, value -> println "Name: $name -> Value $value" }
+
     }
     stage('Deploy') {
       echo "Deploy"
