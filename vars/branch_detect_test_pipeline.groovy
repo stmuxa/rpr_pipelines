e33fda16e851f7c2323c0bc6d0f467f58de0b7ac
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-02-16 16:38:51 +0300

    Update branch_detect_test_pipeline.groovy

--- vars/branch_detect_test_pipeline.groovy
+++ vars/branch_detect_test_pipeline.groovy
@@ -2,24 +2,27 @@ def call() {
   node("ANDREY_A") {
     stage('PreBuild') {
       echo "Prebuld"
-      echo "${BRANCH_NAME}"
-      outputEnvironmentInfo("Windows")
-      
+      echo "=============="
+      if("${BRANCH_NAME}" == "master"){
+        def commitHash = checkout(scm).GIT_COMMIT
+        echo "${BRANCH_NAME} is master branch. build it by sha: "${commitHash}"
+      } else {
+        /*def commitHash = checkout(scm).GIT_COMMIT
+        def commitMessage = checkout(scm).GIT_COMMIT
+        echo "${BRANCH_NAME} */
+      }
       /*checkout([$class: 'GitSCM',
                 userRemoteConfigs: [[url: 'https://github.com/luxteam/branch_detect_test.git']]])
       */
-      /*bat"""
-      echo %CD%
-      set
-      """*/
     }
     stage('Build') {
-      echo "Build"
+      
+      //echo "Build"
       /*def scmVars = checkout scm
       echo scmVars.getClass()
       */
-      def commitHash = checkout(scm).GIT_COMMIT
-      echo "${commitHash}"
+      //def commitHash = checkout(scm).GIT_COMMIT
+      //echo "${commitHash}"
       /*scmVars.each { key, value
         echo "${key} : ${value}"
         println "${key} : ${value}"
