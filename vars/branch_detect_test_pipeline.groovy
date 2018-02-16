222e3559221b85830128f1c673524ae62b335a0a
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-02-16 15:49:11 +0300

    Update branch_detect_test_pipeline.groovy

--- vars/branch_detect_test_pipeline.groovy
+++ vars/branch_detect_test_pipeline.groovy
@@ -3,6 +3,14 @@ def call() {
     stage('PreBuild') {
       echo "Prebuld"
       echo "${BRANCH_NAME}"
+      outputEnvironmentInfo("Windows")
+      
+      checkout([$class: 'GitSCM',
+                userRemoteConfigs: [[url: 'https://github.com/luxteam/branch_detect_test.git']]])
+      
+      bat"""
+      echo %CD%
+      """
     }
     stage('Build') {
       echo "Build"
