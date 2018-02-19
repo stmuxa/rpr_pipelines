c6311bc3ad073aab62a3b6be70c76e00da4021bd
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-02-19 13:12:32 +0300

    Update branch_detect_test_pipeline.groovy

--- vars/branch_detect_test_pipeline.groovy
+++ vars/branch_detect_test_pipeline.groovy
@@ -56,7 +56,7 @@ def call() {
       if(build) {
         echo "Building...."
         echo "checkout from user branch: ${BRANCH_NAME}; repo: ${repoName}, commitId: 9cd800b6933f052a4d005984997cac43c9cbcb31"
-        checkout([$class: 'GitSCM', branches: [[name: "*/${BRANCH_NAME}"]], commitId: 9cd800b6933f052a4d005984997cac43c9cbcb31, doGenerateSubmoduleConfigurations: false, extensions: [
+        checkout([$class: 'GitSCM', branches: [[name: "*/${BRANCH_NAME}"]], commitId: "9cd800b6933f052a4d005984997cac43c9cbcb31", doGenerateSubmoduleConfigurations: false, extensions: [
             [$class: 'CleanBeforeCheckout'],
             [$class: 'CleanCheckout'],
          //   [$class: 'WipeWorkspace'],
