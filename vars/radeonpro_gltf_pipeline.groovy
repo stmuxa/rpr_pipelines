566c71f1adbc79f0f265fd31a90ca0bf80dfbdb2
epozine <epozine@inbox.ru>
2018-02-27 15:47:46 +0300

    Update radeonpro_gltf_pipeline.groovy

--- vars/radeonpro_gltf_pipeline.groovy
+++ vars/radeonpro_gltf_pipeline.groovy
@@ -10,7 +10,7 @@ def executeTests(String osName, String asicName, Map options)
 
 def executeBuildWindows()
 {
-    bat """
+    powershell """
     set msbuild=\"C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe\"
     if not exist %msbuild% (
         set msbuild=\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe\"
