fb5ef7caec91b0c18555afb3166e792dec02b167
epozine <epozine@inbox.ru>
2018-02-27 16:20:53 +0300

    Update radeonpro_gltf_pipeline.groovy

--- vars/radeonpro_gltf_pipeline.groovy
+++ vars/radeonpro_gltf_pipeline.groovy
@@ -18,7 +18,9 @@ def executeBuildWindows()
     set target=build
     set maxcpucount=/maxcpucount 
     set PATH=C:\\Python27\\;%PATH%
-    powershell .\\Tools\\premake\\win\\premake5 vs2015    >> ${STAGE_NAME}.log 2>&1 
+    """
+    powershell """
+    .\\Tools\\premake\\win\\premake5 vs2015    >> ${STAGE_NAME}.log 2>&1 
     set solution=Build\\ProRenderGLTF.sln
     %msbuild% /target:%target% %maxcpucount% /property:Configuration=Release;Platform=x64 %parameters% %solution% >> ${STAGE_NAME}.log 2>&1
     """
