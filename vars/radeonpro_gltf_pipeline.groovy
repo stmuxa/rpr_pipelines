68807e391dbbcacc2713bb1a25d680342e0dc891
Kravchenko Andrey <epozine@inbox.ru>
2018-02-27 14:37:54 +0300

    Add GLTF pipeline

---
+++ vars/radeonpro_gltf_pipeline.groovy
@@ -0,0 +1,107 @@
+def executeTestCommand(String osName)
+{
+    
+}
+
+def executeTests(String osName, String asicName, Map options)
+{
+    
+}
+
+def executeBuildWindows()
+{
+    bat """
+    set msbuild=\"C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe\"
+    if not exist %msbuild% (
+        set msbuild=\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe\"
+    )
+    set target=build
+    set maxcpucount=/maxcpucount 
+    set PATH=C:\\Python27\\;%PATH%
+    .\\Tools\\premake\\win\\premake5 vs2015    >> ${STAGE_NAME}.log 2>&1 """
+}
+
+def executeBuildOSX()
+{
+
+}
+
+def executeBuildLinux()
+{
+
+}
+
+def executeBuild(String osName, Map options)
+{
+    try {
+        checkOutBranchOrScm(options['projectBranch'], 'https://github.com/Radeon-Pro/RadeonProRender-GLTF.git')
+        outputEnvironmentInfo(osName)
+
+        switch(osName)
+        {
+        case 'Windows': 
+            executeBuildWindows(); 
+            break;
+        case 'OSX':
+            break;
+        default: 
+            break;
+        }
+
+       // stash includes: 'Bin/**/*', name: "app${osName}"
+        
+    }
+    catch (e) {
+        currentBuild.result = "FAILED"
+        throw e
+    }
+    finally {
+        archiveArtifacts "${STAGE_NAME}.log"
+    }                        
+}
+
+def executeDeploy(Map options, List platformList, List testResultList)
+{
+    try {
+        platformList.each()
+        {
+            String osName = it;
+            dir(osName)
+            {
+                //unstash "app${osName}"
+                
+                
+     
+            }
+        }
+       
+        
+    }
+    catch (e) {
+        currentBuild.result = "FAILED"
+        throw e
+    }
+    finally {
+        //archiveArtifacts "${STAGE_NAME}.log"
+    }   
+}
+
+def call(String projectBranch = "", 
+         String platforms = 'Windows:AMD_RXVEGA,AMD_WX9100,AMD_WX7100;', 
+         Boolean updateRefs = false, Boolean enableNotifications = true) {
+    
+    String PRJ_NAME="RadeonProRender-GLTF"
+    String PRJ_ROOT="rpr-core"
+    properties([[$class: 'BuildDiscarderProperty', strategy: 
+                 [$class: 'LogRotator', artifactDaysToKeepStr: '', 
+                  artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10']]]);
+
+    multiplatform_pipeline(platforms, null, this.&executeBuild, null, this.&executeDeploy, 
+                           [projectBranch:projectBranch, 
+                            enableNotifications:enableNotifications,
+                            BUILDER_TAG:'BuilderS',
+                            executeBuild:true,
+                            executeTests:false,
+                            PRJ_NAME:PRJ_NAME,
+                            PRJ_ROOT:PRJ_ROOT])
+}
