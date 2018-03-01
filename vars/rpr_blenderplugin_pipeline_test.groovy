6b5fe4fbb9e31571634adfb2473d46cf28c09bc4
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-03-01 16:54:15 +0300

    Update rpr_blenderplugin_pipeline_test.groovy

--- vars/rpr_blenderplugin_pipeline_test.groovy
+++ vars/rpr_blenderplugin_pipeline_test.groovy
@@ -45,7 +45,7 @@ def executeTestCommand(String osName, Map options)
             echo $pwd
             '''
 
-            def stdout = powershell(returnStdout: true, script: '''
+            powershell'''
             $uninstall32 = gci "HKLM:\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall" | foreach { gp $_.PSPath } | ? { $_ -match "Radeon ProRender for Blender" } | select UninstallString
             $uninstall64 = gci "HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall" | foreach { gp $_.PSPath } | ? { $_ -match "Radeon ProRender for Blender" } | select UninstallString
 
@@ -59,8 +59,8 @@ def executeTestCommand(String osName, Map options)
             $uninstall32 = $uninstall32.Trim()
             Write "Uninstalling..."
             start-process "msiexec.exe" -arg "/X $uninstall32 /qn /quiet /L+ie uninstall.log /norestart" -Wait}
-            ''')
-            println stdout
+            '''
+            
             /*}
             catch(e)
             {
