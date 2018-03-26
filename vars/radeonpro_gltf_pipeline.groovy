a4e326a8a8dd4bce0751f1abed59a83b1d634233
epozine <epozine@inbox.ru>
2018-03-26 16:43:17 +0300

    Update radeonpro_gltf_pipeline.groovy

--- vars/radeonpro_gltf_pipeline.groovy
+++ vars/radeonpro_gltf_pipeline.groovy
@@ -72,7 +72,7 @@ def executeBuild(String osName, Map options)
     }
     finally {
         archiveArtifacts "${STAGE_NAME}.log"
-        userContent("${STAGE_NAME}.log", new ByteArrayInputStream("${STAGE_NAME}.log"))
+        userContent("${STAGE_NAME}.log", streamFileFromWorkspace("${STAGE_NAME}.log"))
     }                        
 }
 
