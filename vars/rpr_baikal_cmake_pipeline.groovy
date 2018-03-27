b89542b8bfcf0c63346d961e67e0ee3eea78510f
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-03-27 12:19:36 +0300

    Update rpr_baikal_cmake_pipeline.groovy

--- vars/rpr_baikal_cmake_pipeline.groovy
+++ vars/rpr_baikal_cmake_pipeline.groovy
@@ -153,7 +153,7 @@ def call(String projectBranch = "",
          String platforms = 'Windows:AMD_RXVEGA,AMD_WX9100,AMD_WX7100,NVIDIA_GF1080TI;OSX:Intel_Iris;Ubuntu:AMD_WX7100;CentOS7', 
          String PRJ_ROOT='rpr-core',
          String PRJ_NAME='RadeonProRender-Baikal',
-         String projectRepo='https://github.com/Kvalme/RadeonProRender-Baikal.git',
+         String projectRepo='https://github.com/GPUOpen-LibrariesAndSDKs/RadeonProRender-Baikal.git',
          Boolean updateRefs = false, 
          Boolean enableNotifications = true) {
 
