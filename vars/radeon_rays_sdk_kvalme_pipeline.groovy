479e8bcf58ffd1ea048333e0d39e182840469979
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-03-27 11:52:49 +0300

    Create radeon_rays_sdk_kvalme_pipeline.groovy

---
+++ vars/radeon_rays_sdk_kvalme_pipeline.groovy
@@ -0,0 +1,138 @@
+def executeTestCommand(String osName)
+{
+    switch(osName)
+    {
+    case 'Windows':
+        bat "..\\Build\\bin\\Release\\UnitTest.exe  --gtest_output=xml:../${STAGE_NAME}.gtest.xml >> ..\\${STAGE_NAME}.log  2>&1"
+        break;
+    case 'OSX':
+        sh "../Build/bin/UnitTest           --gtest_output=xml:../${STAGE_NAME}.gtest.xml >> ../${STAGE_NAME}.log  2>&1"
+        break;
+    default:
+        sh """
+        export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:../Build/bin
+        ../Build/bin/UnitTest           --gtest_output=xml:../${STAGE_NAME}.gtest.xml >> ../${STAGE_NAME}.log  2>&1
+        """
+    }  
+}
+
+def executeTests(String osName, String asicName, Map options)
+{
+    try {
+        checkOutBranchOrScm(options['projectBranch'], options['projectURL'])
+
+        outputEnvironmentInfo(osName)
+        unstash "app${osName}"
+
+        dir('UnitTest')
+        {
+            executeTestCommand(osName)
+        }                
+    }
+    catch (e) {
+        println(e.toString());
+        println(e.getMessage());
+        //println(e.getStackTrace());    
+        
+        currentBuild.result = "FAILED"
+        throw e
+    }
+    finally {
+        archiveArtifacts "*.log"
+        junit "*.gtest.xml"
+    }
+}
+
+def executeBuildWindows()
+{
+    bat """
+    set msbuild=\"C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe\"
+    if not exist %msbuild% (
+        set msbuild=\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe\"
+    )
+    mkdir Build
+    cd Build
+    cmake -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 14 2015 Win64" .. >> ..\\${STAGE_NAME}.log 2>&1
+    cmake --build . --config Release >> ..\\${STAGE_NAME}.log 2>&1
+    """
+}
+
+def executeBuildOSX()
+{
+    sh """
+    mkdir Build
+    cd Build
+    cmake -DCMAKE_BUILD_TYPE=Release .. >> ../${STAGE_NAME}.log 2>&1
+    make >> ../${STAGE_NAME}.log 2>&1
+    """
+}
+
+def executeBuildLinux()
+{
+    sh """
+    mkdir Build
+    cd Build
+    cmake -DCMAKE_BUILD_TYPE=Release .. >> ../${STAGE_NAME}.log 2>&1
+    make >> ../${STAGE_NAME}.log 2>&1
+    """
+}
+
+def executeBuild(String osName, Map options)
+{
+    try {
+        checkOutBranchOrScm(options['projectBranch'], options['projectURL'])
+        outputEnvironmentInfo(osName)
+
+        switch(osName)
+        {
+        case 'Windows': 
+            executeBuildWindows(); 
+            break;
+        case 'OSX':
+            executeBuildOSX();
+            break;
+        default: 
+            executeBuildLinux();
+        }
+       
+        stash includes: 'Build/bin/**/*', name: "app${osName}"
+    }
+    catch (e) {
+        currentBuild.result = "FAILED"
+        throw e
+    }
+    finally {
+        archiveArtifacts "${STAGE_NAME}.log"
+    }                        
+
+}
+
+def executeDeploy(Map options, List platformList, List testResultList)
+{
+}
+
+def call(String projectBranch = "", 
+         String platforms = 'Windows:AMD_RXVEGA,AMD_WX9100,AMD_WX7100,NVIDIA_GF1080TI;OSX:Intel_Iris;Ubuntu:AMD_WX7100', 
+         Boolean enableNotifications = true) {
+
+    String PRJ_NAME="RadeonRays_SDK_Kvalme"
+    String PRJ_ROOT="rpr-core"
+    String projectURL = 'https://github.com/Kvalme/RadeonRays_SDK.git'
+    
+    properties([[$class: 'BuildDiscarderProperty', strategy: 
+                 [$class: 'LogRotator', artifactDaysToKeepStr: '', 
+                  artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10']]]);
+    
+    multiplatform_pipeline(platforms, null, this.&executeBuild, this.&executeTests, null, 
+                           [projectBranch:projectBranch,
+                            enableNotifications:enableNotifications,
+                            executeBuild:true,
+                            executeTests:true,
+                            PRJ_NAME:PRJ_NAME,
+                            PRJ_ROOT:PRJ_ROOT,
+                            BUILDER_TAG:'BuilderS',
+                            projectURL:projectURL,
+                            slackChannel:"${SLACK_BAIKAL_CHANNEL}",
+                            slackBaseUrl:"${SLACK_BAIKAL_BASE_URL}",
+                            slackTocken:"${SLACK_BAIKAL_TOCKEN}"])
+}
