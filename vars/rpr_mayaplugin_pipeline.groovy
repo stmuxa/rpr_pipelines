be23d698329262c5cecd5a3db784e3dd816aa006
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-03-28 15:43:57 +0300

    Update rpr_mayaplugin_pipeline.groovy

--- vars/rpr_mayaplugin_pipeline.groovy
+++ vars/rpr_mayaplugin_pipeline.groovy
@@ -154,7 +154,7 @@ def executeBuildWindows(Map options)
         """
         
         String branch_postfix = ""
-        if(binding.hasVariable('BRANCH_NAME') && BRANCH_NAME != "master")
+        if(env.BRANCH_NAME && env.BRANCH_NAME != "master")
         {
             branch_postfix = BRANCH_NAME.replace('/', '-')
         }else if(binding.hasVariable('Branch') && Branch != "master")
@@ -196,7 +196,7 @@ def executeBuildOSX(Map options)
         dir('.installer_build')
         {
             String branch_postfix = ""
-            if(binding.hasVariable('BRANCH_NAME') && BRANCH_NAME != "master")
+            if(env.BRANCH_NAME && env.BRANCH_NAME != "master")
             {
                 branch_postfix = BRANCH_NAME.replace('/', '-')
             }else if(binding.hasVariable('Branch') && Branch != "master")
