20405f23b9d537c0907c3f4d2e0e6f9b6b77ff6e
Andrey Kravchenko <epozine@inbox.ru>
2018-05-10 17:57:23 +0300

    Update render_scene_job.groovy

--- vars/render_scene_job.groovy
+++ vars/render_scene_job.groovy
@@ -1,1 +1,134 @@
+def executeRender(Map options)
+{
+  bat """
+     echo OK
+  """ 
+}
 
+def executePlatform(String osName, String gpuNames, def executeRender, Map options)
+{
+    def retNode =  
+    {   
+        try {
+            
+            if(gpuNames && options['executeTests'])
+            {
+                def testTasks = [:]
+                gpuNames.split(',').each()
+                {
+                    String asicName = it
+                    echo "Scheduling Test ${osName}:${asicName}"
+
+                    testTasks["Test-${it}-${osName}"] = {
+                        node("${osName} && Tester && OpenCL && gpu${asicName}")
+                        {
+                            stage("Test-${asicName}-${osName}")
+                            {
+                                ws("WS/${options.PRJ_NAME}_Test") {
+                                    Map newOptions = options.clone()
+                                    newOptions['testResultsName'] = "testResult-${asicName}-${osName}"
+                                    executeRender(osName, asicName, newOptions)
+                                }
+                            }
+                        }
+                    }
+                }
+                parallel testTasks
+            }
+        }
+        catch (e) {
+            println(e.toString());
+            println(e.getMessage());
+            println(e.getStackTrace());        
+            currentBuild.result = "FAILED"
+            echo "FAILED by executePlatform"
+            throw e
+        }
+    }
+    return retNode
+}
+
+def main(String platforms, def executeRender, Map options) {
+    
+    try {
+        properties([[$class: 'BuildDiscarderProperty', strategy: 
+                     [$class: 'LogRotator', artifactDaysToKeepStr: '', 
+                      artifactNumToKeepStr: '10', daysToKeepStr: '', numToKeepStr: '']]]);
+        
+        timestamps {
+            String PRJ_PATH="${options.PRJ_ROOT}/${options.PRJ_NAME}"
+            String JOB_PATH="${PRJ_PATH}/${JOB_NAME}/Build-${BUILD_ID}".replace('%2F', '_')
+            options['PRJ_PATH']="${PRJ_PATH}"
+            options['JOB_PATH']="${JOB_PATH}"
+
+            def platformList = [];
+            def testResultList = [];
+
+            try {
+              
+                def tasks = [:]
+
+                platforms.split(';').each()
+                {
+                   
+                    List tokens = it.tokenize(':')
+                    String osName = tokens.get(0)
+                    String gpuNames = ""
+                    if (tokens.size() > 1)
+                    {
+                        gpuNames = tokens.get(1)
+                    }
+                    
+                    platformList << osName
+                    if(gpuNames)
+                    {
+                        gpuNames.split(',').each()
+                        {
+                            String asicName = it
+                            testResultList << "testResult-${asicName}-${osName}"
+                        }
+                    }
+
+                    tasks[osName]=executePlatform(osName, gpuNames, executeRender, options)
+                }
+                parallel tasks
+            }     
+    }
+    catch (e) {
+        println(e.toString());
+        println(e.getMessage());
+        println(e.getStackTrace());
+        currentBuild.result = "FAILED"
+        throw e
+    }
+    finally {
+
+        echo "enableNotifications = ${options.enableNotifications}"
+        if("${options.enableNotifications}" == "true")
+        {
+            sendBuildStatusNotification(currentBuild.result, 
+                                        options.get('slackChannel', ''), 
+                                        options.get('slackBaseUrl', ''),
+                                        options.get('slackTocken', ''),
+                                        options.CBR)
+        }
+    }
+}
+  
+def call(String platforms = 'Windows:AMD_RXVEGA', 
+         String PRJ_ROOT='Render_Scene',
+         String PRJ_NAME='Render_Scene'
+         String LinkScene = '',
+         String LinkMSI = '',
+         String Tool = ''
+         ) {
+
+    main(platforms, this.&executeRender,
+                           [
+                            enableNotifications:False,
+                            PRJ_NAME:PRJ_NAME,
+                            PRJ_ROOT:PRJ_ROOT,
+                            LinkScene:LinkScene,
+                            LinkMSI:LinkMSI,
+                            Tool:Tool])
+}
