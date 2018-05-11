b943e254aeaf45e711336821965a52bf814f9204
Andrey Kravchenko <epozine@inbox.ru>
2018-05-11 14:56:07 +0300

    Update render_scene_job.groovy

--- vars/render_scene_job.groovy
+++ vars/render_scene_job.groovy
@@ -16,7 +16,7 @@ def executeRender(Map options)
             bat """
             "C:\\JN\\cis_tools\\receiveFiles.bat" /rpr-plugins/RenderJob/${options.Scene_folder} .
             """
-            String scene=python3("${options.Scene_folder}/find_scene.py")
+            String scene=python3("${options.Scene_folder}/find_scene.py --folder ${options.Scene_folder")
             bat """
             cd "${options.Scene_folder}"
             "C:\\Program Files\\Blender Foundation\\Blender\\blender.exe" -b "${scene}" -P "blender_render.py"
