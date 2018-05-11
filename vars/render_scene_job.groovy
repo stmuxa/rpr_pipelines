0512362681e973e103119c159115a8e5582004d7
Andrey Kravchenko <epozine@inbox.ru>
2018-05-11 12:42:09 +0300

    Update render_scene_job.groovy

--- vars/render_scene_job.groovy
+++ vars/render_scene_job.groovy
@@ -5,9 +5,6 @@ def executeRender(Map options)
   bat """
      "C:\\JN\\cis_tools\\receiveFiles.bat" /rpr-plugins/RenderJob .
   """
-  bat """
-     rmdir /s /q Output
-  """
   switch(options['Tool']) 
   {
     case 'Blender 2.79':
