5278bcdee821a716daf5500782a2c082a3c3b3b7
Andrey Kravchenko <epozine@inbox.ru>
2018-05-11 15:27:46 +0300

    Update render_scene_job.groovy

--- vars/render_scene_job.groovy
+++ vars/render_scene_job.groovy
@@ -38,7 +38,7 @@ def executeRender(Map options)
             bat """
             cd "${options.Scene_folder}"
             set MAYA_SCRIPT_PATH=%cd%;%MAYA_SCRIPT_PATH%
-            "C:\\Program Files\\Autodesk\\Maya2017\\bin\\maya.exe" -command "source maya_render.mel; evalDeferred -lp (rpr_render("'${scene}'"));"
+            "C:\\Program Files\\Autodesk\\Maya2017\\bin\\maya.exe" -command "source maya_render.mel; evalDeferred -lp (rpr_render("""${scene}"""));"
             """
             break;
   }    
