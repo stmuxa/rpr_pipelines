b613cd925e6da1ad14b040622a89aed2a94971a9
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-05-14 14:38:34 +0300

    Update sendBuildStatusNotification.groovy

--- vars/sendBuildStatusNotification.groovy
+++ vars/sendBuildStatusNotification.groovy
@@ -39,7 +39,7 @@ def call(String buildStatus = 'STARTED', String channel = '', String baseUrl = '
 		"title": "${buildStatus}\\nCIS: ${env.JOB_NAME} [${env.BUILD_NUMBER}]",
 		"title_link": "${env.BUILD_URL}",
 		"color": "${colorCode}",
-    "text": ">>> Branch: *${env.BRANCH_NAME}*${INIT_BRANCH}\\nAuthor *${info.author}*\\nCommit message\\n```${info.commitMessage.replace('\n', '\\n')}```",
+    "text": ">>> Branch: *${env.BRANCH_NAME}*${INIT_BRANCH}\\nAuthor: *${info.author}*\\nCommit message:\\n```${info.commitMessage.replace('\n', '\\n')}```",
 		"mrkdwn_in": ["text", "title"],
 		"attachment_type": "default",
 		"actions": [
