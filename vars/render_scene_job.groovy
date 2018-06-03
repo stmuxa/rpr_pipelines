7a01694a44631aa083debf75835ac50ab77f2fc1
Andrey Kravchenko <epozine@inbox.ru>
2018-06-03 21:07:39 +0300

    Update render_scene_job.groovy

--- vars/render_scene_job.groovy
+++ vars/render_scene_job.groovy
@@ -13,7 +13,7 @@ def executeRender(Map options)
             copy "..\\..\\cis_tools\\RenderSceneJob\\find_scene_blender.py" "."
             copy "..\\..\\cis_tools\\RenderSceneJob\\blender_render.py" "."
             """
-            String scene=python3("${options.Scene_folder}/find_scene_blender.py --folder ${options.Scene_folder}").split('\r\n')[2].trim()
+            String scene=python3("${options.Scene_folder}/find_scene_blender.py --folder .").split('\r\n')[2].trim()
             echo "Find scene: ${scene}"
             echo "Launch App"
             bat """
