6eb74344355dbab19452e2f0d1b93be3f4da408f
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-06-06 19:45:48 +0300

    Update branch_detect_test_pipeline.groovy

--- vars/branch_detect_test_pipeline.groovy
+++ vars/branch_detect_test_pipeline.groovy
@@ -1,60 +1,5 @@
 import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException;
 
-def sendBuildStatusNotification_d(String buildStatus = 'STARTED', String channel = '', String baseUrl = '', String token = '', Map info)
-{
-  echo "sending information about build status: ${buildStatus}"
-  
-  // build status of null means successful
-  buildStatus =  buildStatus ?: 'SUCCESSFUL'
-  // if CBR not null - replace buildStatus value
-  buildStatus = info.CBR ?: buildStatus
- 
-  // Default values
-  def colorName = 'RED'
-  def colorCode = '#FF0000'
-
-  // Override default values based on build status
-  if (buildStatus == 'SUCCESSFUL') {
-    color = 'GREEN'
-    colorCode = '#00FF00'
-  } else if (buildStatus == 'SKIPPED') {
-    color = 'BLUE'
-    colorCode = '#0000FF'
-  } else if (buildStatus == 'ABORTED') {
-    colorCode = '#ff8833'
-  } else {
-    color = 'RED'
-    colorCode = '#FF0000'
-  }
-
-  String INIT_BRANCH = env.CHANGE_BRANCH ? "\\nSource branch: *${env.CHANGE_BRANCH}*" : ''
-  String HTML_REPORT_LINK = info.reportName ? "${env.BUILD_URL}${info.reportName}" : ''
-  
-  String slackMessage = """[{		
-		"fallback": "Message if attachment disabled",
-		"title": "${buildStatus}\\nCIS: ${env.JOB_NAME} [${env.BUILD_NUMBER}]",
-		"title_link": "${env.BUILD_URL}",
-		"color": "${colorCode}",
-    "text": ">>> Branch: *${env.BRANCH_NAME}*${INIT_BRANCH}\\nAuthor *${info.author}*\\nCommit message\\n```${info.commitMessage.replace('\n', '\\n')}```",
-		"mrkdwn_in": ["text", "title"],
-		"attachment_type": "default",
-		"actions": [
-			{"text": "Report",
-			"type": "button",
-			"url": "${HTML_REPORT_LINK}"
-			},
-			{"text": "PullRequest on GitHub",
-			"type": "button",
-			"url": "${env.CHANGE_URL}"
-			}
-		]
-	 }]""".replace('%2F', '_')
-	
-  // Send notifications
-  //slackSend (color: colorCode, message: '', channel: channel, baseUrl: baseUrl, token: token, attachment: slackMessage)
-  slackSend(attachments: slackMessage, channel: channel, baseUrl: baseUrl, token: token) 
-}
-
 def call(String projectBranch="")
 {
   Map options = [x:2]
@@ -80,21 +25,11 @@ def call(String projectBranch="")
         }
     }
   }
-  catch(FlowInterruptedException e)
-  {
+  catch(FlowInterruptedException e) {
     CBR = "ABORTED"
   }
-  finally
-  {
-  /*   
-            sendBuildStatusNotification_d(currentBuild.result,
-            'cis_notification_test', 
-            'https://luxcis.slack.com/services/hooks/jenkins-ci/',
-            "${env.SLACK_LUXCIS_TOKEN}",
-            [commitMessage:commitMessage,
-	    author:AUTHOR_NAME])        
-*/	  
-	  githubNotify status: 'ERROR'
+  finally {
+    githubNotify description: 'This is a shorted example',  status: 'SUCCESS'
   }
 }
 
