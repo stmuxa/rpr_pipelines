7702272d7594c1cce3ca7ff57c8402d74b2308e1
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2018-06-19 15:55:34 +0300

    Update rpr_mayaplugin_pipeline.groovy

--- vars/rpr_mayaplugin_pipeline.groovy
+++ vars/rpr_mayaplugin_pipeline.groovy
@@ -356,8 +356,12 @@ def executeDeploy(Map options, List platformList, List testResultList)
             }
 
             dir("jobs_launcher")
-            {   
+            {
+                if(env.Branch) {
+                    options.branchName = Branch 
+                }
                 bat """
+                IF NOT DEFINED BRANCH_NAME (set BRANCH_NAME=${options.branchName})
                 build_reports.bat ..\\summaryTestResults Maya2017 ${options.commitSHA}
                 """
             }   
