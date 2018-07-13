e3c0378c321e838f25b6a5fb7ea0720505d699d2
Andrey Kravchenko <epozine@inbox.ru>
2018-07-13 15:02:35 +0300

    Update render_scene_job.groovy

--- vars/render_scene_job.groovy
+++ vars/render_scene_job.groovy
@@ -15,7 +15,7 @@ def executeRender(osName, Map options) {
         
             if (options['Plugin'] != '') {
                   String plugin = options['Plugin'].split('/')[-1].trim()
-                  String status = python3("..\\..\\cis_tools\\RenderSceneJob\\check_installer.py --plugin_md5 \"${options.md5}\" --folder C:\\JN\\WS\\Render_Scene_Test ").split('\r\n')[2].trim()
+                  String status = python3("..\\..\\cis_tools\\RenderSceneJob\\check_installer.py --plugin_md5 \"${options.md5}\" --folder . ").split('\r\n')[2].trim()
                   print("STATUS: ${status}")
                   if (status == "DOWNLOAD_COPY") {
                           bat """ 
