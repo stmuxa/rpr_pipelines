9e0d01341be3840ea87fc18537e7ca8838f89246
Andrey Kravchenko <epozine@inbox.ru>
2018-07-20 19:22:02 +0300

    Update render_scene_job.groovy

--- vars/render_scene_job.groovy
+++ vars/render_scene_job.groovy
@@ -114,7 +114,7 @@ def executeRender(osName, Map options) {
             }
             finally {
               archiveArtifacts "Output/*"
-              python3("..\\..\\cis_tools\\RenderSceneJob\\send_post.py --django_ip \"http://render.cis.luxoft.com//jenkins_post_form/\" --build_number ${currentBuild.number} --status ${currentBuild.result} --id ${id}")
+              python3("..\\..\\cis_tools\\RenderSceneJob\\send_post.py --django_ip \"https://render.cis.luxoft.com//jenkins_post_form/\" --build_number ${currentBuild.number} --status ${currentBuild.result} --id ${id}")
             }
      break;
     case 'OSX':
