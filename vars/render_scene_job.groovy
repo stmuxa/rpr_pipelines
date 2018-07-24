d971ecb2e289ff5e94c0540ab371aa34b28db0d6
Andrey Kravchenko <epozine@inbox.ru>
2018-07-24 13:02:58 +0300

    Update render_scene_job.groovy

--- vars/render_scene_job.groovy
+++ vars/render_scene_job.groovy
@@ -207,7 +207,7 @@ def executeRender(osName, Map options) {
               case 'Blender':      
               
                       if (options['Plugin'] != 'Skip') {
-                            String plugin = options['Plugin'].split('/')[-1].trim()
+                            String plugin = status.split('/')[-1].trim()
                             sh'''
                             $CIS_TOOLS/installBlenderPlugin.sh ./${plugin} >> install.log 2>&1
                             '''
