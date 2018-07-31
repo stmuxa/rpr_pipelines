d0f3d44b56803aa228f943458faafbcc877449d4
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2018-07-31 14:29:02 +0300

    Update rpr_blenderplugin_pipeline_test.groovy

--- vars/rpr_blenderplugin_pipeline_test.groovy
+++ vars/rpr_blenderplugin_pipeline_test.groovy
@@ -125,7 +125,7 @@ def executeTestCommand(String osName, Map options)
         dir('scripts')
         {
             bat """
-            run.bat ${options.renderDevice} ${options.testsPackage} \"${options.tests}\" ${options[${asicName}-${osName}-continueExecution}] >> ../${STAGE_NAME}.log  2>&1
+            run.bat ${options.renderDevice} ${options.testsPackage} \"${options.tests}\" ${options[${asicName}-${osName}-continueExecution]} >> ../${STAGE_NAME}.log  2>&1
             """
             /*bat """
             set PATH=C:\\Python35\\;C:\\Python35\\scripts\\;%PATH%
@@ -137,7 +137,7 @@ def executeTestCommand(String osName, Map options)
         dir("scripts")
         {           
             sh """
-            ./run.sh ${options.renderDevice} ${options.testsPackage} \"${options.tests}\" ${options[${asicName}-${osName}-continueExecution}] >> ../${STAGE_NAME}.log  2>&1
+            ./run.sh ${options.renderDevice} ${options.testsPackage} \"${options.tests}\" ${options[${asicName}-${osName}-continueExecution]} >> ../${STAGE_NAME}.log  2>&1
             # python ../jobs_launcher/executeTests.py --split_execution ${options.continueExecution} --test_filter ${options.tests} --file_filter ${options.testsPackage} --tests_root ../jobs --work_root ../Work/Results --work_dir Blender --cmd_variables Tool "blender" RenderDevice ${options.renderDevice} ResPath "$CIS_TOOLS/../TestResources/BlenderAssets/scenes" PassLimit 1 rx 0 ry 0 >> ../${STAGE_NAME}.log 2>&1
             """             
         }
@@ -146,7 +146,7 @@ def executeTestCommand(String osName, Map options)
         dir("scripts")
         {           
             sh """
-            ./run.sh ${options.renderDevice} ${options.testsPackage} \"${options.tests}\" ${options[${asicName}-${osName}-continueExecution}] >> ../${STAGE_NAME}.log  2>&1
+            ./run.sh ${options.renderDevice} ${options.testsPackage} \"${options.tests}\" ${options[${asicName}-${osName}-continueExecution]} >> ../${STAGE_NAME}.log  2>&1
             # python ../jobs_launcher/executeTests.py --split_execution ${options.continueExecution} --test_filter ${options.tests} --file_filter ${options.testsPackage} --tests_root ../jobs --work_root ../Work/Results --work_dir Blender --cmd_variables Tool "blender" RenderDevice ${options.renderDevice} ResPath "$CIS_TOOLS/../TestResources/BlenderAssets/scenes" PassLimit 1 rx 0 ry 0 >> ../${STAGE_NAME}.log 2>&1
             """
         }  
