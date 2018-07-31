27d658617192240fb559ec6e4b54ad7eab82748d
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2018-07-31 15:13:38 +0300

    Update multiplatform_pipeline.groovy

--- vars/multiplatform_pipeline.groovy
+++ vars/multiplatform_pipeline.groovy
@@ -24,6 +24,7 @@ def executeTestsNode(String osName, String gpuNames, def executeTests, Map optio
                                     Map newOptions = options.clone()
                                     newOptions['testResultsName'] = "testResult-${asicName}-${osName}"
                                     newOptions['stageName'] = "${asicName}-${osName}"
+                                    newOptions["${asicName}-${osName}-continueExecution"] = ""
                                     continue_execution = executeTests(osName, asicName, newOptions)
                                     if(continue_execution)
                                     {
