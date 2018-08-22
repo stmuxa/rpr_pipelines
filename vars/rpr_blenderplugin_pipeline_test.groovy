042e53a3668c2cddc2e90fd8e142622d9db3280f
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2018-08-22 17:40:48 +0300

    Update rpr_blenderplugin_pipeline_test.groovy

--- vars/rpr_blenderplugin_pipeline_test.groovy
+++ vars/rpr_blenderplugin_pipeline_test.groovy
@@ -540,6 +540,7 @@ def executePreBuild(Map options)
 
     dir('jobs_test_blender')
     {
+        def options.tests = []
         checkOutBranchOrScm(options['testsBranch'], 'https://github.com/luxteam/jobs_test_blender.git')
         String tempTests = readFile("jobs/${options.testsPackage}")
         tempTests.split("\n").each {
