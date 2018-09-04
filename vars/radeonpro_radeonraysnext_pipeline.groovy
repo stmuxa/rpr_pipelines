aa2fd8fb66ae342cfc1e27589cd48d68a1b9c747
Ilias Gazizov <igazizov@luxoft.com>
2018-09-04 19:50:53 +0300

    Added copying file to run from UT folder

--- vars/radeonpro_radeonraysnext_pipeline.groovy
+++ vars/radeonpro_radeonraysnext_pipeline.groovy
@@ -5,8 +5,8 @@ def executeTestCommand(String osName)
     case 'Windows':
         bat """
         cd .\\unittests
-        dir Release
-        Release\\UnitTests.exe  --gtest_output=xml:../${STAGE_NAME}.gtest.xml >> ..\\${STAGE_NAME}.log  2>&1
+        copy /y Release\\UnitTests.exe UnitTests.exe
+        UnitTests.exe  --gtest_output=xml:../${STAGE_NAME}.gtest.xml >> ..\\${STAGE_NAME}.log  2>&1
         """
         break;
     case 'OSX':
