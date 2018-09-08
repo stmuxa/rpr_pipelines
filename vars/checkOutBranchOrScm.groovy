eea3d779e29b723fe2eed7837f2350f193100d3d
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2018-09-08 17:48:31 +0300

    Update checkOutBranchOrScm.groovy

--- vars/checkOutBranchOrScm.groovy
+++ vars/checkOutBranchOrScm.groovy
@@ -4,15 +4,18 @@ def call(String branchName, String repoName) {
     {
         echo "checkout from user branch: ${branchName}; repo: ${repoName}"
         //checkout([$class: 'GitSCM', branches: [[name: "*/${branchName}"]], doGenerateSubmoduleConfigurations: false, extensions: [
-        checkout([$class: 'GitSCM', branches: [[name: "${branchName}"]], doGenerateSubmoduleConfigurations: false, extensions: [  
-            [$class: 'CleanBeforeCheckout'],
-            [$class: 'CleanCheckout'],
-            [$class: 'PruneStaleBranch'],
-            [$class: 'WipeWorkspace'],
-            [$class: 'CheckoutOption', timeout: 30],
-            [$class: 'CloneOption', timeout: 30, noTags: false],
-            [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]
-            ], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'radeonprorender', url: "${repoName}"]]])
+        checkout([$class: 'GitSCM', branches: [[name: "${branchName}"]],
+                  doGenerateSubmoduleConfigurations: false,
+                  extensions: [
+                      [$class: 'PruneStaleBranch'],
+                    [$class: 'CleanBeforeCheckout'],
+                    [$class: 'CleanCheckout'],
+                    [$class: 'WipeWorkspace'],
+                    [$class: 'CheckoutOption', timeout: 30],
+                    [$class: 'CloneOption', timeout: 30, noTags: false],
+                    [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]
+                    ],
+                  submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'radeonprorender', url: "${repoName}"]]])
     }
     else
     {
