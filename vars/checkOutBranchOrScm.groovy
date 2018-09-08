78248eb9421d020f0ac58100d72dd6743937cf49
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2018-09-08 17:43:28 +0300

    Update checkOutBranchOrScm.groovy

--- vars/checkOutBranchOrScm.groovy
+++ vars/checkOutBranchOrScm.groovy
@@ -7,7 +7,8 @@ def call(String branchName, String repoName) {
         checkout([$class: 'GitSCM', branches: [[name: "${branchName}"]], doGenerateSubmoduleConfigurations: false, extensions: [  
             [$class: 'CleanBeforeCheckout'],
             [$class: 'CleanCheckout'],
-         //   [$class: 'WipeWorkspace'],
+            [$class: 'PruneStaleBranch'],
+            [$class: 'WipeWorkspace'],
             [$class: 'CheckoutOption', timeout: 30],
             [$class: 'CloneOption', timeout: 30, noTags: false],
             [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]
