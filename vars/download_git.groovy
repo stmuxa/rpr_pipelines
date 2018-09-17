907bb61a80726ec7137d3f2d1331d59aa38cc490
Andrey Kravchenko <epozine@inbox.ru>
2018-09-17 16:56:42 +0300

    Update download_git.groovy

--- vars/download_git.groovy
+++ vars/download_git.groovy
@@ -1,7 +1,6 @@
 def call(String branchName, String repoName) {
-    if(branchName != "")
-    {
-        echo "checkout from user branch: ${branchName}; repo: ${repoName}"
+    
+        
         try_git = 0
         status = "error"
         while (status == "error" && try_git < 4) {
@@ -14,29 +13,36 @@ def call(String branchName, String repoName) {
             }
         }
             
-    }
-    else
-    {
-        echo 'checkout from scm options'
-        checkout scm
-    }
+    
 }
 
 def checkout_git(String branchName, String repoName) {
-    
-    try {
-        checkout([$class: 'GitSCM', branches: [[name: "${branchName}"]], doGenerateSubmoduleConfigurations: false, extensions: [  
-            [$class: 'CleanBeforeCheckout'],
-            [$class: 'CleanCheckout'],
-         //   [$class: 'WipeWorkspace'],
-            [$class: 'CheckoutOption', timeout: 30],
-            [$class: 'CloneOption', timeout: 30, noTags: false],
-            [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]
-            ], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'radeonprorender', url: "${repoName}"]]])
-            return "success"
-     } catch (e) {
-            print (e)
-            return "error"
-     }
-         
+    if(branchName != "")
+    {
+        try {
+            echo "checkout from user branch: ${branchName}; repo: ${repoName}"
+            checkout([$class: 'GitSCM', branches: [[name: "${branchName}"]], doGenerateSubmoduleConfigurations: false, extensions: [  
+                [$class: 'CleanBeforeCheckout'],
+                [$class: 'CleanCheckout'],
+             //   [$class: 'WipeWorkspace'],
+                [$class: 'CheckoutOption', timeout: 30],
+                [$class: 'CloneOption', timeout: 30, noTags: false],
+                [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]
+                ], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'radeonprorender', url: "${repoName}"]]])
+                return "success"
+         } catch (e) {
+                print (e)
+                return "error"
+         }
+    }
+    else
+    {   
+        try {
+        echo 'checkout from scm options'
+        checkout scm
+            } catch (e) {
+                print (e)
+                return "error"
+         }
+    }
 }
