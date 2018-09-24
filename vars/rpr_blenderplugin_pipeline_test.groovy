21b253adf888ee7c56df1481a2b997ce87abb14c
Andrey Kravchenko <epozine@inbox.ru>
2018-09-24 11:53:33 +0300

    Update rpr_blenderplugin_pipeline_test.groovy

--- vars/rpr_blenderplugin_pipeline_test.groovy
+++ vars/rpr_blenderplugin_pipeline_test.groovy
@@ -635,9 +635,15 @@ def call(String projectBranch = "", String thirdpartyBranch = "master",
 
     try
     {
+        if (1) {
         properties([[$class: 'BuildDiscarderProperty', 
                      strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '',
-                                artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10']]]);
+                                artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '2']]]);
+        } else {
+            properties([[$class: 'BuildDiscarderProperty', 
+                     strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '',
+                                artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '1']]]);
+        }
 
         
         String PRJ_NAME="RadeonProRenderBlenderPlugin"
