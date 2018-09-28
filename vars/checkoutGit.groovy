f191a966db2e606c7d154345158bf4b106c1ea78
Andrey Kravchenko <epozine@inbox.ru>
2018-09-28 15:40:18 +0300

    Update checkoutGit.groovy

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -30,7 +30,7 @@ def call(String branchName, String repoName) {
                 [$class: 'CheckoutOption', timeout: 30],	
                 [$class: 'CloneOption', timeout: 30, noTags: false],	
                 [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]	
-                ], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'radeonprorender', url: "${repoName}"]]])	
+                ], submoduleCfg: [], userRemoteConfigs: [[url: "${repoName}"]]])	
                 return "success"	
          } catch (e) {	
                 print (e)	
