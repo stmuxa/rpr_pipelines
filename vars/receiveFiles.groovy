aeed7dfbb45b6d8037e1dafc4bcad670d1b38c74
Andrey Kravchenko <epozine@inbox.ru>
2018-10-06 18:26:21 +0300

    Update receiveFiles.groovy

--- vars/receiveFiles.groovy
+++ vars/receiveFiles.groovy
@@ -13,7 +13,7 @@ def call(String remote, String local)
             %CIS_TOOLS%\\receiveFiles.bat ${remote} /mnt/c/ReferenceImages/${local}
         """
         bat """
-            xcopy /E /S /Y "%REF_IMAGES%\\${remote}" ".\\Work\\Baseline"
+            xcopy /E /S /Y "%REF_IMAGES%\\${remote}" "Work\\Baseline"
         """
     }
 }
