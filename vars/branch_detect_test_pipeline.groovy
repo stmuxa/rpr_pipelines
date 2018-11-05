f29d0af2d136b7bb98bab70cf202b4cdc8168d44
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2018-11-05 11:35:13 +0300

    Update branch_detect_test_pipeline.groovy

--- vars/branch_detect_test_pipeline.groovy
+++ vars/branch_detect_test_pipeline.groovy
@@ -3,23 +3,25 @@ def call()
     stage("init")
     {
         echo "start"
-        try
-        {
-            echo "go"
-            error 'stage failed'
-        }
-        catch(e)
-        {
-            println(e.getMessage())
-        }
         properties([parameters([string(defaultValue: 'PR', description: 'Test suite', name: 'Tests', trim: false)]), [$class: 'JobPropertyImpl', throttle: [count: 6, durationName: 'hour', userBoost: true]]])
     }
-    stage("second")
-    {
-        
-        sleep(10)
-                properties([[$class: 'BuildDiscarderProperty', strategy: 	
-                         [$class: 'LogRotator', artifactDaysToKeepStr: '', 	
-                          artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10']]]);
+    try{
+        stage("second")
+        {
+            sleep(10)
+            properties([[$class: 'BuildDiscarderProperty', strategy: 	
+                     [$class: 'LogRotator', artifactDaysToKeepStr: '', 	
+                      artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10']]]);
+            error "f"
+        }
     }
+    catch(e)
+    {}
+    finally
+    {
+        stage('th')
+        {
+            sleep(1)
+        }
+    }   
 }
