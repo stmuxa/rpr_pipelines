5b5987c93df171090647b361799d14a5aae95a46
Andrey Kravchenko <epozine@inbox.ru>
2018-11-06 13:57:20 +0300

    Update render_scene_job_testing.groovy

--- vars/render_scene_job_testing.groovy
+++ vars/render_scene_job_testing.groovy
@@ -423,43 +423,6 @@ def install_plugin(osName, tool, plugin) {
 	}
 }
 
-def executePlatform(String osName, String gpuNames, Map options) {
-	def retNode =  {   
-		try {			
-			if(gpuNames) {
-				def testTasks = [:]
-				gpuNames.split(',').each() {
-					String asicName = it
-					echo "Scheduling Render ${osName}:${asicName}"
-					testTasks["Test-${it}-${osName}"] = {
-						node("${osName} && RenderService && gpu${asicName}")
-						{
-							stage("Test-${asicName}-${osName}")
-							{
-								ws("WS/${options.PRJ_NAME}_Test") {
-									Map newOptions = options.clone()
-									newOptions['testResultsName'] = "testResult-${asicName}-${osName}"
-									executeRender(osName, newOptions)
-								}
-							}
-						}
-					}
-				}
-			parallel testTasks
-			}
-		}
-		catch (e) {
-			println(e.toString());
-			println(e.getMessage());
-			println(e.getStackTrace());        
-			currentBuild.result = "FAILED"
-			echo "FAILED by executePlatform"
-			throw e
-		}
-	}
-	return retNode
-}
-
 def main(String platforms, Map options) {
 		
 	try {
@@ -470,8 +433,6 @@ def main(String platforms, Map options) {
 			options['PRJ_PATH']="${PRJ_PATH}"
 			options['JOB_PATH']="${JOB_PATH}"
 
-			def platformList = [];
-			def testResultList = [];
 			def tasks = [:]
 			
 			if (platforms.split(';').size() > 1 && options['startFrame'] != options['endFrame']) {
@@ -482,24 +443,25 @@ def main(String platforms, Map options) {
 
 				List tokens = it.tokenize(':')
 				String osName = tokens.get(0)
-				String gpuNames = ""
-				if (tokens.size() > 1)
-				{
-					gpuNames = tokens.get(1)
-				}
-
-				platformList << osName
-				if(gpuNames)
-				{
-					gpuNames.split(',').each()
+				String gpuNames = tokens.get(1)
+								
+				String asicName = it
+				echo "Scheduling Render ${osName}:${asicName}"
+				testTasks["Test-${it}-${osName}"] = {
+					node("${osName} && RenderService && gpu${asicName}")
 					{
-							String asicName = it
-							testResultList << "testResult-${asicName}-${osName}"
+						stage("Test-${asicName}-${osName}")
+						{
+							ws("WS/${options.PRJ_NAME}_Test") {
+								Map newOptions = options.clone()
+								newOptions['testResultsName'] = "testResult-${asicName}-${osName}"
+								executeRender(osName, newOptions)
+							}
+						}
 					}
 				}
-				tasks[osName + "-" + gpuNames]=executePlatform(osName, gpuNames, options)
 			}
-			parallel tasks
+			parallel testTasks
 		}     
 	}	
 	catch (e) {
@@ -508,17 +470,6 @@ def main(String platforms, Map options) {
 		println(e.getStackTrace());
 		currentBuild.result = "FAILED"
 		throw e
-	}
-	finally {
-		echo "enableNotifications = ${options.enableNotifications}"
-		if("${options.enableNotifications}" == "true")
-		{
-				sendBuildStatusNotification(currentBuild.result, 
-					options.get('slackChannel', ''), 
-					options.get('slackBaseUrl', ''),
-					options.get('slackTocken', ''),
-					options.CBR)
-		}
 	}
 }
 	
