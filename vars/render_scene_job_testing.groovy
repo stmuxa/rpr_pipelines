77c898c0a664ce92d9856d5a2b9a592e177715f4
Andrey Kravchenko <epozine@inbox.ru>
2018-11-07 14:46:27 +0300

    Update render_scene_job_testing.groovy

--- vars/render_scene_job_testing.groovy
+++ vars/render_scene_job_testing.groovy
@@ -59,9 +59,7 @@ def executeRender(osName, gpuName, Map options, uniqueID) {
 							bat """
 							"..\\..\\cis_tools\\7-Zip\\7z.exe" x "${scene_zip}"
 							"""
-							String scene=python3("find_scene_blender.py --folder .").split('\r\n')[2].trim()
-							options['sceneName'] = scene.split['.blend'][0]
-							echo options['sceneName']
+							options['sceneName'] = python3("find_scene_blender.py --folder .").split('\r\n')[2].trim()
 						}
 						bat """
 						copy "..\\..\\cis_tools\\RenderSceneJob\\find_scene_blender.py" "."
@@ -71,6 +69,8 @@ def executeRender(osName, gpuName, Map options, uniqueID) {
 						String scene=python3("find_scene_blender.py --folder .").split('\r\n')[2].trim()
 						echo "Find scene: ${scene}"
 						echo "Launching render"
+						options['sceneName'] = options['sceneName'].split['.blend'][0]
+						echo options['sceneName']
 						python3("launch_blender.py --tool ${version} --render_device_type ${options.RenderDevice} --pass_limit ${options.PassLimit} --scene \"${scene}\" --startFrame ${options.startFrame} --endFrame ${options.endFrame} --sceneName ${options.sceneName}")
 						echo "Done"
 						break;
