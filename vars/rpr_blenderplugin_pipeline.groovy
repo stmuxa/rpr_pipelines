d0b85a9f14a1c8786809445b226d4ee5f6cdf9ce
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2018-11-26 17:11:47 +0300

    Update rpr_blenderplugin_pipeline.groovy

--- vars/rpr_blenderplugin_pipeline.groovy
+++ vars/rpr_blenderplugin_pipeline.groovy
@@ -195,13 +195,13 @@ def executeTests(String osName, String asicName, Map options)
         if(isUnix())
         {
             sh """
-            ${CIS_TOOLS}/receiveFiles.sh ${options.PRJ_ROOT}/${options.PRJ_NAME}/BlenderAssets/* ${CIS_TOOLS}/../TestResources/BlenderAssets
+            ${CIS_TOOLS}/receiveFilesSync.sh ${options.PRJ_ROOT}/${options.PRJ_NAME}/BlenderAssets/* ${CIS_TOOLS}/../TestResources/BlenderAssets
             """
         }
         else
         {
             bat """
-            %CIS_TOOLS%\\receiveFiles.bat ${options.PRJ_ROOT}/${options.PRJ_NAME}/BlenderAssets/* /mnt/c/TestResources/BlenderAssets
+            %CIS_TOOLS%\\receiveFilesSync.bat ${options.PRJ_ROOT}/${options.PRJ_NAME}/BlenderAssets/* /mnt/c/TestResources/BlenderAssets
             """
         }
 
