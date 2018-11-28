50872e55526b6fe096596fd1c256b2c29176a31d
Andrey Alekseevich <magister.demens@gmail.com>
2018-11-28 21:43:49 +0300

    multipipeline split implementation

--- vars/outputEnvironmentInfo.groovy
+++ vars/outputEnvironmentInfo.groovy
@@ -1,14 +1,17 @@
 
-def call(String osName)
+def call(String osName, String logName="")
 {
+    // logName null - use STAGE_NAME
+    logName =  logName ?: "${STAGE_NAME}"
+
     if(osName == 'Windows')
     {
-         bat "HOSTNAME  >  ${STAGE_NAME}.log"
-         bat "set       >> ${STAGE_NAME}.log"
+         bat "HOSTNAME  >  ${logName}.log"
+         bat "set       >> ${logName}.log"
     }
     else
     {
-         sh "uname -a   >  ${STAGE_NAME}.log"
-         sh "env        >> ${STAGE_NAME}.log"
+         sh "uname -a   >  ${logName}.log"
+         sh "env        >> ${logName}.log"
     }
 }
