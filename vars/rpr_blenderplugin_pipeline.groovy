fb5872b60874651f90fe9809323a8fc9905a1126
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2018-12-03 18:28:26 +0300

    Update rpr_blenderplugin_pipeline.groovy

--- vars/rpr_blenderplugin_pipeline.groovy
+++ vars/rpr_blenderplugin_pipeline.groovy
@@ -713,7 +713,7 @@ def executeDeploy(Map options, List platformList, List testResultList)
 
 def call(String projectBranch = "", String thirdpartyBranch = "master", 
          String packageBranch = "master", String testsBranch = "master",
-         String platforms = 'Windows:AMD_RXVEGA,AMD_WX9100,AMD_WX7100,NVIDIA_GF1080TI;Ubuntu;OSX:RadeonPro560', 
+         String platforms = 'Windows:AMD_RXVEGA,AMD_WX9100,AMD_WX7100,NVIDIA_GF1080TI;Ubuntu:AMD_WX7100;OSX:RadeonPro560', 
          Boolean updateRefs = false, Boolean enableNotifications = true,
          Boolean incrementVersion = true,
          Boolean skipBuild = false,
