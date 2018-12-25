4d2910d2270a2155134ef3a5f114d94b5169ee41
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2018-12-25 12:56:02 +0300

    Create rpr_ai2rpr_pipeline.groovy

---
+++ vars/rpr_ai2rpr_pipeline.groovy
@@ -0,0 +1,326 @@
+def executeGenTestRefCommand(String osName, Map options)
+{
+    executeTestCommand(osName, options)
+    
+    dir('scripts')
+    {
+        switch(osName)
+        {
+            case 'Windows':
+                bat """
+                make_results_baseline.bat
+                """
+                break;
+            case 'OSX':
+                sh """
+                ./make_results_baseline.sh
+                """
+                break;
+            default:
+                sh """
+                ./make_results_baseline.sh
+                """
+        }
+    }
+}
+
+def executeTestCommand(String osName, Map options)
+{
+    switch(osName)
+    {
+    case 'Windows':        
+        dir('scripts')
+        {
+            bat """
+            run.bat ${options.testsPackage} \"${options.tests}\">> ../${STAGE_NAME}.log  2>&1
+            """
+        }
+        break;
+    case 'OSX':
+        sh """
+        echo 'sample image' > ./OutputImages/sample_image.txt
+        """
+        break;
+    default:
+        sh """
+        echo 'sample image' > ./OutputImages/sample_image.txt
+        """
+    }
+}
+
+def executeTests(String osName, String asicName, Map options)
+{
+    try {
+        checkoutGit(options['testsBranch'], 'git@github.com:luxteam/jobs_test_ai2rpr.git')
+        
+        // update assets
+        if(isUnix())
+        {
+            sh """
+            ${CIS_TOOLS}/receiveFilesSync.sh /rpr-tools/Arnold2RPR/ArnoldAssets/ ${CIS_TOOLS}/../TestResources/ArnoldAssets
+            """
+        }
+        else
+        {
+            bat """
+            %CIS_TOOLS%\\receiveFilesSync.bat /rpr-tools/Arnold2RPR/ArnoldAssets/ /mnt/c/TestResources/ArnoldAssets
+            """
+        }
+        
+        String REF_PATH_PROFILE="${options.REF_PATH}/${asicName}-${osName}"
+        String JOB_PATH_PROFILE="${options.JOB_PATH}/${asicName}-${osName}"
+        
+        options.REF_PATH_PROFILE = REF_PATH_PROFILE
+        
+        outputEnvironmentInfo(osName)
+        
+        if(options['updateRefs'])
+        {
+            executeGenTestRefCommand(osName, options)
+            sendFiles('./Work/Baseline/', REF_PATH_PROFILE)
+        }
+        else
+        {
+            try{
+                receiveFiles("${REF_PATH_PROFILE}/*", './Work/Baseline/')
+            }
+            catch (e) {
+            }
+            executeTestCommand(osName, options)   
+        }
+    }
+    catch (e) {
+        println(e.toString());
+        println(e.getMessage());
+        currentBuild.result = "FAILED"
+        throw e
+    }
+    finally {
+        archiveArtifacts "*.log"
+        echo "Stashing test results to : ${options.testResultsName}"
+        dir('Work')
+        {
+            stash includes: '**/*', name: "${options.testResultsName}", allowEmpty: true
+            
+            def sessionReport = readJSON file: 'Results/ai2rpr/session_report.json'
+            sessionReport.results.each{ testName, testConfigs ->
+                testConfigs.each{ key, value ->
+                    if ( value.render_duration == 0)
+                    {
+                        error "Crashed tests detected"
+                    }
+                }
+            }
+        }
+    }
+}
+
+def executeBuildWindows(Map options)
+{
+
+}
+
+def executeBuildOSX(Map options)
+{
+
+}
+
+def executeBuildLinux(Map options)
+{
+    
+}
+
+def executeBuild(String osName, Map options)
+{
+    try {        
+        dir('RS2RPRConvertTool')
+        {
+            checkoutGit(options['projectBranch'], 'https://github.com/luxteam/Arnold2RPRConvertTool.git')
+        }
+        
+        outputEnvironmentInfo(osName)
+
+        switch(osName)
+        {
+        case 'Windows': 
+            executeBuildWindows(options); 
+            break;
+        case 'OSX':
+            executeBuildOSX(options);
+            break;
+        default: 
+            executeBuildLinux(options);
+        }
+        
+        //stash includes: 'Bin/**/*', name: "app${osName}"
+    }
+    catch (e) {
+        currentBuild.result = "FAILED"
+        throw e
+    }
+    finally {
+        archiveArtifacts "*.log"
+    }                        
+}
+
+def executePreBuild(Map options)
+{
+    //properties([])
+
+    dir('Arnold2RPRConvertTool')
+    {
+        checkOutBranchOrScm(options['projectBranch'], 'git@github.com:luxteam/Arnold2RPRConvertTool.git')
+
+        AUTHOR_NAME = bat (
+                script: "git show -s --format=%%an HEAD ",
+                returnStdout: true
+                ).split('\r\n')[2].trim()
+
+        echo "The last commit was written by ${AUTHOR_NAME}."
+        options.AUTHOR_NAME = AUTHOR_NAME
+        
+        commitMessage = bat ( script: "git log --format=%%B -n 1", returnStdout: true )
+        echo "Commit message: ${commitMessage}"
+        
+        options.commitMessage = commitMessage.split('\r\n')[2].trim()
+        echo "Opt.: ${options.commitMessage}"
+        options['commitSHA'] = bat(script: "git log --format=%%H -1 ", returnStdout: true).split('\r\n')[2].trim()
+                
+    }
+}
+
+def executeDeploy(Map options, List platformList, List testResultList)
+{
+    try { 
+        if(options['executeTests'] && testResultList)
+        {
+            checkoutGit(options['testsBranch'], 'git@github.com:luxteam/jobs_test_ai2rpr.git')
+
+            dir("summaryTestResults")
+            {
+                testResultList.each()
+                {
+                    dir("$it".replace("testResult-", ""))
+                    {
+                        try
+                        {
+                            unstash "$it"
+                        }catch(e)
+                        {
+                            echo "Can't unstash ${it}"
+                            println(e.toString());
+                            println(e.getMessage());
+                        }
+                    
+                    }
+                }
+            }
+
+            dir("jobs_launcher")
+            {
+                String branchName = env.BRANCH_NAME ?: env.Branch
+
+                try {
+                    withEnv(["JOB_STARTED_TIME=${options.JOB_STARTED_TIME}"])
+                    {
+                        bat """
+                        build_reports.bat ..\\summaryTestResults Arnold2RPR ${options.commitSHA} ${branchName} \"${escapeCharsByUnicode(options.commitMessage)}\"
+                        """
+                    }
+                } catch(e) {
+                    println("ERROR during report building")
+                    println(e.toString())
+                    println(e.getMessage())
+                }
+
+                try
+                {
+                    bat "get_status.bat ..\\summaryTestResults"
+                }
+                catch(e)
+                {
+                    println("ERROR during slack status generation")
+                    println(e.toString())
+                    println(e.getMessage())   
+                }
+
+            }
+            
+            try
+            {
+                def summaryReport = readJSON file: 'summaryTestResults/summary_status.json'
+                if (summaryReport.failed > 0 || summaryReport.error > 0)
+                {
+                    println("Some tests failed")
+                    currentBuild.result="UNSTABLE"
+                }
+            }
+            catch(e)
+            {
+                println(e.toString())
+                println(e.getMessage())
+                println("CAN'T GET TESTS STATUS")
+                currentBuild.result="UNSTABLE"
+            }
+            
+            try
+            {
+                options.testsStatus = readFile("summaryTestResults/slack_status.json")
+            }
+            catch(e)
+            {
+                println(e.toString())
+                println(e.getMessage())
+                options.testsStatus = ""
+            }   
+
+            publishHTML([allowMissing: false, 
+                         alwaysLinkToLastBuild: false, 
+                         keepAll: true, 
+                         reportDir: 'summaryTestResults', 
+                         reportFiles: 'summary_report.html',
+                         reportName: 'Test Report',
+                         reportTitles: 'Summary Report'])
+        }
+    }
+    catch (e) {
+        currentBuild.result = "FAILED"
+        println(e.toString());
+        println(e.getMessage());
+        throw e
+    }
+    finally
+    {}   
+}
+
+def call(String projectBranch = "",
+         String testsBranch = "master",
+         String platforms = 'Windows:NVIDIA_GF1080TI', 
+         Boolean updateRefs = false,
+         Boolean enableNotifications = true,
+         String testsPackage = "",
+         String tests = "") {
+    try
+    {
+        String PRJ_NAME="Arnold2RPRConvertTool"
+        String PRJ_ROOT="rpr-tools"
+
+        multiplatform_pipeline(platforms, this.&executePreBuild, this.&executeBuild, this.&executeTests, this.&executeDeploy, 
+                               [projectBranch:projectBranch, 
+                                testsBranch:testsBranch, 
+                                updateRefs:updateRefs, 
+                                enableNotifications:enableNotifications,
+                                executeTests:true,
+                                PRJ_NAME:PRJ_NAME,
+                                PRJ_ROOT:PRJ_ROOT,
+                                testsPackage:testsPackage,
+                                tests:tests,
+                                reportName:'Test_20Report'])
+    }
+    catch(e) {
+        currentBuild.result = "FAILED"
+        println(e.toString());
+        println(e.getMessage());
+        throw e
+    }
+}
