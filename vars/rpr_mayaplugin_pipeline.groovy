1b05f3c24aecbb6ac8641a9671e1fa72727dbec3
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2018-12-26 16:00:48 +0300

    Update rpr_mayaplugin_pipeline.groovy

--- vars/rpr_mayaplugin_pipeline.groovy
+++ vars/rpr_mayaplugin_pipeline.groovy
@@ -481,21 +481,32 @@ def executeDeploy(Map options, List platformList, List testResultList)
 
             dir("jobs_launcher")
             {
-                if(options.projectBranch != "") {
-                    options.branchName = options.projectBranch
-                } else {
-                    options.branchName = env.BRANCH_NAME
+                String branchName = env.BRANCH_NAME ?: env.Branch
+
+                try
+                {
+                    withEnv(["JOB_STARTED_TIME=${options.JOB_STARTED_TIME}"])
+                    {
+                        bat """
+                        build_reports.bat ..\\summaryTestResults "${escapeCharsByUnicode('Maya 2017')}" ${options.commitSHA} ${branchName} \"${escapeCharsByUnicode(options.commitMessage)}\"
+                        """
+                    }
+                } catch(e) {
+                    println("ERROR during report building")
+                    println(e.toString())
+                    println(e.getMessage())
                 }
-                if(options.incrementVersion) {
-                    options.branchName = "master"
+
+                try
+                {
+                    bat "get_status.bat ..\\summaryTestResults"
                 }
-                
-                options.commitMessage = options.commitMessage.replace("'", "")
-                options.commitMessage = options.commitMessage.replace('"', '')
-                bat """
-                build_reports.bat ..\\summaryTestResults Maya2017 ${options.commitSHA} ${options.branchName} \\"${options.commitMessage}\\"
-                """
-                bat "get_status.bat ..\\summaryTestResults"
+                catch(e)
+                {
+                    println("ERROR during slack status generation")
+                    println(e.toString())
+                    println(e.getMessage())   
+                }
             }
             
             try
