7d2632bb9890e685a5f21852d5dc99adb230951e
Andrey Alekseevich <magister.demens@gmail.com>
2019-02-26 10:30:24 +0300

    init

--- vars/checkOutBranchOrScm.groovy
+++ vars/checkOutBranchOrScm.groovy
@@ -1,22 +1,26 @@
 
-def call(String branchName, String repoName) {
+def call(String branchName, String repoName, Boolean polling=false, Boolean changelog=false, String SCMname="") {
+    polling = polling ?: true
+    changelog = changelog ?: true
+
+    // TODO: implement retray - WipeWorkspace
     if(branchName != "")
     {
         echo "checkout from user branch: ${branchName}; repo: ${repoName}"
-        //checkout([$class: 'GitSCM', branches: [[name: "*/${branchName}"]], doGenerateSubmoduleConfigurations: false, extensions: [
-        checkout([$class: 'GitSCM', branches: [[name: "${branchName}"]], doGenerateSubmoduleConfigurations: false,
+        checkout changelog: changelog, poll: polling, csm: 
+        [$class: 'GitSCM', branches: [[name: "${branchName}"]], doGenerateSubmoduleConfigurations: false,
             extensions: [
+                [$class: 'ScmName', name: "${SCMname}"],
                 [$class: 'PruneStaleBranch'],
                 [$class: 'CleanBeforeCheckout'],
                 [$class: 'CleanCheckout'],
-                // [$class: 'WipeWorkspace'],
                 [$class: 'CheckoutOption', timeout: 30],
                 [$class: 'CloneOption', timeout: 30, noTags: false],
                 [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]
             ],
             submoduleCfg: [],
             userRemoteConfigs: [[credentialsId: 'radeonprorender', url: "${repoName}"]]
-        ])
+        ]
     }
     else
     {
