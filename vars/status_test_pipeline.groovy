3d8f378a8ef3c58803e315d0dce89fea35a87890
Andrey Alekseevich <magister.demens@gmail.com>
2019-03-11 09:07:27 +0300

    init

--- vars/status_test_pipeline.groovy
+++ vars/status_test_pipeline.groovy
@@ -1,47 +1,173 @@
-def call() {
-    stage("build") {
-        echo "build"
-        echo "${env}"
+def executeGenTestRefCommand(String osName)
+{
+    switch(osName)
+    {
+    case 'Windows':
+        bat """
+        echo 'sample image' > .\\ReferenceImages\\sample_image.txt
+        """
+        break;
+    case 'OSX':
+        sh """
+        echo 'sample image' > ./ReferenceImages/sample_image.txt
+        """
+        break;
+    default:
+        sh """
+        echo 'sample image' > ./ReferenceImages/sample_image.txt
+        """
+    }
+}
+
+def executeTestCommand(String osName)
+{
+    switch(osName)
+    {
+    case 'Windows':
+        bat """
+        echo 'sample image' > ${STAGE_NAME}.${options.RENDER_QUALITY}.log
+        """
+        break;
+    case 'OSX':
+        sh """
+        echo 'sample image' > ${STAGE_NAME}.${options.RENDER_QUALITY}.log
+        """
+        break;
+    default:
+        sh """
+        echo 'sample image' > ${STAGE_NAME}.${options.RENDER_QUALITY}.log
+        """
+    }
+}
+
+def executeTestsCustomQuality(String osName, String asicName, Map options)
+{
+    cleanWs()
+    executeTestCommand(osName, options)
+    archiveArtifacts "*.log"
+}
+
+def executeTests(String osName, String asicName, Map options)
+{
+    options['testsQuality'].split(",").each() {
+        options['RENDER_QUALITY'] = "${it}"
+        String status = "successful"
+        try {
+            executeTestsCustomQuality(osName, asicName, options)
+        } catch(e) {
+            status = "failed"
+            println("Exception during [${options.RENDER_QUALITY}] quality tests execution")
+        }
+        finally {
+            pullRequest.createStatus(status,
+                "[TEST] ${osName}-${asicName}-${it}",
+                "Testing finished",
+                "${env.BUILD_URL}/artifact/${STAGE_NAME}.${options.RENDER_QUALITY}.log")
+        }
+    }
+}
+
+def executeBuildWindows()
+{
+    bat """
+    HOSTNAME > ${STAGE_NAME}.log
+
+    """
+}
+
+def executeBuildOSX()
+{
+    sh """
+    uname -a > ${STAGE_NAME}.log
+
+    """
+}
+
+def executeBuildLinux()
+{
+    sh """
+    uname -a > ${STAGE_NAME}.log
+
+    """
+}
+
+def executePreBuild(Map options)
+{
+    checkOutBranchOrScm(options['projectBranch'], options['projectRepo'])
+
+    if(env.CHANGE_ID)
+    {
+        options['platforms'].split(';').each()
+        { platform ->
+            List tokens = platform.tokenize(':')
+            String osName = tokens.get(0)
+            // Statuses for builds
+            pullRequest.createStatus("pending", "[BUILD] ${osName}", "Init", "${env.BUILD_URL}")
+            if (tokens.size() > 1)
+            {
+                gpuNames = tokens.get(1)
+                gpuNames.split(',').each()
+                { gpuName ->
+                    options['testsQuality'].split(",").each()
+                    { testQuality ->
+                        // Statuses for tests
+                        pullRequest.createStatus("pending",
+                            "[TEST] ${osName}-${gpuName}-${testQuality}",
+                            "Init",
+                            "${env.BUILD_URL}")
+                    }
+                }
+            }
+        }
+    }
+}
+
+def executeBuild(String osName, Map options)
+{
+    try {
+        checkOutBranchOrScm(options['projectBranch'], 'https://github.com/luxteam/MultiplatformSampleProject.git')
+        outputEnvironmentInfo(osName)
+
+        switch(osName)
+        {
+        case 'Windows': 
+            executeBuildWindows(); 
+            break;
+        case 'OSX':
+            executeBuildOSX();
+            break;
+        default: 
+            executeBuildLinux();
+        }
         
-        if (env.CHANGE_ID) {
-            
-            for (commit in pullRequest.commits) {
-              for (status  in commit.statuses) {
-                 echo "Commit: ${commit.sha}, State: ${status.state}, Context: ${status.context}, URL: ${status.targetUrl}"
-              }
-            }
-            
-            echo "----statuses"
-            
-            for (status in pullRequest.statuses) {
-                echo "Commit: ${pullRequest.head}, State: ${status.state}, Context: ${status.context}, URL: ${status.targetUrl}"
-            }
-            
-            pullRequest.addLabel("Build Success")
-            pullRequest.createStatus("success", "context", "description", "https://rpr.cis.luxoft.com/targetUrl")
-        }
-        else {
-            node("master") {
-            //commit.createStatus("success", "context", "description", "https://rpr.cis.luxoft.com/targetUrl")
-            checkOutBranchOrScm("", "")
-                step([$class: 'GitHubCommitStatusSetter',
-                  commitShaSource: [$class: 'ManuallyEnteredShaSource', sha: 'dfb2604ddf563b13265e3a2a4c9b66f1c9e7764c'],
-                  contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: 'context'],
-                  reposSource: [$class: 'ManuallyEnteredRepositorySource', url: 'https://github.com/luxteam/statustest'],
-                  statusResultSource: [$class: 'ConditionalStatusResultSource',
-                                       results: [[$class: 'AnyBuildResult', message: 'message', state: 'PENDING']]
-                                      ]
-                 ])
-            
-            }
-        }
+        //stash includes: 'Bin/**/*', name: "app${osName}"
     }
+    catch (e) {
+        currentBuild.result = "FAILED"
+        throw e
+    }
+    finally {
+        archiveArtifacts "*.log"
+        pullRequest.createStatus(currentBuild.result ?: "successful",
+            "[BUILD] ${osName}", "Build finished", "${env.BUILD_URL}/artifact/${STAGE_NAME}.log")
+    }                        
 
-    stage("test") {
-        echo "test"
-    }
+}
 
-    stage("deploy") {
-        echo "deploy"
-    }
+def executeDeploy(Map options)
+{
+}
+
+def call(String projectBranch = "", 
+         String platforms = 'Windows:AMD_RXVEGA,AMD_WX9100,AMD_WX7100,NVIDIA_GF1080TI;OSX:RadeonPro560;Ubuntu;AMD_WX7100', 
+         String testsQuality = "low,medium",
+         Boolean updateRefs = false,
+         Boolean enableNotifications = false) {
+    
+    multiplatform_pipeline(platforms, this.&executePreBuild, this.&executeBuild, this.&executeTests, null,
+                           [projectBranch:projectBranch,
+                           updateRefs:updateRefs, 
+                           enableNotifications:enableNotifications,
+                           platforms:platforms,
+                           options:testsQuality])
 }
