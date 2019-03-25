6067da952b135c4da77d3c6038a4523d67df05c1
Andrey Kravchenko <epozine@inbox.ru>
2019-03-25 15:22:43 +0300

    Update render_scene_job_testing.groovy

--- vars/render_scene_job_testing.groovy
+++ vars/render_scene_job_testing.groovy
@@ -141,7 +141,7 @@ def executeRender(osName, gpuName, Map options, uniqueID) {
 				    options['sceneName'] = python3("find_scene_blender.py --folder .").split('\\\\')[-1]
 				}
 				
-				String scene=python3("find_scene_blender.py --folder .").split('\r\n')[2].trim()
+				String scene=python3("find_scene_blender.py --folder .").trim()
 				echo "Find scene: ${scene}"
 				echo "Launching render"
 				python3("..\\..\\cis_tools\\${options.cis_tools}\\send_status.py --django_ip \"${options.django_url}/\" --tool ${tool} --status \"Rendering scene\" --id ${id}")
