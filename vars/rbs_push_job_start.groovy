f5095db4c388c7af98eddc2290f35cb81d1fc743
Andrey Alekseevich <magister.demens@gmail.com>
2019-04-09 22:18:17 +0300

    fixed missed quotes

--- vars/rbs_push_job_start.groovy
+++ vars/rbs_push_job_start.groovy
@@ -4,10 +4,12 @@ def call(String url, String token, String branchTag, String toolName, Map option
                             "primary_time": "${options.JOB_STARTED_TIME}",
                             "branch": "${branchTag}",
                             "tool": "${toolName}",
-                            "groups": ["${options.testsList.join(",")}"],
+                            "groups": ["${options.testsList.join('","')}"],
                             "count_test_machine" : ${options.gpusCount}}"""
 
+    requestData = requestData.replaceAll("\n", "")
+
     def response = httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', customHeaders: [[name: 'Authorization', value: "Token ${token}"]], httpMode: 'POST', ignoreSslErrors: true, url: "${url}?data=${java.net.URLEncoder.encode(requestData, 'UTF-8')}", validResponseCodes: '200'
-    
+
     echo "Status: ${response.status}\nContent: ${response.content}"
 }
