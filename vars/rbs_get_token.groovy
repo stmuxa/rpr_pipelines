b2dd1cfe26ac0cfecdacd265ea2ab52350a05084
Andrey Alekseevich <magister.demens@gmail.com>
2019-04-09 19:10:36 +0300

    return value fix

--- vars/rbs_get_token.groovy
+++ vars/rbs_get_token.groovy
@@ -1,5 +1,6 @@
 def call(String url, String credentialId)
 {
     def response = httpRequest consoleLogResponseBody: true, httpMode: 'POST', authentication: "${credentialId}",  url: "${url}", validResponseCodes: '200'
-    return readJSON text: "${response.content}"
+    def token = readJSON text: "${response.content}"
+    return "${token}"
 }
