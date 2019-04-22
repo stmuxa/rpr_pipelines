d3253a43bed04f209a11d2218154e1db12006bbf
Andrey Alekseevich <magister.demens@gmail.com>
2019-04-22 17:37:31 +0300

    renamed pipeline for rs2rpr maya

--- vars/rpr_rs2rpr_maya_pipeline.groovy
+++ vars/rpr_rs2rpr_maya_pipeline.groovy
@@ -1,7 +1,7 @@
 def executeGenTestRefCommand(String osName, Map options)
 {
     executeTestCommand(osName, options)
-    
+
     dir('scripts')
     {
         switch(osName)
@@ -28,7 +28,7 @@ def executeTestCommand(String osName, Map options)
 {
     switch(osName)
     {
-    case 'Windows':        
+    case 'Windows':
         dir('scripts')
         {
             bat """
@@ -75,7 +75,7 @@ def installPlugins(String osName, Map options)
         // install new plugin
         dir('temp/install_plugin')
         {
-            receiveFiles("/bin_storage/r18q4/radeonprorenderformaya.msi", "/mnt/c/TestResources/")            
+            receiveFiles("/bin_storage/r18q4/radeonprorenderformaya.msi", "/mnt/c/TestResources/")
 
             bat """
             msiexec /i "C:\\TestResources\\radeonprorenderformaya.msi" /quiet /qn PIDKEY=${env.RPR_PLUGIN_KEY} /L+ie ../../${options.stageName}.install.log /norestart
@@ -102,7 +102,7 @@ def installPlugins(String osName, Map options)
                 echo "Error while deinstall plugin"
                 echo e.toString()
             }
-            
+
             receiveFiles("/bin_storage/RadeonProMaterialLibrary.msi", "/mnt/c/TestResources/")
             bat """
             msiexec /i "C:\\TestResources\\RadeonProMaterialLibrary.msi" /quiet /L+ie ${STAGE_NAME}.matlib.install.log /norestart
@@ -135,29 +135,29 @@ def executeTests(String osName, String asicName, Map options)
             }
             unstash "convertionScriptPython"
         }
-        
+
         // update assets
         if(isUnix())
         {
             sh """
-            ${CIS_TOOLS}/receiveFilesSync.sh /rpr-tools/Redshift2RPR/RedshiftAssets/ ${CIS_TOOLS}/../TestResources/RedshiftAssets
+            ${CIS_TOOLS}/receiveFilesSync.sh /${options.PRJ_PATH}/RedshiftAssets/ ${CIS_TOOLS}/../TestResources/RedshiftAssets
             """
         }
         else
         {
             bat """
-            %CIS_TOOLS%\\receiveFilesSync.bat /rpr-tools/Redshift2RPR/RedshiftAssets/ /mnt/c/TestResources/RedshiftAssets
+            %CIS_TOOLS%\\receiveFilesSync.bat /${options.PRJ_PATH}/RedshiftAssets/ /mnt/c/TestResources/RedshiftAssets
             """
         }
-        
+
         String REF_PATH_PROFILE="${options.REF_PATH}/${asicName}-${osName}"
         String JOB_PATH_PROFILE="${options.JOB_PATH}/${asicName}-${osName}"
-        
+
         String REF_PATH_PROFILE_OR="${options.REF_PATH}/Redshift-${osName}"
         String JOB_PATH_PROFILE_OR="${options.JOB_PATH}/Redshift-${osName}"
-        
+
         outputEnvironmentInfo(osName)
-       
+
         if(options['updateORRefs'])
         {
             dir('scripts')
@@ -189,7 +189,7 @@ def executeTests(String osName, String asicName, Map options)
                     receiveFiles("${REF_PATH_PROFILE_OR}/${it}", './Work/Baseline/')
                 }
             } catch (e) {}
-            executeTestCommand(osName, options)   
+            executeTestCommand(osName, options)
         }
     }
     catch (e) {
@@ -204,7 +204,7 @@ def executeTests(String osName, String asicName, Map options)
         dir('Work')
         {
             stash includes: '**/*', name: "${options.testResultsName}", allowEmpty: true
-            
+
             def sessionReport = readJSON file: 'Results/rs2rpr/session_report.json'
             if (sessionReport.summary.total == 0) {
                 options.failureMessage = "Noone test was finished for: ${asicName}-${osName}"
@@ -234,32 +234,32 @@ def executeBuildOSX(Map options)
 
 def executeBuildLinux(Map options)
 {
-    
+
 }
 
 def executeBuild(String osName, Map options)
 {
-    try {        
+    try {
         // dir('RS2RPRConvertTool')
         // {
         //     checkoutGit(options['projectBranch'], 'https://github.com/luxteam/RS2RPRConvertTool.git')
         //     stash includes: "convertRS2RPR.mel", name: "convertionScript"
         // }
-        
+
         outputEnvironmentInfo(osName)
 
         switch(osName)
         {
-        case 'Windows': 
-            executeBuildWindows(options); 
+        case 'Windows':
+            executeBuildWindows(options);
             break;
         case 'OSX':
             executeBuildOSX(options);
             break;
-        default: 
+        default:
             executeBuildLinux(options);
         }
-        
+
         //stash includes: 'Bin/**/*', name: "app${osName}"
     }
     catch (e) {
@@ -268,7 +268,7 @@ def executeBuild(String osName, Map options)
     }
     finally {
         archiveArtifacts "*.log"
-    }                        
+    }
 }
 
 def executePreBuild(Map options)
@@ -289,17 +289,17 @@ def executePreBuild(Map options)
 
         echo "The last commit was written by ${AUTHOR_NAME}."
         options.AUTHOR_NAME = AUTHOR_NAME
-        
+
         commitMessage = bat ( script: "git log --format=%%B -n 1", returnStdout: true )
         echo "Commit message: ${commitMessage}"
-        
+
         options.commitMessage = commitMessage.split('\r\n')[2].trim()
         echo "Opt.: ${options.commitMessage}"
         options['commitSHA'] = bat(script: "git log --format=%%H -1 ", returnStdout: true).split('\r\n')[2].trim()
-                
+
     }
 
-   
+
     def tests = []
     if(options.testsPackage != "none")
     {
@@ -329,12 +329,12 @@ def executePreBuild(Map options)
         }
         options.testsList = tests
     }
-    
+
 }
 
 def executeDeploy(Map options, List platformList, List testResultList)
 {
-    try { 
+    try {
         if(options['executeTests'] && testResultList)
         {
             checkoutGit(options['testsBranch'], 'git@github.com:luxteam/jobs_test_rs2rpr.git')
@@ -354,7 +354,7 @@ def executeDeploy(Map options, List platformList, List testResultList)
                             println(e.toString());
                             println(e.getMessage());
                         }
-                    
+
                     }
                 }
             }
@@ -384,11 +384,11 @@ def executeDeploy(Map options, List platformList, List testResultList)
                 {
                     println("ERROR during slack status generation")
                     println(e.toString())
-                    println(e.getMessage())   
+                    println(e.getMessage())
                 }
 
             }
-            
+
             try
             {
                 def summaryReport = readJSON file: 'summaryTestResults/summary_status.json'
@@ -405,7 +405,7 @@ def executeDeploy(Map options, List platformList, List testResultList)
                 println("CAN'T GET TESTS STATUS")
                 currentBuild.result="UNSTABLE"
             }
-            
+
             try
             {
                 options.testsStatus = readFile("summaryTestResults/slack_status.json")
@@ -415,12 +415,12 @@ def executeDeploy(Map options, List platformList, List testResultList)
                 println(e.toString())
                 println(e.getMessage())
                 options.testsStatus = ""
-            }   
+            }
 
-            publishHTML([allowMissing: false, 
-                         alwaysLinkToLastBuild: false, 
-                         keepAll: true, 
-                         reportDir: 'summaryTestResults', 
+            publishHTML([allowMissing: false,
+                         alwaysLinkToLastBuild: false,
+                         keepAll: true,
+                         reportDir: 'summaryTestResults',
                          reportFiles: 'summary_report.html',
                          reportName: 'Test Report',
                          reportTitles: 'Summary Report'])
@@ -433,12 +433,12 @@ def executeDeploy(Map options, List platformList, List testResultList)
         throw e
     }
     finally
-    {}   
+    {}
 }
 
 def call(String projectBranch = "",
          String testsBranch = "master",
-         String platforms = 'Windows:NVIDIA_GF1080TI', 
+         String platforms = 'Windows:NVIDIA_GF1080TI',
          Boolean updateORRefs = false,
          Boolean updateRefs = false,
          Boolean enableNotifications = true,
@@ -446,12 +446,12 @@ def call(String projectBranch = "",
          String tests = "") {
     try
     {
-        String PRJ_NAME="RS2RPRConvertTool"
+        String PRJ_NAME="RS2RPRConvertTool-Maya"
         String PRJ_ROOT="rpr-tools"
 
-        multiplatform_pipeline(platforms, this.&executePreBuild, this.&executeBuild, this.&executeTests, this.&executeDeploy, 
-                               [projectBranch:projectBranch, 
-                                testsBranch:testsBranch, 
+        multiplatform_pipeline(platforms, this.&executePreBuild, this.&executeBuild, this.&executeTests, this.&executeDeploy,
+                               [projectBranch:projectBranch,
+                                testsBranch:testsBranch,
                                 updateORRefs:updateORRefs,
                                 updateRefs:updateRefs,
                                 enableNotifications:enableNotifications,
