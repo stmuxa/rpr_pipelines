0ad7f8cc968295229b4cb46073e12dbbdc266376
Andrey Alekseevich <magister.demens@gmail.com>
2019-04-22 19:17:52 +0300

    implemented rbs_push_builder_failure function

---
+++ vars/rbs_push_builder_failure.groovy
@@ -0,0 +1,6 @@
+def call(String url, String token, String branchTag, String toolName)
+
+    def response = consoleLogResponseBody: true, customHeaders: [[name: 'Authorization', value: "Token ${token}"]], httpMode: 'POST', ignoreSslErrors: true, url: "${url}?name=${env.BUILD_NUMBER}&tool=${toolName}&branch=${branchTag}&status=FAILURE", validResponseCodes: '200'
+
+    echo "Status: ${response.status}\nContent: ${response.content}"
+}
