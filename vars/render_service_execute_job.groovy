64232d95631373057f9da26a17ffbc280a23fde8
Andrey Kravchenko <epozine@inbox.ru>
2019-05-31 16:23:39 +0300

    Update render_service_execute_job.groovy

--- vars/render_service_execute_job.groovy
+++ vars/render_service_execute_job.groovy
@@ -45,10 +45,10 @@ def executeBuildViewer(osName, gpuName, Map options, uniqueID) {
 						rm -rf *
 					'''
 					print(python3("${CIS_TOOLS}\\${options.cis_tools}\\send_execute_status.py --django_ip \"${options.django_url}/\" --status \"Downloading package\" --id ${id}"))
-					bat """ 
+					sh """ 
 						wget --no-check-certificate "${options.data_link}"
 					"""
-					bat """
+					sh """
 						7z x "${filename}"
 					"""
 
