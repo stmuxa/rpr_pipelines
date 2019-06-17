16adfb7683fd4e69ac911762d1ed039c9279ab8e
Andrey Alekseevich <magister.demens@gmail.com>
2019-06-17 17:20:14 +0300

    build log fix

--- vars/radeon_ml_miopen.groovy
+++ vars/radeon_ml_miopen.groovy
@@ -38,7 +38,7 @@ def executeBuildLinux(Map options)
     mkdir build-direct
     cd build-direct
     cmake ${options['cmakeKeys']} .. >> ../${STAGE_NAME}.log 2>&1
-    make -j
+    make -j >> ../${STAGE_NAME}.log 2>&1
     copy ../third_party/miopen/MIOpen.so ./Release/MIOpen.so
     copy ../third_party/miopen/MIOpen.so.1 ./Release/MIOpen.so.1
     """
