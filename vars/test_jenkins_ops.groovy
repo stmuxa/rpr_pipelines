4810a7e23276cf5b86dc94e2735a0293a21c53bb
Andrey Kravchenko <epozine@inbox.ru>
2019-06-27 17:53:26 +0300

    Update test_jenkins_ops.groovy

--- vars/test_jenkins_ops.groovy
+++ vars/test_jenkins_ops.groovy
@@ -1,52 +1,14 @@
 
 
 def executeTests() {
-    
-    timeStart = new Date()
     stash includes: "1.zip", name: "1.zip", allowEmpty: true
-	timeStop = new Date()
-	print (timeStart - timeStop)
-
-	timeStart = new Date()
     stash includes: "2.zip", name: "2.zip", allowEmpty: true
-	timeStop = new Date()
-	print (timeStart - timeStop)
 
-	timeStart = new Date()
-    stash includes: "3.zip", name: "3.zip", allowEmpty: true
-	timeStop = new Date()
-	print (timeStart - timeStop)
-
-	timeStart = new Date()
     unstash "1.zip"
-	timeStop = new Date()
-	print (timeStart - timeStop)
-
-	timeStart = new Date()
     unstash "2.zip"
-	timeStop = new Date()
-	print (timeStart - timeStop)
 
-	timeStart = new Date()
-    unstash "3.zip"
-	timeStop = new Date()
-	print (timeStart - timeStop)
-
-	timeStart = new Date()
     archiveArtifacts "1.zip"
-	timeStop = new Date()
-	print (timeStart - timeStop)
-
-	timeStart = new Date()
-    archiveArtifacts "2.zip"
-	timeStop = new Date()
-	print (timeStart - timeStop)
-
-	timeStart = new Date()
-    archiveArtifacts "3.zip"
-	timeStop = new Date()
-	print (timeStart - timeStop)
-				
+    archiveArtifacts "2.zip"			
 }
 
 
