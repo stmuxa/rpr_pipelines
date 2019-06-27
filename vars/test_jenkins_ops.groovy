70df2b2677e583b50cdfdf304f103b6552388500
Andrey Kravchenko <epozine@inbox.ru>
2019-06-27 13:15:00 +0300

    Create test_jenkins_ops.groovy

---
+++ vars/test_jenkins_ops.groovy
@@ -0,0 +1,98 @@
+
+
+def executeTests() {
+    
+    def timeStart = new Date()
+    stash includes: "C:\\TestOps\\1.zip", name: "1.zip", allowEmpty: true
+	def timeStop = new Date()
+	TimeDuration duration = TimeCategory.minus(timeStop, timeStart)
+	println duration
+
+	def timeStart = new Date()
+    stash includes: "C:\\TestOps\\2.zip", name: "2.zip", allowEmpty: true
+	def timeStop = new Date()
+	TimeDuration duration = TimeCategory.minus(timeStop, timeStart)
+	println duration
+
+	def timeStart = new Date()
+    stash includes: "C:\\TestOps\\3.zip", name: "3.zip", allowEmpty: true
+	def timeStop = new Date()
+	TimeDuration duration = TimeCategory.minus(timeStop, timeStart)
+	println duration
+
+	def timeStart = new Date()
+    unstash "1.zip"
+	def timeStop = new Date()
+	TimeDuration duration = TimeCategory.minus(timeStop, timeStart)
+	println duration
+
+	def timeStart = new Date()
+    unstash "2.zip"
+	def timeStop = new Date()
+	TimeDuration duration = TimeCategory.minus(timeStop, timeStart)
+	println duration
+
+	def timeStart = new Date()
+    unstash "3.zip"
+	def timeStop = new Date()
+	TimeDuration duration = TimeCategory.minus(timeStop, timeStart)
+	println duration
+
+	def timeStart = new Date()
+    archiveArtifacts "1.zip"
+	def timeStop = new Date()
+	TimeDuration duration = TimeCategory.minus(timeStop, timeStart)
+	println duration
+
+	def timeStart = new Date()
+    archiveArtifacts "2.zip"
+	def timeStop = new Date()
+	TimeDuration duration = TimeCategory.minus(timeStop, timeStart)
+	println duration
+
+	def timeStart = new Date()
+    archiveArtifacts "3.zip"
+	def timeStop = new Date()
+	TimeDuration duration = TimeCategory.minus(timeStop, timeStart)
+	println duration
+				
+}
+
+
+
+def main(String platforms, Map options) {
+	 
+	try {
+		timestamps {
+		    String PRJ_PATH="${options.PRJ_ROOT}/${options.PRJ_NAME}"
+		    String JOB_PATH="${PRJ_PATH}/${JOB_NAME}/Build-${BUILD_ID}".replace('%2F', '_')
+		    options['PRJ_PATH']="${PRJ_PATH}"
+		    options['JOB_PATH']="${JOB_PATH}"
+
+
+		    echo "Scheduling Test Ops"
+			node("TestOps"){
+				stage("Test"){
+					timeout(time: 360, unit: 'MINUTES'){
+					    ws("WS/${options.PRJ_NAME}") {
+							executeTests()
+						}
+					}
+				}
+			}
+		}			  
+    } catch (e) {
+		println(e.toString());
+		println(e.getMessage());
+		println(e.getStackTrace());
+		currentBuild.result = "FAILED"
+		throw e
+   	}
+}
+    
+def call() {
+	String PRJ_ROOT='TestJenkinsOps'
+	String PRJ_NAME='TestJenkinsOps'  
+	  main()
+    }
+
