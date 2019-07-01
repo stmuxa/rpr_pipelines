d5ff796389f72f6f4bd6945b91f0529bef65869c
Andrey Kravchenko <epozine@inbox.ru>
2019-07-01 12:48:40 +0300

    Update test_jenkins_ops.groovy

--- vars/test_jenkins_ops.groovy
+++ vars/test_jenkins_ops.groovy
@@ -1,29 +1,51 @@
 
 
 def executeTests(operations) {
-	try {
-		bat '''
-			wget "https://drive.google.com/open?id=1W4AA5Rn-6cBls8lAS1gVdXejvchHP0nP"
-			wget "https://drive.google.com/open?id=1sHQwwtbEKHydEYRbs9SBdQh6AD7XPH42"
-		'''
-	} catch (e) {
-		println(e.toString());
-		println(e.getMessage());
-		println(e.getStackTrace());
-		throw e	
+	
+	def zip_one_exists = fileExists '1.zip'
+	if (!zip_one_exists) {
+		try {
+			echo "Downloading 1st zip test package"
+			bat '''
+				wget --no-check-certificate "https://drive.google.com/open?id=1W4AA5Rn-6cBls8lAS1gVdXejvchHP0nP"
+			'''
+		} catch (e) {
+			println(e.toString());
+			println(e.getMessage());
+			println(e.getStackTrace());
+			throw e	
+		}
 	}
+	
+	def zip_two_exists = fileExists '1.zip'
+	if (!zip_two_exists) {
+		try {
+			echo "Downloading 2nd zip test package"
+			bat '''
+				wget --no-check-certificate "https://drive.google.com/open?id=1sHQwwtbEKHydEYRbs9SBdQh6AD7XPH42"
+			'''
+		} catch (e) {
+			println(e.toString());
+			println(e.getMessage());
+			println(e.getStackTrace());
+			throw e	
+		}
+	}
+	
 	if (operations.contains("stash")) {
-		print("Stash")
+		print("Stashing test")
 		stash includes: "1.zip", name: "1.zip", allowEmpty: true
 		stash includes: "2.zip", name: "2.zip", allowEmpty: true
 	}
 
 	if (operations.contains("unstash")) {
+		print("Unstashing test")
 		unstash "1.zip"
 		unstash "2.zip"
 	}
 	
 	if (operations.contains("archiveArtifacts")) {
+		print("archiveArtifacts test")
 		archiveArtifacts "1.zip"
 		archiveArtifacts "2.zip"		
 	}
