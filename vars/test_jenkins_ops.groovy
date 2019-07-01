ba345352124507b2a0875aa33d49a41c2d033152
Andrey Kravchenko <epozine@inbox.ru>
2019-07-01 11:29:50 +0300

    Update test_jenkins_ops.groovy

--- vars/test_jenkins_ops.groovy
+++ vars/test_jenkins_ops.groovy
@@ -1,15 +1,15 @@
 
 
 def executeTests(operations) {
-    if ("stash" in operations) {
-	    stash includes: "1.zip", name: "1.zip", allowEmpty: true
-	    stash includes: "2.zip", name: "2.zip", allowEmpty: true
-    }
+	if ("stash" in operations) {
+		stash includes: "1.zip", name: "1.zip", allowEmpty: true
+		stash includes: "2.zip", name: "2.zip", allowEmpty: true
+	}
 
-    if ("unstash" in operations) {
-	    unstash "1.zip"
-	    unstash "2.zip"
-    }
+	if ("unstash" in operations) {
+		unstash "1.zip"
+		unstash "2.zip"
+	}
 	
 	if ("archiveArtifacts" in operations) {
 		archiveArtifacts "1.zip"
@@ -17,47 +17,41 @@ def executeTests(operations) {
 	}
 }
 
-def nodeNames() {
-  return jenkins.model.Jenkins.instance.nodes.collect { node -> node.name }
-}
-
 def main(label, operations, type) {
 	try {
 		timestamps {
 			
-			def testTasks = [:]
-			def names = nodeNames()
+			def testers = [:]
 
-			for (int i=0; i<names.size(); ++i) {
-			  def nodeName = names[i];
-				testTasks["Test-${nodeName}"] = {
-			      node(nodeName) {
-				  echo "Triggering on " + nodeName
-				  stage("Test"){
+			testers["${label}"] = {
+				node("${label}") {
+					stage("Test"){
 						timeout(time: 360, unit: 'MINUTES'){
 							ws("WS/TestOps") {
 								executeTests(operations)
 							}
 						}
-				   }
+				    }
 				}
-			  }
 			}
-			parallel testTasks
+
+			parallel testers
 		}			  
-    } catch (e) {
+	} catch (e) {
 		println(e.toString());
 		println(e.getMessage());
 		println(e.getStackTrace());
 		currentBuild.result = "FAILED"
 		throw e
-   	}
+	}
 }
-    
-def call(String label = "",
-	String operations = "",
-	String type = ""
-	) {
-	  main(label, operations, type)
-    }
+	
+def call(
+		String label = "",
+		String operations = "",
+		String type = ""
+		) {
+	  	main(label, operations, type)
+	}
+
 
