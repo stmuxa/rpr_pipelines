165e0a0fe621991ae05612274f49a741dfef8056
Andrey Alekseevich <magister.demens@gmail.com>
2019-07-16 17:12:52 +0300

    Extend TEST_TIMEOUT

--- vars/rpr_blender28plugin_pipeline.groovy
+++ vars/rpr_blender28plugin_pipeline.groovy
@@ -935,7 +935,7 @@ def call(String projectBranch = "",
                                 forceBuild:forceBuild,
                                 reportName:'Test_20Report',
                                 splitTestsExectuion:splitTestsExectuion,
-                                TEST_TIMEOUT:120,
+                                TEST_TIMEOUT:150,
                                 TESTER_TAG:"Blender2.8"])
     }
     catch(e)
