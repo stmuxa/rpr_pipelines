9b75f253c99f8d01b6984a70742ee3dccd1eb1a4
Andrey Kravchenko <epozine@inbox.ru>
2019-09-07 15:49:01 +0300

    Update render_service_convert_job.groovy

--- vars/render_service_convert_job.groovy
+++ vars/render_service_convert_job.groovy
@@ -1,4 +1,4 @@
-def executeRender(osName, gpuName, Map options, uniqueID) {
+def executeConvert(osName, gpuName, Map options, uniqueID) {
     currentBuild.result = 'SUCCESS'
     
     String tool = options['Tool'].split(':')[0].trim()
@@ -19,53 +19,45 @@ def executeRender(osName, gpuName, Map options, uniqueID) {
 		''' 
 
 		switch(tool) {
-		    case 'RedshiftConvert':
+		    case 'Maya Redshift':
 			    
 				bat """
-					cd "..\\..\\RenderServiceStorage\\RS2RPRConvertTool" 
+					cd "${CIS_TOOLS}\\..\\RenderServiceStorage\\RS2RPRConvertTool" 
 					git pull
 					cd "..\\..\\WS\\Render_Scene_Render"
-					copy "..\\..\\RenderServiceStorage\\RS2RPRConvertTool\\convertRS2RPR.py" "."
+					copy "${CIS_TOOLS}\\..\\RenderServiceStorage\\RS2RPRConvertTool\\convertRS2RPR.py" "."
 				"""
 
 				bat """
-					copy "..\\..\\cis_tools\\${options.cis_tools}\\find_scene_maya.py" "."
-					copy "..\\..\\cis_tools\\${options.cis_tools}\\launch_redshift_render_conv.py" "."
-					copy "..\\..\\cis_tools\\${options.cis_tools}\\launch_converted_render.py" "."
-					copy "..\\..\\cis_tools\\${options.cis_tools}\\maya_convert_render.py" "."
+					copy "${CIS_TOOLS}\\${options.cis_tools}\\find_scene_maya.py" "."
+					copy "${CIS_TOOLS}\\${options.cis_tools}\\launch_redshift_render_conv.py" "."
+					copy "${CIS_TOOLS}\\${options.cis_tools}\\launch_converted_render.py" "."
+					copy "${CIS_TOOLS}\\${options.cis_tools}\\maya_convert_render.py" "."
 				"""
-				
-				String scene_exists = python3("..\\..\\cis_tools\\${options.cis_tools}\\check_scene_exists.py --file_name ${scene_name} ").split('\r\n')[2].trim()
-				if (scene_exists == "file_exists") {
-				    bat """
-						copy "..\\..\\RenderServiceStorage\\scenes\\${scene_name}" "."
-				    """
-				} else {
-				    python3("..\\..\\cis_tools\\${options.cis_tools}\\send_status.py --django_ip \"${options.django_url}/\" --tool ${tool} --status \"Downloading scene\" --id ${id}")
-				    bat """ 
-				    	"..\\..\\cis_tools\\${options.cis_tools}\\download.bat" "${options.Scene}"
-				    """
-				    bat """
-						copy ${scene_name} "..\\..\\RenderServiceStorage\\scenes" 
-				    """
-				}
+			
+				print(python3("${CIS_TOOLS}\\${options.cis_tools}\\send_convert_status.py --django_ip \"${options.django_url}/\" --tool \"${tool}\" --status \"Downloading scene\" --id ${id}"))
+				bat """ 
+					wget --no-check-certificate "${options.Scene}"
+				"""
 
 				if ("${scene_name}".endsWith('.zip') || "${scene_name}".endsWith('.7z')) {
 				    bat """
-				    	"..\\..\\cis_tools\\7-Zip\\7z.exe" x "${scene_name}"
+				    	7z x "${scene_name}"
 				    """
-				    options['sceneName'] = python3("find_scene_maya.py --folder . ").split('\r\n')[2].trim()
+				    options['sceneName'] = python3("find_scene_maya.py --folder .").split('\r\n')[2].trim()
 				}
 				
 				String scene=python3("find_scene_maya.py --folder . ").split('\r\n')[2].trim()
 				echo "Find scene: ${scene}"
 				echo "Launching conversion and render"
-				python3("..\\..\\cis_tools\\${options.cis_tools}\\send_status.py --django_ip \"${options.django_url}/\" --tool ${tool} --status \"Rendering Redshift scene\" --id ${id}")
+				print(python3("${CIS_TOOLS}\\${options.cis_tools}\\send_convert_status.py --django_ip \"${options.django_url}/\" --tool \"${tool}\" --status \"Rendering Redshift scene\" --id ${id}"))
 				python3("launch_redshift_render_conv.py --tool ${version} --django_ip \"${options.django_url}/\" --id ${id} --scene \"${scene}\" --sceneName ${options.sceneName}")
-				python3("..\\..\\cis_tools\\${options.cis_tools}\\send_status.py --django_ip \"${options.django_url}/\" --tool ${tool} --status \"Rendering converted scene\" --id ${id}")
+				print(python3(".${CIS_TOOLS}\\${options.cis_tools}\\send_convert_status.py --django_ip \"${options.django_url}/\" --tool \"${tool}\" --status \"Rendering converted scene\" --id ${id}"))
 				python3("launch_converted_render.py --tool ${version} --django_ip \"${options.django_url}/\" --id ${id} --scene \"${scene}\" --sceneName ${options.sceneName}")
 				echo "Preparing results"
-				python3("..\\..\\cis_tools\\${options.cis_tools}\\send_status.py --django_ip \"${options.django_url}/\" --tool ${tool} --status \"Completed\" --id ${id}")
+				print(python3("${CIS_TOOLS}\\${options.cis_tools}\\send_convert_status.py --django_ip \"${options.django_url}/\" --tool \"${tool}\" --status \"Completed\" --id ${id}"))
+				
+				archiveArtifacts 'Output/*'
 				break;
 			}   
 	    } catch(e) {
@@ -73,8 +65,7 @@ def executeRender(osName, gpuName, Map options, uniqueID) {
 			print e
 			echo "Error while render"
 	    } finally {
-			String stashName = osName + "_" + gpuName + "_" + uniqueID
-			stash includes: 'Output/*', name: stashName, allowEmpty: true
+			print(python3("${CIS_TOOLS}\\${options.cis_tools}\\send_convert_results.py --django_ip \"${options.django_url}\" --build_number ${currentBuild.number} --jenkins_job \"${options.jenkins_job}\" --status ${currentBuild.result} --id ${id}"))
 	    }
 	  break;
 	}
@@ -82,7 +73,7 @@ def executeRender(osName, gpuName, Map options, uniqueID) {
 }
 
 
-def main(String platforms, Map options) {
+def main(String PCs, Map options) {
 	
     try {
 
@@ -106,27 +97,10 @@ def main(String platforms, Map options) {
 		options['jenkins_job'] = "RenderServiceConvertJob"
 	    }
 
-
 	    def testTasks = [:]
-	    def nodes = platforms.split(';')
+	    def nodes = PCs.split(';')
 	    int platformCount = nodes.size()
-	    int frameStep = 0
-	    int frameCount = 0
 	    
-	    try {
-
-		if (platformCount > 1 && options['startFrame'] != options['endFrame']) {
-		    int startFrame = options['startFrame'] as Integer
-		    int endFrame = options['endFrame'] as Integer
-		    frameCount = endFrame - startFrame + 1
-		    if (frameCount % platformCount == 0) {
-			frameStep = frameCount / platformCount
-		    } else {
-			int absFrame = frameCount + (platformCount - frameCount % platformCount)
-			frameStep = absFrame / platformCount
-		    }
-		}
-	
 		for (i = 0; i < platformCount; i++) {
 
 		    String uniqueID = Integer.toString(i)
@@ -134,69 +108,34 @@ def main(String platforms, Map options) {
 		    String item = nodes[i]
 		    Map newOptions = options.clone()
 
-		    if (platformCount > 1 && newOptions['startFrame'] != newOptions['endFrame']) {
-			if (i != (platformCount - 1)) {
-			    newOptions['startFrame'] = Integer.toString(i * frameStep + 1)
-			    newOptions['endFrame'] = Integer.toString((i + 1) * frameStep)
-			} else {
-			    newOptions['startFrame'] = Integer.toString(i * frameStep + 1)
-			    newOptions['endFrame'] = Integer.toString(frameCount)
-			}
-		    }
-
 		    List tokens = item.tokenize(':')
 		    String osName = tokens.get(0)
 		    String deviceName = tokens.get(1)
 		    
 		    String renderDevice = ""
 		    if (deviceName == "ANY") {
-			String tool = options['Tool'].split(':')[0].trim()
-			renderDevice = tool
+				renderDevice = "Redshift"
 		    } else {
-			if (options['RenderDevice'] == "gpu") {
 			    renderDevice = "gpu${deviceName}"
-			} else {
-			    renderDevice = "cpu${deviceName}"
-			}
 		    }
 		    
-		    echo "Scheduling Render ${osName}:${deviceName}"
+		    echo "Scheduling converter task ${osName}:${deviceName}"
 		    testTasks["Test-${osName}-${deviceName}"] = {
-			node("${osName} && RenderService && ${renderDevice}")
-			{
-			    stage("Render-${osName}-${deviceName}")
-			    {
-				timeout(time: 60, unit: 'MINUTES')
-				{
-				    ws("WS/${newOptions.PRJ_NAME}_Render") {
-					executeRender(osName, deviceName, newOptions, uniqueID)
+				node("${osName} && RenderService && ${renderDevice}"){
+				    stage("BuildViewer-${osName}-${deviceName}"){
+						timeout(time: 60, unit: 'MINUTES'){
+						    ws("WS/${newOptions.PRJ_NAME}") {
+								executeConvert(osName, deviceName, newOptions, uniqueID)
+						    }
+						}
 				    }
 				}
-			    }
-			}
 		    }
-
 		}
 
 		parallel testTasks
-
-	    } finally {
-		node("Windows && RSReportBuilder")
-		{
-		    stage("Deploy")
-		    {
-			timeout(time: 15, unit: 'MINUTES')
-			{
-			    ws("WS/${options.PRJ_NAME}_Deploy") {
-				executeDeploy(nodes, options)
-			    }
-			}
-		    }
-		}
-	    }
 	}    
-    }   
-    catch (e) {
+    } catch (e) {
 	println(e.toString());
 	println(e.getMessage());
 	println(e.getStackTrace());
@@ -207,23 +146,19 @@ def main(String platforms, Map options) {
     
 def call(String Tool = '',
     String Scene = '',  
-    String platforms = '',
-    String RenderDevice = '',
+    String PCs = '',
     String id = '',
-    String Plugin_Link = '',
     String sceneName = '',
     ) {
 	String PRJ_ROOT='RenderServiceRenderJob'
 	String PRJ_NAME='RenderServiceRenderJob'  
-	main(platforms,[
+	main(PCs,[
 	    enableNotifications:false,
 	    PRJ_NAME:PRJ_NAME,
 	    PRJ_ROOT:PRJ_ROOT,
 	    Tool:Tool,
 	    Scene:Scene,
-	    RenderDevice:RenderDevice,
 	    id:id,
-	    Plugin_Link:Plugin_Link,
-	    sceneName:sceneName,
+	    sceneName:sceneName
 	    ])
     }
