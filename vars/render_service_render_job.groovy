766c164a0afe58cc1244961df2ab37a7f8ad394c
Andrey Kravchenko <epozine@inbox.ru>
2019-09-07 13:19:40 +0300

    Update render_service_render_job.groovy

--- vars/render_service_render_job.groovy
+++ vars/render_service_render_job.groovy
@@ -105,6 +105,34 @@ def executeRender(osName, gpuName, Map options, uniqueID) {
 				echo "Preparing results"
 				python3("${CIS_TOOLS}\\${options.cis_tools}\\send_render_status.py --django_ip \"${options.django_url}/\" --tool ${tool} --status \"Completed\" --id ${id}")
 				break;
+			
+		    case 'Maya (Redshift)':
+		    
+				bat """
+					copy "${CIS_TOOLS}\\${options.cis_tools}\\find_scene_maya.py" "."
+					copy "${CIS_TOOLS}\\${options.cis_tools}\\launch_maya_redshift.py" "."
+				"""
+
+				python3("${CIS_TOOLS}\\${options.cis_tools}\\send_render_status.py --django_ip \"${options.django_url}/\" --tool ${tool} --status \"Downloading scene\" --id ${id}")
+				bat """ 
+					wget --no-check-certificate "${options.Scene}"
+				"""
+
+				if ("${scene_name}".endsWith('.zip') || "${scene_name}".endsWith('.7z')) {
+				    bat """
+				    	7z x "${scene_name}"
+				    """
+				    options['sceneName'] = python3("find_scene_maya.py --folder . ").split('\r\n')[2].trim()
+				}
+				
+				String scene=python3("find_scene_maya.py --folder . ").split('\r\n')[2].trim()
+				echo "Find scene: ${scene}"
+				echo "Launching render"
+				python3("${CIS_TOOLS}\\${options.cis_tools}\\send_render_status.py --django_ip \"${options.django_url}/\" --tool ${tool} --status \"Rendering scene\" --id ${id}")
+				python3("launch_maya.py --tool ${version} --django_ip \"${options.django_url}/\" --id ${id} --min_samples ${options.Min_Samples} --max_samples ${options.Max_Samples} --noise_threshold ${options.Noise_threshold} --scene \"${scene}\" --sceneName \"${options.sceneName}\" ")
+				echo "Preparing results"
+				python3("${CIS_TOOLS}\\${options.cis_tools}\\send_render_status.py --django_ip \"${options.django_url}/\" --tool ${tool} --status \"Completed\" --id ${id}")
+				break;
 		    
 		    case 'Core':
 				    
