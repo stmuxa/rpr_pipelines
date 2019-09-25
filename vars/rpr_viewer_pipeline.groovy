d0365c1e77bcd2d0fca69e11124f95c3f5fd969d
Andrey Alekseevich <magister.demens@gmail.com>
2019-09-25 19:43:49 +0300

    assests pull fix

--- vars/rpr_viewer_pipeline.groovy
+++ vars/rpr_viewer_pipeline.groovy
@@ -65,9 +65,19 @@ def executeTests(String osName, String asicName, Map options)
 {
     cleanWs()
 
-    bat """
-    %CIS_TOOLS%\\receiveFilesSync.bat ${options.PRJ_ROOT}/${options.PRJ_NAME}/Assets/ /mnt/c/TestResources/RprViewer
-    """
+    // update assets
+    if(isUnix())
+    {
+        sh """
+        ${CIS_TOOLS}/receiveFilesSync.sh ${options.PRJ_ROOT}/${options.PRJ_NAME}/Assets/ ${CIS_TOOLS}/../TestResources/RprViewer
+        """
+    }
+    else
+    {
+        bat """
+        %CIS_TOOLS%\\receiveFilesSync.bat ${options.PRJ_ROOT}/${options.PRJ_NAME}/Assets/ /mnt/c/TestResources/RprViewer
+        """
+    }
 
     String REF_PATH_PROFILE="${options.REF_PATH}/${asicName}-${osName}"
     String JOB_PATH_PROFILE="${options.JOB_PATH}/${asicName}-${osName}"
