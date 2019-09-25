a69ac894c7584312b6214ca862204e3c542f9ceb
Andrey Alekseevich <magister.demens@gmail.com>
2019-09-25 18:28:10 +0300

    premake vs2017

--- vars/radeonpro_imageprocessing_pipeline.groovy
+++ vars/radeonpro_imageprocessing_pipeline.groovy
@@ -79,7 +79,7 @@ def executeBuildWindows(String cmakeKeys)
     set target=build
     set maxcpucount=/maxcpucount
     set PATH=C:\\Python27\\;%PATH%
-    .\\Tools\\premake\\win\\premake5 --embed_kernels vs2015 --generate_build_info ${cmakeKeys} >> ${STAGE_NAME}.log 2>&1
+    .\\Tools\\premake\\win\\premake5 --embed_kernels vs2017 --generate_build_info ${cmakeKeys} >> ${STAGE_NAME}.log 2>&1
     set solution=.\\RadeonImageFilters.sln
     %msbuild% /target:%target% %maxcpucount% /property:Configuration=Release;Platform=x64 %parameters% %solution% >> ${STAGE_NAME}.log 2>&1
     %msbuild% /target:%target% %maxcpucount% /property:Configuration=Debug;Platform=x64 %parameters% %solution% >> ${STAGE_NAME}.log 2>&1
