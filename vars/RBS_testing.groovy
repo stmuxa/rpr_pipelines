ee4143169f9d13fe4735d9078d0c36ade72a56e6
Dmitry Galkin <galkin.dmitry.485@gmail.com>
2019-10-07 10:43:50 +0300

    Init Tests

--- vars/RBS_testing.groovy
+++ vars/RBS_testing.groovy
@@ -1,5 +1,7 @@
 import RBS
 
-rbs = new RBS(this, "Blender28", env.JOB_NAME, env)
 
-println(rbs.buildName)
+def call() {
+	rbs = new RBS(this, "Blender28", env.JOB_NAME, env)
+	println(rbs.buildName)	
+}
