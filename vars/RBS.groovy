5793c0ae86f5ac9281f6445742a0f69c291774ba
dm1tryG <galkin.dmitry.485@gmail.com>
2019-10-07 14:38:30 +0300

    Add test module

--- vars/RBS.groovy
+++ vars/RBS.groovy
@@ -31,10 +31,6 @@ class RBS {
     def rbsPassword
     def instancesConfig = [
         [
-            "url": "https://rbsdb.cis.luxoft.com",
-            "credentialId": "ddd49290-412d-45c3-9ae4-65dba573b4c0"
-        ],
-        [
             "url" : "https://rbsdbdev.cis.luxoft.com",
             "credentialId": "847a5a5d-700d-439b-ace1-518f415eb8d8"
         ]
@@ -107,8 +103,9 @@ class RBS {
                         "groups": ["${options.testsList.join('","')}"],
                         "count_test_machine" : ${options.gpusCount}}
                     """.replaceAll("\n", "")
-
+                     this.context.println(requestData)
                     def response = this.context.httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', customHeaders: [[name: 'Authorization', value: "Token ${i.token}"]], httpMode: 'POST', ignoreSslErrors: true, url: "${i.url}/report/job?data=${java.net.URLEncoder.encode(requestData, 'UTF-8')}", validResponseCodes: '200'
+                    this.buildID = response.content
                     this.context.echo "Status: ${response.status}\nContent: ${response.content}"
                 }
 
--- vars/RBS_testing.groovy
+++ vars/RBS_testing.groovy
@@ -1,7 +1,16 @@
 import RBS
 
-
 def call() {
-	rbs = new RBS(this, "Blender28", env.JOB_NAME, env)
-	println(rbs.buildName)	
+    node("Windows") {
+        rbs = new RBS(this, "Blender28", env.JOB_NAME, env)
+    	println(rbs.buildName)
+        Map options = [
+            JOB_STARTED_TIME: rbs.getTime(),
+            testsList: ["Smoke"],
+            gpusCount: 2
+        ]
+        rbs.startBuild(options)
+        println(options.JOB_STARTED_TIME)  
+        println(rbs.buildID)
+    }
 }
