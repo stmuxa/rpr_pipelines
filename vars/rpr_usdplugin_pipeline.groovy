bed965ff47fc335a846e33be99ec5647114adb5c
Andrey Alekseevich <magister.demens@gmail.com>
2019-10-16 10:36:02 +0300

    add feature to skip USD build

--- vars/rpr_usdplugin_pipeline.groovy
+++ vars/rpr_usdplugin_pipeline.groovy
@@ -79,17 +79,21 @@ def executeTests(String osName, String asicName, Map options)
 
 def executeBuildWindows(Map options)
 {
+    String CMD_BUILD_USD = """
+    if exist USDgen rmdir /s/q USDgen
+    if exist USDinst rmdir /s/q USDinst
+    python USD\\build_scripts\\build_usd.py -v --build ${WORKSPACE}/USDgen/build --src ${WORKSPACE}/USDgen/src ${WORKSPACE}/USDinst > ${STAGE_NAME}_USD.log 2>&1
+    """
+
+    CMD_BUILD_USD = options.rebuildUSD ? CMD_BUILD_USD : "echo \"Skip USD build\""
+
     withEnv(["PATH=c:\\python27\\;c:\\python27\\scripts\\;${PATH}", "WORKSPACE=${env.WORKSPACE.toString().replace('\\', '/')}"]) {
         bat """
-        if exist USDgen rmdir /s/q USDgen
-        if exist USDinst rmdir /s/q USDinst
-        if exist RadeonProRenderUSD\\build rmdir /s/q RadeonProRenderUSD\\build
-
         call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat" amd64 >> ${STAGE_NAME}.log 2>&1
         python -m pip install --upgrade pip >> ${STAGE_NAME}.log 2>&1
         python -m pip install pyside PyOpenGL >> ${STAGE_NAME}.log 2>&1
 
-        python USD\\build_scripts\\build_usd.py -v --build ${WORKSPACE}/USDgen/build --src ${WORKSPACE}/USDgen/src ${WORKSPACE}/USDinst > ${STAGE_NAME}_USD.log 2>&1
+        ${CMD_BUILD_USD}
 
         set PATH=${WORKSPACE}\\USDinst\\bin;${WORKSPACE}\\USDinst\\lib;%PATH%
         set PYTHONPATH=${WORKSPACE}\\USDinst\\lib\\python;%PYTHONPATH%
@@ -115,9 +119,8 @@ def executeBuildWindows(Map options)
     }
 }
 
-def executeBuildOSX(Map options)
-{
-    sh """
+def executeBuildOSX(Map options) {
+    String CMD_BUILD_USD = """
     if [ -d "./USDgen" ]; then
         rm -fdr ./USDgen
     fi
@@ -126,68 +129,74 @@ def executeBuildOSX(Map options)
         rm -fdr ./USDinst
     fi
 
-    if [ -d "./RadeonProRenderUSD/build" ]; then
-        rm -fdr ./RadeonProRenderUSD/build
+    mkdir -p USDgen
+    mkdir -p USDinst
+
+    python USD/build_scripts/build_usd.py -vvv --build USDgen/build --src USDgen/src USDinst > ${STAGE_NAME}_USD.log 2>&1
+    """
+
+    CMD_BUILD_USD = options.rebuildUSD ? CMD_BUILD_USD : "echo \"Skip USD build\""
+
+    withEnv(["OS="]) {
+        sh """
+        pip install --user --upgrade pip >> ${STAGE_NAME}.log 2>&1
+        pip install --user pyside2 >> ${STAGE_NAME}.log 2>&1
+        pip install --user PyOpenGL >> ${STAGE_NAME}.log 2>&1
+
+        ${CMD_BUILD_USD}
+
+        export PATH=${WORKSPACE}/USDinst/bin:\$PATH
+        export PYTHONPATH=${WORKSPACE}/USDinst/lib/python:\$PYTHONPATH
+
+        mkdir -p RadeonProRenderUSD/build
+        pushd RadeonProRenderUSD/build
+
+        cmake -DUSD_INCLUDE_DIR=${WORKSPACE}/USDinst/include -DUSD_LIBRARY_DIR=${WORKSPACE}/USDinst/lib \
+        -DRPR_LOCATION=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProRender-SDK/Mac \
+        -DRPR_LOCATION_LIB=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProRender-SDK/Mac/lib \
+        -DRPR_LOCATION_INCLUDE=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProRender-SDK/Mac/inc \
+        -DRIF_LOCATION=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProImageProcessing/Mac \
+        -DRIF_LOCATION_LIB=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProImageProcessing/Mac/lib \
+        -DRIF_LOCATION_INCLUDE=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProImageProcessing/Mac/inc \
+        -DRPR_BUILD_AS_HOUDINI_PLUGIN=${options.enableHoudini.toString().toUpperCase()} \
+        -DHOUDINI_ROOT=/Applications/Houdini/Current \
+        -DGLEW_LOCATION=${WORKSPACE}/USDinst \
+        -DCMAKE_INSTALL_PREFIX=${WORKSPACE}/USDinst \
+        -DCMAKE_PREFIX_PATH=${WORKSPACE}/USDinst \
+        -DCMAKE_BUILD_TYPE=Release \
+        .. >> ../../${STAGE_NAME}.log 2>&1
+
+        make >> ../../${STAGE_NAME}.log 2>&1
+        """
+    }
+}
+
+def executeBuildLinux(Map options) {
+    String CMD_BUILD_USD = """
+    if [ -d "./USDgen" ]; then
+        rm -fdr ./USDgen
     fi
 
-    pip install --user --upgrade pip >> ${STAGE_NAME}.log 2>&1
-    pip install --user pyside2 >> ${STAGE_NAME}.log 2>&1
-    pip install --user PyOpenGL >> ${STAGE_NAME}.log 2>&1
+    if [ -d "./USDinst" ]; then
+        rm -fdr ./USDinst
+    fi
 
     mkdir -p USDgen
     mkdir -p USDinst
 
     python USD/build_scripts/build_usd.py -vvv --build USDgen/build --src USDgen/src USDinst > ${STAGE_NAME}_USD.log 2>&1
-
-    export PATH=${WORKSPACE}/USDinst/bin:\$PATH
-    export PYTHONPATH=${WORKSPACE}/USDinst/lib/python:\$PYTHONPATH
-
-    mkdir -p RadeonProRenderUSD/build
-    pushd RadeonProRenderUSD/build
-
-    cmake -DUSD_INCLUDE_DIR=${WORKSPACE}/USDinst/include -DUSD_LIBRARY_DIR=${WORKSPACE}/USDinst/lib \
-    -DRPR_LOCATION=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProRender-SDK/Mac \
-    -DRPR_LOCATION_LIB=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProRender-SDK/Mac/lib \
-    -DRPR_LOCATION_INCLUDE=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProRender-SDK/Mac/inc \
-    -DRIF_LOCATION=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProImageProcessing/Mac \
-    -DRIF_LOCATION_LIB=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProImageProcessing/Mac/lib \
-    -DRIF_LOCATION_INCLUDE=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProImageProcessing/Mac/inc \
-    -DRPR_BUILD_AS_HOUDINI_PLUGIN=${options.enableHoudini.toString().toUpperCase()} \
-    -DGLEW_LOCATION=${WORKSPACE}/USDinst \
-    -DCMAKE_INSTALL_PREFIX=${WORKSPACE}/USDinst \
-    -DCMAKE_PREFIX_PATH=${WORKSPACE}/USDinst \
-    -DCMAKE_BUILD_TYPE=Release \
-    .. >> ../../${STAGE_NAME}.log 2>&1
-
-    make >> ../../${STAGE_NAME}.log 2>&1
     """
-}
 
-def executeBuildLinux(Map options)
-{
-    withEnv(["OS=Linux"]) {
+    CMD_BUILD_USD = options.rebuildUSD ? CMD_BUILD_USD : "echo \"Skip USD build\""
+    // set $OS=null because it use in TBB build script
+    withEnv(["OS="]) {
         sh """
-        if [ -d "./USDgen" ]; then
-            rm -fdr ./USDgen
-        fi
-
-        if [ -d "./USDinst" ]; then
-            rm -fdr ./USDinst
-        fi
-
-        if [ -d "./RadeonProRenderUSD/build" ]; then
-            rm -fdr ./RadeonProRenderUSD/build
-        fi
-
         echo ${SUDO_PASS} | sudo -S apt install -y libglew-dev libxrandr-dev libxcursor-dev libxinerama-dev libxi-dev >> ${STAGE_NAME}.log 2>&1
         echo ${SUDO_PASS} | sudo -S apt install -y python-pyside pyside-tools >> ${STAGE_NAME}.log 2>&1
         pip install --user --upgrade pip >> ${STAGE_NAME}.log 2>&1
         pip install --user PyOpenGL >> ${STAGE_NAME}.log 2>&1
 
-        mkdir -p USDgen
-        mkdir -p USDinst
-
-        python USD/build_scripts/build_usd.py -v --build USDgen/build --src USDgen/src USDinst > ${STAGE_NAME}_USD.log 2>&1
+        ${CMD_BUILD_USD}
 
         export PATH=${WORKSPACE}/USDinst/bin:\$PATH
         export PYTHONPATH=${WORKSPACE}/USDinst/lib/python:\$PYTHONPATH
@@ -203,6 +212,7 @@ def executeBuildLinux(Map options)
         -DRIF_LOCATION_LIB=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProImageProcessing/Linux/Ubuntu/lib64 \
         -DRIF_LOCATION_INCLUDE=${WORKSPACE}/RadeonProRenderThirdPartyComponents/RadeonProImageProcessing/Linux/Ubuntu/include \
         -DRPR_BUILD_AS_HOUDINI_PLUGIN=${options.enableHoudini.toString().toUpperCase()} \
+        -HOUDINI_ROOT=/opt/hfs18.0.251 \
         -DGLEW_LOCATION=${WORKSPACE}/USDinst \
         -DCMAKE_INSTALL_PREFIX=${WORKSPACE}/USDinst \
         -DCMAKE_PREFIX_PATH=${WORKSPACE}/USDinst \
@@ -214,40 +224,36 @@ def executeBuildLinux(Map options)
     }
 }
 
-def executeBuild(String osName, Map options)
-{
+def executeBuild(String osName, Map options) {
     try {
-        dir('RadeonProRenderUSD')
-        {
-            checkoutGit(options['projectBranch'], 'https://github.com/GPUOpen-LibrariesAndSDKs/RadeonProRenderUSD.git')
+        if (options.rebuildUSD) {
+            dir('RadeonProRenderUSD') {
+                checkoutGit(options['projectBranch'], 'https://github.com/GPUOpen-LibrariesAndSDKs/RadeonProRenderUSD.git')
+            }
         }
-        dir('RadeonProRenderThirdPartyComponents')
-        {
+        dir('RadeonProRenderThirdPartyComponents') {
             checkoutGit(options['thirdpartyBranch'], 'https://github.com/Radeon-Pro/RadeonProRenderThirdPartyComponents.git')
         }
-        dir('USD')
-        {
+        dir('USD') {
             checkoutGit(options['usdBranch'], 'https://github.com/PixarAnimationStudios/USD.git')
         }
 
         outputEnvironmentInfo(osName)
 
-        switch(osName)
-        {
-        case 'Windows':
-            executeBuildWindows(options);
-            break;
-        case 'OSX':
-            executeBuildOSX(options);
-            break;
-        default:
-            executeBuildLinux(options);
+        switch(osName) {
+            case 'Windows':
+                executeBuildWindows(options);
+                break;
+            case 'OSX':
+                executeBuildOSX(options);
+                break;
+            default:
+                executeBuildLinux(options);
         }
     }
     catch (e) {
         currentBuild.result = "FAILED"
-        if (options.sendToRBS)
-        {
+        if (options.sendToRBS) {
             try {
                 options.rbs_prod.setFailureStatus()
                 options.rbs_dev.setFailureStatus()
@@ -262,11 +268,9 @@ def executeBuild(String osName, Map options)
     }
 }
 
-def executePreBuild(Map options)
-{
+def executePreBuild(Map options) {
     currentBuild.description = ""
-    ['projectBranch', 'thirdpartyBranch', 'packageBranch'].each
-    {
+    ['projectBranch', 'thirdpartyBranch', 'packageBranch'].each {
         if(options[it] != 'master' && options[it] != "")
         {
             currentBuild.description += "<b>${it}:</b> ${options[it]}<br/>"
@@ -293,7 +297,8 @@ def call(String projectBranch = "",
         Boolean forceBuild = false,
         Boolean splitTestsExectuion = false,
         Boolean sendToRBS = false,
-        Boolean enableHoudini = false)
+        Boolean enableHoudini = false,
+        Boolean rebuildUSD = true)
 {
     try
     {
