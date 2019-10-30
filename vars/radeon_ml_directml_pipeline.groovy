b331e089305eb22b9e0ee35298df64c048719661
Andrey Alekseevich <magister.demens@gmail.com>
2019-10-30 16:59:21 +0300

    GitHub notify

--- vars/radeon_ml_directml_pipeline.groovy
+++ vars/radeon_ml_directml_pipeline.groovy
@@ -27,6 +27,7 @@ def executeTestCommand(String osName, Map options)
 def executeTests(String osName, String asicName, Map options)
 {
     cleanWs()
+    String error_message = ""
 
     try {
         String REF_PATH_PROFILE="${options.REF_PATH}/${asicName}-${osName}"
@@ -46,6 +47,15 @@ def executeTests(String osName, String asicName, Map options)
     finally {
         archiveArtifacts "*.log"
         junit "*gtest.xml"
+
+        if (env.CHANGE_ID) {
+            String context = "${options.PRJ_NAME} [TEST] ${osName}-${asicName}"
+            String description = error_message ? "Testing finished with error message: ${error_message}" : "Testing finished"
+            String status = error_message ? "failure" : "success"
+            String url = "${env.BUILD_URL}/artifact/${STAGE_NAME}.log"
+            pullRequest.createStatus(status, context, description, url)
+            options['commitContexts'].remove(context)
+        }
     }
 }
 
@@ -74,9 +84,16 @@ def executePreBuild(Map options)
 
 def executeBuild(String osName, Map options)
 {
+    String error_message = ""
+    String context = "[${options.PRJ_NAME}] [BUILD] ${osName}"
+
     try {
         checkOutBranchOrScm(options['projectBranch'], options['projectRepo'])
         outputEnvironmentInfo(osName)
+
+        if (env.CHANGE_ID) {
+            pullRequest.createStatus("pending", context, "Checkout has been finished. Trying to build...", "${env.JOB_URL}")
+        }
 
         switch(osName) {
         case 'Windows':
@@ -93,12 +110,19 @@ def executeBuild(String osName, Map options)
     }
     catch (e) {
         println(e.getMessage())
+        error_message = e.getMessage()
         currentBuild.result = "FAILED"
         throw e
     }
     finally {
         archiveArtifacts "${STAGE_NAME}.log"
         zip archive: true, dir: 'build-direct/Release', glob: 'RadeonML-DirectML.*, *.exe', zipFile: "${osName}_Release.zip"
+
+        if (env.CHANGE_ID) {
+            String status = error_message ? "failure" : "success"
+            pullRequest.createStatus("${status}", context, "Build finished as '${status}'", "${env.BUILD_URL}/artifact/${STAGE_NAME}.log")
+            options['commitContexts'].remove(context)
+        }
     }
 }
 
@@ -115,6 +139,31 @@ def call(String projectBranch = "",
          Boolean enableNotifications = false,
          String cmakeKeys = '-G "Visual Studio 15 2017 Win64" -DRML_DIRECTML=ON -DRML_MIOPEN=OFF -DRML_TENSORFLOW_CPU=OFF -DRML_TENSORFLOW_CUDA=OFF') {
 
+    // set pending status for all
+    if(env.CHANGE_ID) {
+
+        def commitContexts = []
+        platforms.split(';').each()
+        { platform ->
+            List tokens = platform.tokenize(':')
+            String osName = tokens.get(0)
+            // Statuses for builds
+            String context = "[${PRJ_NAME}] [BUILD] ${osName}"
+            commitContexts << context
+            pullRequest.createStatus("pending", context, "Scheduled", "${env.JOB_URL}")
+            if (tokens.size() > 1) {
+                gpuNames = tokens.get(1)
+                gpuNames.split(',').each()
+                { gpuName ->
+                    // Statuses for tests
+                    context = "[${PRJ_NAME}] [TEST] ${osName}-${gpuName}"
+                    commitContexts << context
+                    pullRequest.createStatus("pending", context, "Scheduled", "${env.JOB_URL}")
+                }
+            }
+        }
+    }
+
     multiplatform_pipeline(platforms, null, this.&executeBuild, this.&executeTests, null,
                            [platforms:platforms,
                             projectBranch:projectBranch,
@@ -127,4 +176,12 @@ def call(String projectBranch = "",
                             executeBuild:true,
                             executeTests:true,
                             cmakeKeys:cmakeKeys])
+
+    // set error statuses for PR, except if current build has been superseded by new execution
+    if (env.CHANGE_ID && !currentBuild.nextBuild) {
+        // if jobs was aborted or crushed remove pending status for unfinished stages
+        options['commitContexts'].each() {
+            pullRequest.createStatus("error", it, "Build has been terminated unexpectedly", "${env.BUILD_URL}")
+        }
+    }
 }
