a66a67f47fecf39ea85b28f29e3f3ad81a373db9
Andrey Alekseevich <magister.demens@gmail.com>
2019-10-31 12:01:15 +0300

    file name fix

--- vars/radeon_ml_miopen_pipeline.groovy
+++ vars/radeon_ml_miopen_pipeline.groovy
@@ -18,7 +18,7 @@ def executeTestCommand(String osName, Map options)
                 break;
             default:
                 sh """
-                chmod +x tests
+                chmod +x tests-MIOpen
                 export LD_LIBRARY_PATH=\$PWD:\$LD_LIBRARY_PATH
                 ./tests-MIOpen --gtest_output=xml:../../${STAGE_NAME}.gtest.xml >> ../../${STAGE_NAME}.log 2>&1
                 """
