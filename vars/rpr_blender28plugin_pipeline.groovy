74a95d2a046dfb0e3dd3528c5fd86b3fa32fd25d
kataderon <kataderon@gmail.com>
2020-02-06 15:08:47 +0300

    fix syntax in blender pipeline

--- vars/rpr_blender28plugin_pipeline.groovy
+++ vars/rpr_blender28plugin_pipeline.groovy
@@ -275,7 +275,7 @@ def executeTestCommand(String osName, Map options)
         dir("scripts")
         {
             sh """
-            ./run.sh ${options.renderDevice} ${options.testsPackage} \"${options.tests}\" >${options.resX} ${options.resY} ${options.SPU} ${options.iter} ${options.theshold} > ../${options.stageName}.log 2>&1
+            ./run.sh ${options.renderDevice} ${options.testsPackage} \"${options.tests}\" ${options.resX} ${options.resY} ${options.SPU} ${options.iter} ${options.theshold} >> ../${options.stageName}.log 2>&1
             """
         }
         break;
