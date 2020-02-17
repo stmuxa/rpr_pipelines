f5d0a37e96870c848d30fbb9cdaa8006df63faab
kataderon <kataderon@gmail.com>
2020-02-17 19:28:43 +0300

    fix elvis operator

--- vars/rpr_maxplugin_pipeline.groovy
+++ vars/rpr_maxplugin_pipeline.groovy
@@ -632,11 +632,10 @@ def call(String projectBranch = "",
         String iter = '50',
         String theshold = '0.05') 
 {
-    resX = (resX == 'Default') ? '0':
-    resY = (resY == 'Default') ? '0':
-    SPU = (SPU == 'Default') ? '25':
-    iter = (iter == 'Default') ? '50':
-    theshold = (theshold == 'Default') ? '0.05':
+    resX = (resX == 'Default') ? '0' : resX
+    resY = (resY == 'Default') ? '0' : resY
+    SPU = (SPU == 'Default') ? '25' : SPU
+    iter = (iter == 'Default') ? '50' : iter
 
     try
     {
