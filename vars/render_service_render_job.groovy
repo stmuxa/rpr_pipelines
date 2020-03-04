b3666f239ae7367e2d0ee48a4b2d647f06f487bc
Andrey Kravchenko <epozine@inbox.ru>
2020-03-04 17:14:20 +0300

    Update render_service_render_job.groovy

--- vars/render_service_render_job.groovy
+++ vars/render_service_render_job.groovy
@@ -19,24 +19,25 @@ def executeRender(osName, gpuName, Map options) {
 					'''
 					// Download render service scripts
 					try {
-					    print("Downloading scripts and install requirements")
-                        checkOutBranchOrScm(options['scripts_branch'], 'git@github.com:luxteam/render_service_scripts.git')
-                        dir(".\\install"){
-                            bat '''
-                                install_pylibs.bat
-                            '''
-                        }
+					        print("Downloading scripts and install requirements")
+						checkOutBranchOrScm(options['scripts_branch'], 'git@github.com:luxteam/render_service_scripts.git')
+						dir(".\\install"){
+						    bat '''
+							install_pylibs.bat
+						    '''
+						}
 					} catch(e) {
+						currentBuild.result = 'FAILURE'
 						print e
 						fail_reason = "Downloading scripts failed"
 					}
 					// download scene, check if it is already downloaded
 					try {
 					    // initialize directory RenderServiceStorage
-					    dir("..\\..\\RenderServiceStorage"){
-					        writeFile file:'test', text:'dir created'
-					    }
-						print(python3(".\\render_service_scripts\\send_render_status.py --django_ip \"${options.django_url}/\" --tool \"${tool}\" --status \"Downloading scene\" --id ${id}"))
+					    bat """
+						if not exist "..\\..\\RenderServiceStorage" mkdir "..\\..\\RenderServiceStorage"
+					    """
+						print(python3("render_service_scripts\\send_render_status.py --django_ip \"${options.django_url}/\" --tool \"${tool}\" --status \"Downloading scene\" --id ${id}"))
 						def exists = fileExists "..\\..\\RenderServiceStorage\\${scene_user}\\${scene_name}"
 						if (exists) {
 							print("Scene is copying from Render Service Storage on this PC")
@@ -56,6 +57,7 @@ def executeRender(osName, gpuName, Map options) {
 							"""
 						}
 					} catch(e) {
+						currentBuild.result = 'FAILURE'
 						print e
 						fail_reason = "Downloading scene failed"
 					}
@@ -63,13 +65,14 @@ def executeRender(osName, gpuName, Map options) {
 						case 'Blender':
 							// copy necessary scripts for render
 							bat """
-								copy ".\\render_service_scripts\\blender_render.py" "."
-								copy ".\\render_service_scripts\\launch_blender.py" "."
+								copy "render_service_scripts\\blender_render.py" "."
+								copy "render_service_scripts\\launch_blender.py" "."
 							"""
 							// Launch render
 							try {
 								python3("launch_blender.py --tool ${version} --django_ip \"${options.django_url}/\" --scene_name \"${scene_name}\" --id ${id} --build_number ${currentBuild.number} --min_samples ${options.Min_Samples} --max_samples ${options.Max_Samples} --noise_threshold ${options.Noise_threshold} --height ${options.Height} --width ${options.Width} --startFrame ${options.startFrame} --endFrame ${options.endFrame} ")
 							} catch(e) {
+								currentBuild.result = 'FAILURE'
 								print e
 								// if status == failure then copy full path and send to slack
 								bat """
@@ -82,13 +85,14 @@ def executeRender(osName, gpuName, Map options) {
 						case 'Max':
 							// copy necessary scripts for render
 							bat """
-								copy ".\\render_service_scripts\\max_render.ms" "."
-								copy ".\\render_service_scripts\\launch_max.py" "."
+								copy "render_service_scripts\\max_render.ms" "."
+								copy "render_service_scripts\\launch_max.py" "."
 							"""
 							// Launch render
 							try {
 								python3("launch_max.py --tool ${version} --django_ip \"${options.django_url}/\" --scene_name \"${scene_name}\" --id ${id} --build_number ${currentBuild.number} --min_samples ${options.Min_Samples} --max_samples ${options.Max_Samples} --noise_threshold ${options.Noise_threshold} --width ${options.Width} --height ${options.Height} --startFrame ${options.startFrame} --endFrame ${options.endFrame} ")
 							} catch(e) {
+								currentBuild.result = 'FAILURE'
 								print e
 								// if status == failure then copy full path and send to slack
 								bat """
@@ -101,13 +105,14 @@ def executeRender(osName, gpuName, Map options) {
 						case 'Maya':
 							// copy necessary scripts for render	
 							bat """
-								copy ".\\render_service_scripts\\maya_render.py" "."
-								copy ".\\render_service_scripts\\launch_maya.py" "."
+								copy "render_service_scripts\\maya_render.py" "."
+								copy "render_service_scripts\\launch_maya.py" "."
 							"""
 							// Launch render
 							try {
 								python3("launch_maya.py --tool ${version} --django_ip \"${options.django_url}/\" --scene_name \"${scene_name}\" --id ${id} --build_number ${currentBuild.number} --min_samples ${options.Min_Samples} --max_samples ${options.Max_Samples} --noise_threshold ${options.Noise_threshold} --width ${options.Width} --height ${options.Height} --startFrame ${options.startFrame} --endFrame ${options.endFrame} ")
 							} catch(e) {
+								currentBuild.result = 'FAILURE'
 								print e
 								// if status == failure then copy full path and send to slack
 								bat """
@@ -120,13 +125,34 @@ def executeRender(osName, gpuName, Map options) {
 						case 'Maya (Redshift)':
 							// copy necessary scripts for render	
 							bat """
-								copy ".\\render_service_scripts\\redshift_render.py" "."
-								copy ".\\render_service_scripts\\launch_maya_redshift.py" "."
+								copy "render_service_scripts\\redshift_render.py" "."
+								copy "render_service_scripts\\launch_maya_redshift.py" "."
 							"""
 							// Launch render
 							try {
 								python3("launch_maya_redshift.py --tool ${version} --django_ip \"${options.django_url}/\" --id ${id} --build_number ${currentBuild.number} --min_samples ${options.Min_Samples} --max_samples ${options.Max_Samples} --noise_threshold ${options.Noise_threshold} --width ${options.Width} --height ${options.Height} --startFrame ${options.startFrame} --endFrame ${options.endFrame} ")
 							} catch(e) {
+								currentBuild.result = 'FAILURE'
+								print e
+								// if status == failure then copy full path and send to slack
+								bat """
+									mkdir "..\\..\\RenderServiceStorage\\failed_${scene_name}_${id}_${currentBuild.number}"
+									copy "*" "..\\..\\RenderServiceStorage\\failed_${scene_name}_${id}_${currentBuild.number}"
+								"""
+							}
+							break;
+					
+						case 'Maya (Arnold)':
+							// copy necessary scripts for render	
+							bat """
+								copy "render_service_scripts\\arnold_render.py" "."
+								copy "render_service_scripts\\launch_maya_arnold.py" "."
+							"""
+							// Launch render
+							try {
+								python3("launch_maya_arnold.py --tool ${version} --django_ip \"${options.django_url}/\" --id ${id} --build_number ${currentBuild.number} --min_samples ${options.Min_Samples} --max_samples ${options.Max_Samples} --noise_threshold ${options.Noise_threshold} --width ${options.Width} --height ${options.Height} --startFrame ${options.startFrame} --endFrame ${options.endFrame} ")
+							} catch(e) {
+								currentBuild.result = 'FAILURE'
 								print e
 								// if status == failure then copy full path and send to slack
 								bat """
@@ -159,7 +185,7 @@ def executeRender(osName, gpuName, Map options) {
 				} catch(e) {
 					currentBuild.result = 'FAILURE'
 					print e
-					print(python3(".\\render_service_scripts\\send_render_results.py --django_ip \"${options.django_url}/\" --build_number ${currentBuild.number} --status ${currentBuild.result} --fail_reason \"${fail_reason}\" --id ${id}"))
+					print(python3("render_service_scripts\\send_render_results.py --django_ip \"${options.django_url}/\" --build_number ${currentBuild.number} --status ${currentBuild.result} --fail_reason \"${fail_reason}\" --id ${id}"))
 				}
 				break;
 		}
