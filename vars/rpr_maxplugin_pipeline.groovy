c060deed2cced4469c34f427c368b4049d02a125
Ilia Nemankov <INemankov@luxoft.com>
2020-03-17 20:16:28 +0200

    Fix logs archiving in case of pre-built plugin usage in rpr_maxplugin_pipeline

--- vars/rpr_maxplugin_pipeline.groovy
+++ vars/rpr_maxplugin_pipeline.groovy
@@ -372,7 +372,9 @@ def executeBuild(String osName, Map options)
         throw e
     }
     finally {
-        archiveArtifacts "*.log"
+        if (!isPreBuilt) {
+            archiveArtifacts "*.log"
+        }
     }
 }
 
